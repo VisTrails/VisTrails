32943f0d4949317fa5dd18bdc41748ad7341a7cd
Carlos Scheidegger <cscheid@sci.utah.edu>
2006-10-27 23:24:59 +0000

    Uses new VisApplication interface. Only imports vis_application if __name__ == '__main__'.

--- vistrails/vistrails.py
+++ vistrails/vistrails.py
@@ -1,18 +1,17 @@
 """Main file for the VisTrails distribution."""
 
-from gui.vis_application import VistrailsApplication
-
-import sys
-try:
-    app = VistrailsApplication()
-except SystemExit, e:
-    sys.exit(e)
-except Exception, e:
-    print "Uncaught exception on initialization: %s" % e
-    import traceback
-    traceback.print_exc()
-    sys.exit(255)
-if app.configuration.interactiveMode:
-    v = app.exec_()
-    sys.exit(v)
-    
+if __name__ == '__main__':
+    import gui.vis_application
+    import sys
+    try:
+        app = gui.vis_application.VistrailsApplication()
+    except SystemExit, e:
+        sys.exit(e)
+    except Exception, e:
+        print "Uncaught exception on initialization: %s" % e
+        import traceback
+        traceback.print_exc()
+        sys.exit(255)
+    if app.configuration.interactiveMode:
+        v = app.exec_()
+        sys.exit(v)
