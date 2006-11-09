c372f72727afd475499cf0da89c65202afa728f7
Huy Vo <hvo@sci.utah.edu>
2006-11-09 20:13:25 +0000

    trunk/vistrails/core/common.py:  - TestCommon::testMemo: fixed division by zero where the execution time is 0.0.

--- vistrails/core/common.py
+++ vistrails/core/common.py
@@ -214,8 +214,10 @@ class TestCommon(unittest.TestCase):
         d1 = t2 - t1
         d2 = t3 - t2
         d3 = t4 - t3
-        r1 = d2 / d1
-        r2 = d3 / d2
+        if d1==0: r1 = 0
+        else: r1 = d2 / d1
+        if d2==0: r2 =0
+        else: r2 = d3 / d2
         self.assertEquals(r1< 2.618, True)
         self.assertEquals(r2< 2.618, True)
     def testMemo2(self):
