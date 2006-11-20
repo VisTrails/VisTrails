00090f220bf9632e2b0740c86d18b2ef5b2bd58c
Carlos Scheidegger <cscheid@sci.utah.edu>
2006-11-20 22:59:41 +0000

    Fixed outstanding issues so that HTTP package is working and useful. Renamed methods so that they conform to coding conventions. Added docstring to module.

--- vistrails/packages/HTTP/__init__.py
+++ vistrails/packages/HTTP/__init__.py
@@ -1,31 +1,49 @@
-import modules
-import modules.module_registry
-from modules.vistrails_module import Module, ModuleError
+"""HTTP provides packages for HTTP-based file fetching. This provides
+a location-independent way of referring to files. This package uses a
+local cache of the files, inside the per-user VisTrails
+directory. This way, files that haven't been changed do not need
+downloading. The check is performed efficiently using the HTTP GET
+headers.
+"""
 
+from core.modules.vistrails_module import Module, ModuleError
+import core.modules
+import core.modules.basic_modules
+import core.modules.module_registry
+import core.system
 import httplib
+import os.path
+import sys
+import time
+import urllib
+
+package_directory = None
+
+###############################################################################
 
 class HTTP(Module):
-    def compute(self):
-        pass
+    pass
 
-class HTTPGetFile(HTTP):
+class HTTPFile(HTTP):
 
     def __init__(self):
-        cal = {}
-        cal['Jan'] = 1
-        cal['Feb'] = 2
-        cal['Mar'] = 3
-        cal['Apr'] = 4
-        cal['May'] = 5
-        cal['Jun'] = 6
-        cal['Jul'] = 7
-        cal['Aug'] = 8
-        cal['Sep'] = 9
-        cal['Oct'] = 10
-        cal['Nov'] = 11
-        cal['Dec'] = 12
+        HTTP.__init__(self)
+        self.cal = {}
+        self.cal['Jan'] = 1
+        self.cal['Feb'] = 2
+        self.cal['Mar'] = 3
+        self.cal['Apr'] = 4
+        self.cal['May'] = 5
+        self.cal['Jun'] = 6
+        self.cal['Jul'] = 7
+        self.cal['Aug'] = 8
+        self.cal['Sep'] = 9
+        self.cal['Oct'] = 10
+        self.cal['Nov'] = 11
+        self.cal['Dec'] = 12
     
-    def parseURL(self, url):
+    def parse_url(self, url):
+        # TODO: There's gotta be a urllib method for this.
         url = url.strip('http://')
         split = url.split('/')
         self.host = split[0]
@@ -41,18 +59,15 @@ class HTTPGetFile(HTTP):
         suf = suf.split('.')
         self.suffix = suf[suf.__len__() - 1]
 
-    def isOutdated(self, remoteHeader, localFile):
-        # This return is to account for the vis_application.py
-        # and Qt problems discussed in the check-in.  Upon
-        # resolution of this, we simply must remove this
-        # line and full functionality will be restored.
-        return True
+    def is_outdated(self, remoteHeader, localFile):
+        """Checks whether local file is up-to-date."""
+        # TODO: There's gotta be a time method for this.
         mod = remoteHeader.split()
-        day = mod[1]
-        mon = self.cal[mod[2]]
-        yr = mod[3]
+        day = int(mod[1])
+        mon = int(self.cal[mod[2]])
+        yr = int(mod[3])
         t = mod[4]
-        ltime = time.localtime(os.path.getmtime(localFile))
+        ltime = time.gmtime(os.path.getmtime(localFile))
         if ltime[0] < yr:
             return True
         if ltime[1] < mon:
@@ -61,46 +76,63 @@ class HTTPGetFile(HTTP):
             return True
         
         t = t.split(':')
-        if ltime[3] < t[0]:
+        if ltime[3] < int(t[0]):
             return True
-        if ltime[4] < t[1]:
+        if ltime[4] < int(t[1]):
             return True
-        if ltime[5] < t[2]:
+        if ltime[5] < int(t[2]):
             return True
 
         return False
     
     def compute(self):
-        url = self.getInputFromPort("URL")
-        self.parseURL(url)
+        self.checkInputPort('url')
+        url = self.getInputFromPort("url")
+        self.parse_url(url)
         conn = httplib.HTTPConnection(self.host)
         conn.request("GET", self.filename)
         response = conn.getresponse()
-        mod = response.msg.getheader('Last-Modified')
-        
-        if self.isOutdated(mod, localfile):
+        mod = response.msg.getheader('last-modified')
+        local_filename = package_directory + '/' + urllib.quote_plus(url)
+        result = core.modules.basic_modules.File()
+        result.name = local_filename
+        if (not os.path.isfile(local_filename) or
+            not mod or
+            self.is_outdated(mod, local_filename)):
+            # FIXME: This is bad for large files
             data = response.read()
-            f = self.interpreter.filePool.createFile(suffix=self.suffix)
-            fn = open(f.name, "w")
-            f.write(data)
-            f.close()
-            conn.close()
-            self.setResult("File", f)
-            self.setResult("Filename", f.name)
-        else:
-            result = basic_modules.File()
-            result.name = localfile
-            result.upToDate = True
-            conn.close()
-            self.setResult("File", result)
+            try:
+                fn = file(local_filename, "w")
+            except:
+                raise ModuleError(self, ("Could not create local file '%s'" %
+                                         local_filename))
+            fn.write(data)
+            fn.close()
+        conn.close()
+        self.setResult("file", result)
+        self.setResult("local_filename", local_filename)
             
         
 def initialize(*args, **keywords):
-    reg = modules.module_registry
-    basic = modes.basic_modules
+    reg = core.modules.module_registry
+    basic = core.modules.basic_modules
 
     reg.addModule(HTTP)
 
-    reg.addModule(HTTPGetFile)
-    reg.addInputPort(HTTPGetFile, "URL", (basic.String, 'URL'))
-    reg.addOutputPort(HTTPGetFile, "File", (basic.File, 'File'))
+    reg.addModule(HTTPFile)
+    reg.addInputPort(HTTPFile, "url", (basic.String, 'URL'))
+    reg.addOutputPort(HTTPFile, "file", (basic.File, 'local File object'))
+    reg.addOutputPort(HTTPFile, "local_filename", (basic.String, 'local filename'))
+
+    global package_directory
+    package_directory = core.system.defaultDotVistrails() + "/HTTP"
+
+    if not os.path.isdir(package_directory):
+        try:
+            print "Creating package directory..."
+            os.mkdir(package_directory)
+        except:
+            print "Could not create package directory. Make sure"
+            print "'%s' does not exist and parent directory is writable"
+            sys.exit(1)
+        print "Ok."
