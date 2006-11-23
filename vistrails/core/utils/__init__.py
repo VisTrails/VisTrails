220ad8458151d64677f40d1a32268d955607373e
Carlos Scheidegger <cscheid@sci.utah.edu>
2006-11-23 00:44:50 +0000

    Adds tracemethod.

--- vistrails/core/utils/__init__.py
+++ vistrails/core/utils/__init__.py
@@ -3,6 +3,7 @@ This module defines common functions and exception class definitions
 used all over VisTrails.
 """
 from core.utils.enum import enum
+from core.utils.tracemethod import trace_method, bump_trace
 
 ################################################################################
 
