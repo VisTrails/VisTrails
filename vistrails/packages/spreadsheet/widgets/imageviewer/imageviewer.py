60d4f50abd0f37f2ad566b92bfe48da7307b3496
Carlos Scheidegger <cscheid@sci.utah.edu>
2006-11-30 21:56:43 +0000

    ImageViewerCell.compute() - Holds a link to the file to prevent temporary deletion.

--- vistrails/packages/spreadsheet/widgets/imageviewer/imageviewer.py
+++ vistrails/packages/spreadsheet/widgets/imageviewer/imageviewer.py
@@ -5,6 +5,7 @@ from PyQt4 import QtCore, QtGui
 from core.modules.vistrails_module import Module
 from packages.spreadsheet.basic_widgets import SpreadsheetCell
 from packages.spreadsheet.spreadsheet_helpers import CellToolBar
+from packages.spreadsheet.spreadsheet_controller import spreadsheetController
 import imageviewer_rc
 
 ################################################################################
@@ -20,7 +21,9 @@ class ImageViewerCell(SpreadsheetCell):
         
         """
         if self.hasInputFromPort("File"):
-            fileValue = self.getInputFromPort("File")
+            window = spreadsheetController.findSpreadsheetWindow()
+            file_to_display = self.getInputFromPort("File")
+            fileValue = window.file_pool.make_local_copy(file_to_display.name)
         else:
             fileValue = None
         self.display(ImageViewerCellWidget, (fileValue, ))
