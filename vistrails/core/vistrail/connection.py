5152350f6c4b11074de7d83a6f3d09577a17d5a9
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-01-18 23:55:59 +0000

    Connection.__init__(): Empty connection now has sensible port source and destination types.

--- vistrails/core/vistrail/connection.py
+++ vistrails/core/vistrail/connection.py
@@ -1,3 +1,24 @@
+############################################################################
+##
+## Copyright (C) 2006-2007 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
 """ This python module defines Connection class.
 """
 if __name__ == '__main__':
@@ -78,6 +99,8 @@ class Connection(object):
         """
         self.__source = Port()
         self.__dest = Port()
+        self.source.endPoint = PortEndPoint.Source
+        self.destination.endPoint = PortEndPoint.Destination
 
     def findSignature(self, sig, signatures):
         """findSignature(sig:str, signatures:[]) -> str 
@@ -305,6 +328,12 @@ class TestConnection(unittest.TestCase):
         def bogus(asd):
             return 3
         assert type(c) == type(bogus)
+
+    def testEmptyConnection(self):
+        """Tests sane initialization of empty connection"""
+        c = Connection()
+        self.assertEquals(c.source.endPoint, PortEndPoint.Source)
+        self.assertEquals(c.destination.endPoint, PortEndPoint.Destination)
         
 if __name__ == '__main__':
     unittest.main()
