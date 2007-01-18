e737f7c386677529a829871df954046e0743252e
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-01-18 23:53:37 +0000

    imports lock_method from lockmethod.py

--- vistrails/core/utils/__init__.py
+++ vistrails/core/utils/__init__.py
@@ -1,3 +1,24 @@
+############################################################################
+##
+## Copyright (C) 2006-2007 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
 """
 This module defines common functions and exception class definitions
 used all over VisTrails.
@@ -5,6 +26,7 @@ used all over VisTrails.
 from core.utils.enum import enum
 from core.utils.tracemethod import trace_method, bump_trace
 from core.utils.color import ColorByName
+from core.utils.lockmethod import lock_method
 
 ################################################################################
 
