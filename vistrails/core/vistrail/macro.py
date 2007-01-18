eb867996c4a68460ff3da2e939000c167f354424
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-01-18 23:56:14 +0000

    Removed import *.

--- vistrails/core/vistrail/macro.py
+++ vistrails/core/vistrail/macro.py
@@ -1,3 +1,24 @@
+############################################################################
+##
+## Copyright (C) 2006-2007 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
 """ This module is responsible for dealing with macros inside a vistrail.
 These are the classes defined here:
  - Macro
@@ -9,7 +30,7 @@ import copy
 from core.utils import unimplemented, abstract, VistrailsInternalError, enum
 from core.vistrail.connection import Connection
 from core.data_structures import Point
-from core.vistrail.action import *
+from core.vistrail.action import Action
 
 ###############################################################################
 
