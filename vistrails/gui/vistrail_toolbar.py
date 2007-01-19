f5af08df66f55be22749c31c1f13d8724a9f1559
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-01-19 00:10:57 +0000

    Mass commit that adds copyright and GPL notice header to VisTrails files.

--- vistrails/gui/vistrail_toolbar.py
+++ vistrails/gui/vistrail_toolbar.py
@@ -1,3 +1,24 @@
+############################################################################
+##
+## Copyright (C) 2006-2007 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
 """ This holds a customized toolbar class for QVistrailView
 
 QVistrailViewPaddedTabBar
@@ -360,5 +381,5 @@ class QVistrailViewTabBar(QtGui.QTabBar):
         self.addTab('Query')
         self.invisible = False
         self.setSizePolicy(QtGui.QSizePolicy.Maximum,
-                                QtGui.QSizePolicy.Maximum)
+                           QtGui.QSizePolicy.Maximum)
         self.setCurrentIndex(1)
