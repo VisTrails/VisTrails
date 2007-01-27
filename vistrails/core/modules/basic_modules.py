5c876481fd74d96dbe1392852b26510e611b6931
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-01-27 01:15:13 +0000

    File: new outputPort local_filename
    Unzip: new module.

--- vistrails/core/modules/basic_modules.py
+++ vistrails/core/modules/basic_modules.py
@@ -28,6 +28,9 @@ from core.modules import port_configure
 from core.modules import vistrails_module
 import core.system
 import os
+import zipfile
+
+_reg = module_registry.registry
 
 ###############################################################################
 
@@ -68,6 +71,9 @@ class Constant(vistrails_module.Module):
     def valueAsString(self):
         return str(self)
 
+_reg.addModule(Constant)
+
+
 def new_constant(name, conversion):
     """new_constant(conversion) -> Module
 
@@ -88,6 +94,22 @@ def new_constant(name, conversion):
     module_registry.registry.addOutputPort(m, "value", m)
     return m
 
+def bool_conv(x):
+    s = str(x).upper()
+    if s == 'TRUE':
+        return True
+    if s == 'FALSE':
+        return False
+    raise ValueError('Boolean from String in VisTrails should be either \
+"true" or "false"')
+
+Boolean = new_constant('Boolean' , bool_conv)
+Float   = new_constant('Float'   , float)
+Integer = new_constant('Integer' , int)
+String  = new_constant('String'  , str)
+_reg.addOutputPort(Constant, "value_as_string", String)
+
+##############################################################################
 
 class File(vistrails_module.Module):
     """File is a VisTrails Module that represents a file stored on a
@@ -97,9 +119,14 @@ class File(vistrails_module.Module):
         self.checkInputPort("name")
         n = self.getInputFromPort("name")
         self.name = n
+        self.setResult("local_filename", self.name)
 
-    def __str__(self):
-        return "file"
+_reg.addModule(File)
+_reg.addInputPort(File, "name", String)
+_reg.addOutputPort(File, "self", File)
+_reg.addOutputPort(File, "local_filename", String)
+
+##############################################################################
 
 class FileSink(vistrails_module.Module):
     """FileSink is a VisTrails Module that takes a file and writes it
@@ -125,6 +152,12 @@ class FileSink(vistrails_module.Module):
                 msg = "Could not create file '%s': %s" % (v2, e)
                 raise vistrails_module.ModuleError(self, msg)
 
+_reg.addModule(FileSink)
+_reg.addInputPort(FileSink,  "file", File)
+_reg.addInputPort(FileSink,  "outputName", String)
+_reg.addInputPort(FileSink,  "overrideFile", Boolean)
+
+##############################################################################
 
 # class OutputWindow(vistrails_module.Module):
     
@@ -135,6 +168,12 @@ class FileSink(vistrails_module.Module):
 #                                       "VisTrails",
 #                                       str(v))
 
+#Removing Output Window because it does not work with current threading
+#reg.addModule(OutputWindow)
+#reg.addInputPort(OutputWindow, "value",
+#                               vistrails_module.Module)
+
+##############################################################################
 
 class StandardOutput(vistrails_module.Module):
     """StandardOutput is a VisTrails Module that simply prints the
@@ -144,6 +183,11 @@ class StandardOutput(vistrails_module.Module):
     def compute(self):
         v = self.getInputFromPort("value")
         print v
+
+_reg.addModule(StandardOutput)
+_reg.addInputPort(StandardOutput, "value", vistrails_module.Module)
+
+##############################################################################
 
 # Tuple will be reasonably magic right now. We'll integrate it better
 # with vistrails later.
@@ -156,6 +200,9 @@ class Tuple(vistrails_module.Module):
     def compute(self):
         values = tuple([self.getInputFromPort(i) for i in range(self.length)])
         self.setResult("value", values)
+_reg.addModule(Tuple)
+
+##############################################################################
 
 # TODO: Create a better Module for ConcatenateString.
 class ConcatenateString(vistrails_module.Module):
@@ -175,10 +222,16 @@ class ConcatenateString(vistrails_module.Module):
             port = "str%s" % v
             if self.hasInputFromPort(port):
                 inp = self.getInputFromPort(port)
-                print inp
                 result += inp
         self.setResult("value", result)
+_reg.addModule(ConcatenateString)
+for i in range(ConcatenateString.fieldCount):
+    j = i+1
+    port = "str%s" % j
+    _reg.addInputPort(ConcatenateString, port, String)
+_reg.addOutputPort(ConcatenateString, "value", String)
 
+##############################################################################
 
 # TODO: Create a better Module for List.
 class List(vistrails_module.Module):
@@ -201,12 +254,24 @@ class List(vistrails_module.Module):
 
         self.setResult("value", head + tail)
 
+_reg.addModule(List)
+
+_reg.addInputPort(List, "head", vistrails_module.Module)
+_reg.addInputPort(List, "tail", List)
+_reg.addOutputPort(List, "value", List)
+
+##############################################################################
+
 # TODO: Null should be a subclass of Constant?
 class Null(vistrails_module.Module):
     """Null is the class of None values."""
     
     def compute(self):
         self.setResult("value", None)
+
+_reg.addModule(Null)
+
+##############################################################################
 
 class PythonSource(vistrails_module.Module):
     """PythonSource is a Module that executes an arbitrary piece of
@@ -219,71 +284,71 @@ class PythonSource(vistrails_module.Module):
         import urllib
         exec urllib.unquote(str(self.forceGetInputFromPort('source', '')))
 
+_reg.addModule(PythonSource,
+              None, module_configure.PythonSourceConfigurationWidget)
+_reg.addInputPort(PythonSource, 'source', String, True)
+
+##############################################################################
+
 class TestPortConfig(vistrails_module.Module):
     
     def compute(self):
         pass
-    
-###############################################################################
 
-reg = module_registry.registry
-
-reg.addModule(Constant)
-
-def bool_conv(x):
-    s = str(x).upper()
-    if s == 'TRUE':
-        return True
-    if s == 'FALSE':
-        return False
-    raise ValueError('Boolean from String in VisTrails should be either \
-"true" or "false"')
-
-Boolean = new_constant('Boolean' , bool_conv)
-Float   = new_constant('Float'   , float)
-Integer = new_constant('Integer' , int)
-String  = new_constant('String'  , str)
-
-reg.addOutputPort(Constant, "value_as_string", String)
-
-reg.addModule(Null)
-
-reg.addModule(File)
-reg.addInputPort(File, "name", String)
-reg.addOutputPort(File, "self", File)
-
-reg.addModule(FileSink)
-reg.addInputPort(FileSink,  "file", File)
-reg.addInputPort(FileSink,  "outputName", String)
-reg.addInputPort(FileSink,  "overrideFile", Boolean)
-
-#Removing Output Window because it does not work with current threading
-#reg.addModule(OutputWindow)
-#reg.addInputPort(OutputWindow, "value",
-#                               vistrails_module.Module)
-
-reg.addModule(StandardOutput)
-reg.addInputPort(StandardOutput, "value", vistrails_module.Module)
-
-reg.addModule(PythonSource,
-              None, module_configure.PythonSourceConfigurationWidget)
-reg.addInputPort(PythonSource, 'source', String, True)
-
-reg.addModule(TestPortConfig)
-reg.addInputPort(TestPortConfig,
+_reg.addModule(TestPortConfig)
+_reg.addInputPort(TestPortConfig,
                  'ColorPort', [Float,Float,Float],
                  False, port_configure.ColorConfigurationWidget)
-reg.addInputPort(TestPortConfig, 'IntegerPort', Integer)
+_reg.addInputPort(TestPortConfig, 'IntegerPort', Integer)
 
-reg.addModule(Tuple)
-reg.addModule(ConcatenateString)
-for i in range(ConcatenateString.fieldCount):
-    j = i+1
-    port = "str%s" % j
-    reg.addInputPort(ConcatenateString, port, String)
-reg.addOutputPort(ConcatenateString, "value", String)
+##############################################################################
 
-reg.addModule(List)
-reg.addInputPort(List, "head", vistrails_module.Module)
-reg.addInputPort(List, "tail", List)
-reg.addOutputPort(List, "value", List)
+class _ZIPDecompressor(object):
+
+    """_ZIPDecompressor extracts a file from a .zip file. On Win32, uses
+the zipfile library from python. On Linux/Macs, uses command line, because
+it avoids moving the entire file contents to/from memory."""
+
+    # TODO: Figure out a way of doing this right on Win32
+
+    def __init__(self, archive, filename_in_archive, output_filename):
+        self._archive = archive
+        self._filename_in_archive = filename_in_archive
+        self._output_filename = output_filename
+
+    if core.system.systemType in ['Windows', 'Microsoft']:
+        def extract(self):
+            import zipfile
+            output_file = file(self._output_filename, 'w')
+            zip_file = zipfile.ZipFile(self._archive)
+            contents = zip_file.read(self._filename_in_archive)
+            output_file.write(contents)
+            output_file.close()
+    else:
+        def extract(self):
+            os.system("unzip -p %s %s > %s" %
+                      (self._archive,
+                       self._filename_in_archive,
+                       self._output_filename))
+            
+
+class Unzip(vistrails_module.Module):
+    """Unzip extracts a file from a ZIP archive."""
+
+    def compute(self):
+        self.checkInputPort("archive_file")
+        self.checkInputPort("filename_in_archive")
+        filename_in_archive = self.getInputFromPort("filename_in_archive")
+        archive_file = self.getInputFromPort("archive_file")
+        suffix = self.interpreter.filePool.guess_suffix(filename_in_archive)
+        output = self.interpreter.filePool.create_file(suffix=suffix)
+        dc = _ZIPDecompressor(archive_file.name,
+                              filename_in_archive,
+                              output.name)
+        dc.extract()
+        self.setResult("file", output)
+
+_reg.addModule(Unzip)
+_reg.addInputPort(Unzip, 'archive_file', File)
+_reg.addInputPort(Unzip, 'filename_in_archive', String)
+_reg.addOutputPort(Unzip, 'file', File)
