47e4b907044fec0712a83a3ca60ba4990996733a
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-03-15 15:24:12 +0000

    Updated ubuntu dist generator to newest version, now includes examples and data.

--- dist/ubuntu/create_debian_pkg.sh
+++ dist/ubuntu/create_debian_pkg.sh
@@ -2,6 +2,7 @@
 rm -rf debian
 mkdir -p debian/usr/local/bin
 mkdir -p debian/usr/local/vistrails
+mkdir -p debian/usr/share/vistrails/examples/data
 mkdir -p debian/DEBIAN
 cp _debian/* debian/DEBIAN
 
@@ -17,6 +18,16 @@ cp -R ../../vistrails debian/usr/local
 find debian/usr/local/vistrails -name '.svn' -exec rm -rf {} \; 2>/dev/null
 find debian/usr/local/vistrails -name '*~' -exec rm {} \; 2>/dev/null
 find debian/usr/local/vistrails -name '*.pyc' -exec rm {} \; 2>/dev/null
+
+cp ../../examples/*.xml debian/usr/share/vistrails/examples
+cp ../../examples/data/carotid.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/gktbhFA.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/gktbhL123.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/head.120.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/mummy.128.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/spx.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/torus.vtk debian/usr/share/vistrails/examples/data
+cp ../../examples/data/vslice_circ1.bp debian/usr/share/vistrails/examples/data
 
 dpkg-deb --build debian
 mv debian.deb vistrails.deb
