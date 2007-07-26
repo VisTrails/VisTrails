41e49de9ec72f5a0dea97cec64d0aaf386ab6c90
Carlos Scheidegger <cscheid@sci.utah.edu>
2007-07-26 22:27:01 +0000

    -- More of the same. --
    gui/extras: new directory
    
    gui/extras/__init__.py: new file
    
    gui/extras/db: new directory
    
    gui/extras/db/__init__.py: new file
    
    gui/extras/db/services: new directory
    
    gui/extras/db/services/__init__.py: new file
    
    gui/extras/db/services/io.py: new file

---
+++ vistrails/gui/extras/db/services/io.py
@@ -0,0 +1,108 @@
+############################################################################
+##
+## Copyright (C) 2006-2007 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
+
+from gui.open_db_window import QOpenDBWindow
+from db.services.io import DBLocator, XMLFileLocator
+from PyQt4 import QtGui
+import core.system
+import os
+
+##############################################################################
+# DB dialogs
+
+def get_load_db_locator_from_gui(parent):
+    config, vistrailId = QOpenDBWindow.getOpenVistrail()
+    if config == {} or vistrailId == -1:
+        return None
+    return DBLocator(config['host'],
+                     config['port'],
+                     config['db'],
+                     vistrailId,
+                     config.get('conn_id', None))
+
+def get_save_db_locator_from_gui(parent, locator=None):
+    if locator is None:
+        config, name = QOpenDBWindow, getSaveVistrail()
+        if config == {} or name == '':
+            return None
+        return DBLocator(config['host'],
+                         config['port'],
+                         config['db'],
+                         vistrailId,
+                         config.get('conn_id', None))
+
+    name = QtGui.QInputDialog.getText(parent,
+                                      "Save Vistrail...",
+                                      "Vistrail name:",
+                                      QtGui.QLineEdit.Normal,
+                                      "",
+                                      False)
+    if name.isEmpty():
+        return None
+    return DBLocator(locator.host,
+                     locator.port,
+                     locator.db,
+                     locator.vistrail_id,
+                     locator.connection_id)
+                     
+    
+
+##############################################################################
+# XML File dialogs
+
+def get_load_xml_file_locator_from_gui(parent):
+    fileName = QtGui.QFileDialog.getOpenFileName(
+        parent,
+        "Open Vistrail...",
+        core.system.vistrails_directory(),
+        "Vistrail files (*.xml)\nOther files (*)")
+    if fileName.isEmpty():
+        return None
+    return XMLFileLocator(str(fileName))
+
+def get_save_xml_file_locator_from_gui(parent, locator=None):
+    # Ignore current locator for now
+    # In the future, use locator to guide GUI for better starting directory
+    print parent
+    fileName = QtGui.QFileDialog.getSaveFileName(
+        parent,
+        "Save Vistrail...",
+        core.system.vistrails_directory(),
+        "VisTrails files (*.xml)",
+        None,
+        QtGui.QFileDialog.DontConfirmOverwrite)
+    if fileName.isEmpty():
+        return None
+    f = str(fileName)
+    if not f.endswith('.xml'):
+        f += '.xml'
+    if os.path.isfile(f):
+        msg = QtGui.QMessageBox(QtGui.QMessageBox.Question,
+                                "VisTrails",
+                                "File exists. Overwrite?",
+                                (QtGui.QMessageBox.Yes |
+                                 QtGui.QMessageBox.No),
+                                parent)
+        if msg.exec_() == QtGui.QMessageBox.No:
+            return None
+    return XMLFileLocator(f)
+
