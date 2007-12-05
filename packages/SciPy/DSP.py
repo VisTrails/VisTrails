4d469207fe012b5848a2759527278ad44633ae2e
Erik ANderson <eranders@sci.utah.edu>
2007-12-05 23:08:46 +0000

    Added support for 2D FFT and automatic Windowed Fourier Transform.  The 2D FFT module is FFT2 and provides output in the form of a 2D matrix of the appropriate size.  THe WindowedFourierTransform module takes as inputs the Signal and Sampling Rate of the input and outputs a SparseMatrix where the rows are the timeslices and the columns are the FFT output for that timeslice.  If the user wants more control over this, there are optional inputs Window Size and Stride.  The Window Size input controls the size of the sliding window and the Stride input controls the number of samples the window moves per time-slice.  The default to the sampling rate and the samplingrate/2 respectively.

--- packages/SciPy/DSP.py
+++ packages/SciPy/DSP.py
@@ -24,7 +24,8 @@ import core.modules.module_registry
 from core.modules.vistrails_module import Module, ModuleError
 from SciPy import SciPy
 from Matrix import *
-from scipy import sparse, fft
+from scipy import sparse, fftpack
+import numpy
 
 #################################################################
 
@@ -36,11 +37,58 @@ class FFT(DSP):
     def compute(self):
         mat = self.getInputFromPort("Signals")
         pts = self.getInputFromPort("FFT Samples")
-        if hasattr(fft,"fft"):
-            phasors = fft.fft(mat.matrix.data, pts)
-        else:
-            phasors = fft(mat.matrix.data, pts)
+
+        phasors = fftpack.fft(mat.matrix.data, pts)
         outmat = sparse.csc_matrix(phasors)
         out = SparseMatrix()
         out.matrix = outmat
+        self.setResult("FFT Output", out)
+
+class FFT2(DSP):
+    def compute(self):
+        mat = self.getInputFromPort("Signals")
+
+        phasors = fftpack.fftn(mat.matrix.data)
+        outmat = sparse.csc_matrix(phasors)
+        out = SparseMatrix()
+        out.matrix = outmat
+        self.setResult("FFT Output", out)
+
+class WindowedFourierTransform(DSP):
+    def compute(self):
+        mat = self.getInputFromPort("Signal")
+        
+        sr = self.getInputFromPort("Sampling Rate")
+        if self.hasInputFromPort("Window Size"):
+            window = self.getInputFromPort("Window Size")
+        else:
+            window = sr
+
+        if self.hasInputFromPort("Stride"):
+            stride = self.getInputFromPort("Stride")
+        else:
+            stride = int(sr / 2)
+
+        
+        signal_array = mat.matrix.toarray().ravel()
+
+        # We now have a 1-D array that we can have good indexing into
+        pad = signal_array[0:int(window/2)]
+        signal_array = numpy.concatenate((pad,signal_array))
+        win_low = 0
+        win_hi = window - 1
+        phasors = fftpack.fft(signal_array[win_low:win_hi])
+        out_array = phasors.ravel()
+
+        win_low += stride
+        win_hi += stride
+
+        while win_hi < signal_array.shape[0]:
+            phasors = fftpack.fft(signal_array[win_low:win_hi])
+            win_low += stride
+            win_hi += stride
+            out_array = numpy.vstack([out_array, phasors.ravel()])
+
+        out = SparseMatrix()
+        out.matrix = sparse.csc_matrix(out_array)
         self.setResult("FFT Output", out)
