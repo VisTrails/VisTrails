0ae73691fa0835e474d9348cb29884e0e7f718c1
Carlos Scheidegger <cscheid@sci.utah.edu>
2008-06-21 17:57:44 +0000

    Script to fix paths so that test suite can be run on macs

---
+++ vistrails/tests/run_on_mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+THIS_DIR=`dirname $0`
+# WATCH OUT!
+# You have to change BASE_DIR so that it points to the bundle you're currently using.
+BASE_DIR="${THIS_DIR}/../../../VisTrails Trunk Bundle/Vistrails Trunk.app/Contents"
+LIB_DIR=${BASE_DIR}/Resources/lib/python2.5
+PYTHONPATH=$PYTHONPATH:${LIB_DIR}:${LIB_DIR}/lib-dynload "${BASE_DIR}/MacOS/python" ${THIS_DIR}/runtestsuite.py
