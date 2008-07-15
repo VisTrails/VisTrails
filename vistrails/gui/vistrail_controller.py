a976035896da0352b9f12ff876fc3a7ceb444a33
Emanuele Santos <emanuele@sci.utah.edu>
2008-07-15 18:55:07 +0000

    VistrailController.ungroup()   Removed another bad print statement

--- vistrails/gui/vistrail_controller.py
+++ vistrails/gui/vistrail_controller.py
@@ -1534,8 +1534,9 @@ class VistrailController(QtCore.QObject):
                           
         del_action_list.append(('delete', group))
         action = core.db.action.create_action(add_action_list + del_action_list)
-        for op in action.db_operations:
-            print op.vtType, op.what, op.old_obj_id, op.new_obj_id
+        # Commenting out this as old_obj_id does not exist anymore
+        #for op in action.db_operations:
+        #    print op.vtType, op.what, op.old_obj_id, op.new_obj_id
         self.add_new_action(action)
         self.perform_action(action)
 
