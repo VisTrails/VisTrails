8cb83b5642b50af5be6820fa42fae4154bfbd2b7
Emanuele Santos <emanuele@sci.utah.edu>
2009-06-17 22:08:00 +0000

    <ticket>#263</ticket> <bugfix>Reverted list2cmdline to the original code.</bugfix> <bugfix>Increased font size of the notes in protein_visualization.vt example.</bugfix> -- Closes ticket #263 in v1.2 branch and trunk. -- * branches/v1.2/vistrails/core/system/unix.py * trunk/vistrails/core/system/unix.py   list2cmdline()     changed code to not use escaping. subprocess.list2cmdline() should take care of this already.
    * trunk/examples/protein_visualization.vt
       Increased the font size of the notes so they are legible on Macs.

--- vistrails/core/system/unix.py
+++ vistrails/core/system/unix.py
@@ -65,15 +65,9 @@ def executable_is_in_pythonpath(filename):
     return ""
 
 def list2cmdline(lst):
-    ignore_list = ['>']
-    res = []
     for el in lst:
         assert type(el) == str
-        if el not in ignore_list:
-            res.append(re.escape(el))
-        else:
-            res.append(el)
-    return subprocess.list2cmdline(res)
+    return subprocess.list2cmdline(lst)
 
 def execute_cmdline(lst, output):
     """execute_cmdline(lst: list of str)-> int
