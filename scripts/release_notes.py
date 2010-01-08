36bd072eba7e47796f1a280d7c721413582298c1
Emanuele Santos <emanuele@sci.utah.edu>
2010-01-08 04:29:03 +0000

    Improvements to the script that generates the release notes:   - Added support for more that one element in a message   - Added support for <feature>...</feature> items   - Added support to get ticket information from the Trac system using xmlrpc calls

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import xmlrpclib
 import pysvn
 import getpass
 import sys
@@ -26,11 +26,11 @@ def rev(n):
 client = pysvn.Client()
 client.callback_get_login = userpass
 
-version_start = 1530
-version_end = 1537
-
+version_start = 1562
+version_end = 1675
+release_name = "1.4"
 logs = client.log('https://vistrails.sci.utah.edu/svn',
-                  revision_end=rev(1520))
+                  revision_end=rev(version_start))
 
 ##############################################################################
 
@@ -56,8 +56,105 @@ def print_changes(release):
                 print "    %s - %s" % (str(summary.summarize_kind)[0].upper(),
                                        summary.path)
 
-re_ticket = re.compile(r'<ticket>(.*)</ticket>')
-re_bugfix = re.compile(r'<bugfix>(.*)</bugfix>')
+re_ticket = re.compile(r'<ticket>(.*?)</ticket>', re.M | re.S)
+re_bugfix = re.compile(r'<bugfix>(.*?)</bugfix>', re.M | re.S)
+re_feature = re.compile(r'<feature>(.*?)</feature>', re.M | re.S)
+
+def build_release_notes(release):
+    global username
+    global password
+
+    #populate dictionaries
+    bugfixes = {}
+    tickets = {}
+    features = {}
+    changes = {}
+    ticket_info = {}
+    if release not in release_changes:
+        return
+    for (v, log, diff_summaries) in release_changes[release]:
+        lf = re_feature.findall(log.message)
+        lt = re_ticket.findall(log.message)
+        lb = re_bugfix.findall(log.message)
+        if len(lf) > 0:
+            features[v] = []
+            for f in lf:
+                features[v].append(f)
+        if len(lt) > 0:
+            tickets[v] = []
+            for t in lt:
+                tickets[v].append(t)
+        if len(lb) > 0:
+            bugfixes[v] = []
+            for b in lb:
+                bugfixes[v].append(b)
+        if  len(lf) == 0 and len(lt) == 0 and len(lb) == 0:
+            changes[v] = log.message
+
+    #get ticket summaries from xmlrpc plugin installed on vistrails trac
+    print "Will connect to VisTrails Trac with authentication..."
+    if not username:
+        print "Username:",
+        sys.stdout.flush()
+        username = raw_input()
+        password = getpass.getpass()
+
+    url = "https://%s:%s@vistrails.sci.utah.edu/login/xmlrpc"%(username,
+                                                               password)
+    server = xmlrpclib.ServerProxy(url)
+    for (r,tl) in tickets.iteritems():
+        for t in tl:
+            if not ticket_info.has_key(t):
+                try:
+                    tid = int(t[1:])
+                    ticket_info[t] = server.ticket.get(tid)
+                except Exception, e:
+                    tickets[r].remove(t)
+                    print "revision %s: Could not get info for ticket %s"%(r,t)
+    print
+    print
+    print "Release Name: v%s build %s"%(release_name,version_end)
+    print 
+    print "Enhancements: "
+    revisions = sorted(features.keys())
+    revisions.reverse()
+    for r in revisions:
+        if tickets.has_key(r):
+            for t in tickets[r]:
+                if ticket_info[t][3]['type'] == 'enhancement':
+                    print " - Ticket %s: %s (r%s)"%(t,ticket_info[t][3]['summary'],r)
+        rfeats = features[r]
+        for f in rfeats:
+            print " - %s (r%s)" %(f,r)
+    print
+    print "Bug fixes: "
+    revisions = sorted(bugfixes.keys())
+    revisions.reverse()
+    for r in revisions:
+        if tickets.has_key(r):
+            for t in tickets[r]:
+                if ticket_info[t][3]['type'] == 'defect':
+                    print " - Ticket %s: %s (r%s)"%(t,ticket_info[t][3]['summary'],r)
+        rbugs = bugfixes[r]
+        for b in rbugs:
+            print " - %s (r%s)" %(b,r)
+    print
+    print "Other changes: "
+    revisions = sorted(changes.keys())
+    revisions.reverse()
+    for r in revisions:
+        print "(r%s): "%r
+        print "  - %s... "%changes[r][0:100]
+
+def get_features(release):
+    if release not in release_changes:
+        return
+    print "New features in '%s': " % release
+    for (v, log, diff_summaries) in release_changes[release]:
+        s = re_feature.search(log.message)
+        if s:
+            print " - %s (r%s)" % (s.groups()[0],v)
+            
 def get_ticket_closes(release):
     if release not in release_changes:
         return
@@ -66,7 +163,7 @@ def get_ticket_closes(release):
         s = re_ticket.search(log.message)
         if s:
             print "revision %s closed ticket %s" % (v, s.groups()[0])
-
+            
 def get_bugfixes(release):
     if release not in release_changes:
         return
@@ -76,12 +173,6 @@ def get_bugfixes(release):
         if s:
             print " - %s (r%s)" % (s.groups()[0], v)
     
-#             print "  Affected files:"
-#             for summary in diff_summaries:
-#                 if release_changed(summary.path) == release:
-#                     print "    %s - %s" % (str(summary.summarize_kind)[0].upper(),
-#                                            summary.path)
-
 ################################################################################
 # collect all changes
 
@@ -108,8 +199,10 @@ for (v, log, diff_summaries) in change_sets:
     for which in which_releases:
         release_changes.setdefault(which, []).append((v, log, diff_summaries))
 
-# print_changes('trunk')
-get_ticket_closes('trunk')
-get_bugfixes('trunk')
+build_release_notes('trunk')
+#print_change_summaries('trunk')
+#get_features('trunk')
+#get_ticket_closes('trunk')
+#get_bugfixes('trunk')
 
 
