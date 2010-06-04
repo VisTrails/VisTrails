0e1439265f7c41544738baf3c53bd3023ed8c39f
David Koop <dakoop@cs.utah.edu>
2010-06-04 19:55:29 +0000

    Fix script that updates binaries to be python2.6-aware.  Now, the script looks for python2.6, and if that exists, replaces the directories there.  If not, it looks for python2.5, and if that exists, replaces those directories.
    scripts/mac_update_bin.sh:
      - be python2.6-aware for new binary versions

--- scripts/mac_update_bin.sh
+++ scripts/mac_update_bin.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 SRC_PATH="vistrails"
-BIN_PATH="Contents/Resources/lib/python2.5"
+BIN_PATH_25="Contents/Resources/lib/python2.5"
+BIN_PATH_26="Contents/Resources/lib/python2.6"
 DIRS="api core db gui packages tests"
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
     echo "usage: $0 <src_dir> <bin_dir>"
     exit 65
+fi
+
+if [ -e "$2/$BIN_PATH_26/$dir" ]
+then
+    BIN_PATH=$BIN_PATH_26
+elif [ -e "$2/$BIN_PATH_25/$dir" ]
+then
+    BIN_PATH=$BIN_PATH_25
 fi
 
 for dir in $DIRS
