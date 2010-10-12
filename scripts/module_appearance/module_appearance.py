c541231f8c139395d50d240869ac97efc865e3dc
Carlos Scheidegger <cscheid@sci.utah.edu>
2010-10-11 20:32:02 -0400

    -- Fixed the pythonCalc example to show some of the other options on add_module, to make the module fringe options more obvious for users. Rewrote part of the documentation on ModuleRegistry.add_module to reflect which options are typically settable by users in their packages and which are only for internal VisTrails usage.
    Added a new utility (currently at scripts/module_appearance) to draw
    fringes visually, which generates output that can be pasted directly
    into the add_module call.
    --
    
    scripts/module_appearance: new directory
    
    vistrails/core/modules/module_registry.py:
    
      ModuleRegistry.add_module: reworked documentation
    
    vistrails/packages/pythonCalc/init.py:
    
      initialize: added moduleFringe examples, pointed to documentation at
      the module registry and to the module_appearance script.

---
+++ scripts/module_appearance/module_appearance.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'module_appearance.ui'
+#
+# Created: Mon Oct 11 20:15:11 2010
+#      by: PyQt4 UI code generator 4.7.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(819, 602)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 811, 551))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.graphicsView = QtGui.QGraphicsView(self.verticalLayoutWidget)
+        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout.addWidget(self.graphicsView)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.textEdit = QtGui.QTextEdit(self.verticalLayoutWidget)
+        self.textEdit.setObjectName("textEdit")
+        self.horizontalLayout_2.addWidget(self.textEdit)
+        self.textEdit_help = QtGui.QTextEdit(self.verticalLayoutWidget)
+        self.textEdit_help.setObjectName("textEdit_help")
+        self.horizontalLayout_2.addWidget(self.textEdit_help)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_symmetric = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_symmetric.setObjectName("pushButton_symmetric")
+        self.horizontalLayout.addWidget(self.pushButton_symmetric)
+        self.pushButton_mirrored = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_mirrored.setObjectName("pushButton_mirrored")
+        self.horizontalLayout.addWidget(self.pushButton_mirrored)
+        self.pushButton_Clear = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_Clear.setObjectName("pushButton_Clear")
+        self.horizontalLayout.addWidget(self.pushButton_Clear)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pushButton_Quit = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_Quit.setObjectName("pushButton_Quit")
+        self.horizontalLayout.addWidget(self.pushButton_Quit)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.textEdit_help.setHtml(QtGui.QApplication.translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">To edit the shape, double-click the fringe lines and drag the vertices. To delete the vertices, select them and press &quot;Delete&quot; on your keyboard. </p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">The resulting code snippet can be used to generate a module fringe in VisTrails like the one being displayed.</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">The buttons &quot;Make rotationally symmetric&quot; and &quot;Make mirrored&quot; use the left side of the module to produce the right side. They <span style=\" font-weight:600;\">will erase</span> the decorations on the right side.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_symmetric.setText(QtGui.QApplication.translate("MainWindow", "Make rotationally symmetric", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_mirrored.setText(QtGui.QApplication.translate("MainWindow", "Make mirrored", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_Clear.setText(QtGui.QApplication.translate("MainWindow", "Clear", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_Quit.setText(QtGui.QApplication.translate("MainWindow", "Quit", None, QtGui.QApplication.UnicodeUTF8))
+
