3123297d6a1b374a7b720af850b6ee46e7955b16
Emanuele Santos <emanuele@sci.utah.edu>
2010-11-02 18:44:55 -0300

    <ticket>#382</ticket> <feature>LaTex extension: VisTrails can now embed a workflow graph or a history tree in LaTex pdf</feature> <feature>LaTex extension: Caching latex instructions and images for embedding images in case python is not available</feature> <bugfix>LaTex extension: removed hardcoded url for downloading vt files upon clicking on the images in the generated pdf. It can now be set using the \vistrailsdownload in the latex file</bugfix>
    --
    See extensions/latex/README for documentation.
    
    extensions/latex/README
      - Updated instructions and documentation for new features.
      - Fixed minor typos
    
    extensions/latex/example.tex
    extensions/latex/example.pdf
      - Updated example file to show how to embed a workflow graph,
        history tree as pdf files.
    
    extensions/latex/includevistrail.py
      - Added new parameters to input file:
        + download=http://url.from.where.to.download.vt.files
        + workflow=False
        + tree=False
      - Logging requests to vistrails.log file
      - Added functions to download a workflow graph and history tree,
        both as png or pdf
      - Removed hardcoded url for downloading the vt files.
      - Writing generated latex instructions to a folder to be used directly
        by LaTex later
    
    extensions/latex/vistrails.sty
      - Added newcommand \vistrailsdownload for storing the url used to download
        vtl files
      - Checking if provided \vistrailspythonpath is valid before using it.
        If it is invalid it will include cached files directly
      - Added new options to \vistrails command:
        + showworkflow
        + showtree
    
    extensions/http/run_vistrails.php
      - Added support for getting workflow graphs and history tree graphs from
        VisTrails server.

--- extensions/http/run_vistrails.php
+++ extensions/http/run_vistrails.php
@@ -47,6 +47,8 @@ $PATH_TO_IMAGES = '/images/vistrails/';
 // Change this to the web accessible path to the folder where the images were generated
 $WEB_PATH_TO_IMAGES = '/images/';
 
+$URL_TO_GRAPHS = '/images/vistrails/graphs/'
+
 $SUPPORTED_IMAGE_FILES = array("png", "jpg", "gif");
 // set variables with default values
 $host = 'vistrails.sci.utah.edu';
@@ -58,6 +60,8 @@ $username = "vtserver";
 $version_tag = '';
 $force_build = 'False';
 $pdf= 'False';
+$showtree = 'False';
+$showworkflow = 'False';
 
 //Get the variables from the url
 if(array_key_exists('host', $_GET))
@@ -77,7 +81,7 @@ if(array_key_exists('tag',$_GET)){
 																 $dbname, $vtid,
 																 $version_tag));
 		$response = do_call($VT_HOST,$VT_PORT,$request);
-		$version = get_result_from_response($response);
+		$version = get_version_from_response($response);
 	}
 }
 if(array_key_exists('buildalways',$_GET))
@@ -85,56 +89,90 @@ if(array_key_exists('buildalways',$_GET))
 	
 if(array_key_exists('pdf',$_GET))
     $pdf = $_GET['pdf'];
+    
+if(array_key_exists('showworkflow',$_GET))
+    $showworkflow = $_GET['showworkflow'];
+    
+if(array_key_exists('showtree',$_GET))
+    $showtree = $_GET['showtree'];
+    
 //echo $force_build;
 
 //Check if vtid and version were provided
-if($vtid != '' and $version != ''){
-    $pdf_option = '';
+if(($vtid != '' and $version != '') or 
+  ($vtid != '' and strcasecmp($showtree,'True') == 0)){
     $pdf_bool = False;
     $pdf_tag = '';
     if (strcasecmp($pdf,'True') == 0){
-        $pdf_option = '-p ';
         $pdf_bool = True;
         $pdf_tag = '_pdf';
     }
-	$destdir = $PATH_TO_IMAGES;
-	$destversion = $host . '_'. $dbname .'_' . $port . '_' .$vtid . '_' .
-	               $version. $pdf_tag;
-	$destversion = md5($destversion);
-	$destdir = $destdir . $destversion;
-	$result = '';
-	if((!file_exists($destdir)) or strcasecmp($force_build,'True') == 0) {
-		if (!file_exists($destdir)){
-			mkdir($destdir);//,0770);
-			chmod($destdir,0770);
-		}
-		if(!$USE_VISTRAILS_XML_RPC_SERVER){
-			chdir($PATH_TO_VISTRAILS);
-			//$curdir =  exec("pwd")."\n";
-			//echo exec("echo $DISPLAY");
-			$setVariables = 'export PATH=$PATH:/usr/bin/X11;export HOME=/var/lib/wwwrun; export TEMP=/tmp; export DISPLAY=localhost:1.0; export LD_LIBRARY_PATH=/usr/local/lib;';
-			$mainCommand = 'python vistrails.py -b -e '. $pdf_option . $destdir.' -t ' .
-							$host .' -r ' . $port . ' -f ' . $dbname . ' -u ' .
-							$username . ' "' . $vtid . ':' . $version .'"';
-			//echo $mainCommand."\n";
-			$result = exec($setVariables.$mainCommand . ' 2>&1', $output, $result);
-		}
-		else {
-			$request = xmlrpc_encode_request('run_from_db',
+	
+	if (strcasecmp($showtree,'True') == 0){
+        if ($pdf_bool == True)
+		    $func = "get_vt_graph_pdf";
+		else 
+		    $func = "get_vt_graph_png";
+		$request = xmlrpc_encode_request($func, array($host, 
+				                                      $port, 
+				                                      $dbname, 
+				                                      $vtid));
+        $response = do_call($VT_HOST,$VT_PORT,$request);
+        $result = clean_up($response);
+        if ($pdf_bool == True)
+            $res = '<a href="' . $URL_TO_GRAPHS . $result . 
+                            '">' . $result . '</a>';
+        else
+            $res = '<img src="'. $URL_TO_GRAPHS . $result.
+                       "\" alt=\"vt_id:$vtid\" />";
+        echo $res;
+	}
+	elseif (strcasecmp($showworkflow,'True') == 0){
+		if ($pdf_bool == True)
+             $func = "get_wf_graph_pdf";
+        else 
+             $func = "get_wf_graph_png";
+		$request = xmlrpc_encode_request($func, array($host, 
+				                                      $port, 
+				                                      $dbname, 
+				                                      $vtid,
+                                                      $version));
+        $response = do_call($VT_HOST,$VT_PORT,$request);
+        $result = clean_up($response);
+        if ($pdf_bool == True)
+            $res = '<a href="' . $URL_TO_GRAPHS . $result . 
+                            '">' . $result . '</a>';
+        else
+            $res = '<img src="'. $URL_TO_GRAPHS . $result.
+                       "\" alt=\"vt_id:$vtid version:$version\" />";
+        echo $res;
+	}
+	else{
+	   $destdir = $PATH_TO_IMAGES;
+       $destversion = $host . '_'. $dbname .'_' . $port . '_' .$vtid . '_' .
+                     $version. $pdf_tag;
+       $destversion = md5($destversion);
+       $destdir = $destdir . $destversion;
+       $result = '';
+       if((!file_exists($destdir)) or strcasecmp($force_build,'True') == 0) {
+            if (!file_exists($destdir)){
+                mkdir($destdir);//,0770);
+                chmod($destdir,0770);
+            }
+			    $request = xmlrpc_encode_request('run_from_db',
 											 array($host, $port, $dbname, $vtid,
 													$destdir, $version, $pdf_bool));
-			$response = do_call($VT_HOST,$VT_PORT,$request);
-			$result = get_result_from_response($response);
-			//echo $result;
+			    $response = do_call($VT_HOST,$VT_PORT,$request);
+			    $result = clean_up($response);
 		}
-	}
-	$files = scandir($destdir);
-	$n = sizeof($files);
-	if ($n > 2){
-		$res = '';
-		foreach($files as $filename) {
-			if($filename != '.' and $filename != '..'){
-				$info = pathinfo($filename);
+		//echo $result;
+	   $files = scandir($destdir);
+	   $n = sizeof($files);
+	   if ($n > 2){
+		  $res = '';
+		  foreach($files as $filename) {
+			 if($filename != '.' and $filename != '..'){
+			 	$info = pathinfo($filename);
 				if ( in_arrayi($info['extension'],$SUPPORTED_IMAGE_FILES)){ 
 				    list($width, $height, $type, $attr) = getimagesize($destdir.'/'.$filename);
 				    if ($width > 600)
@@ -150,38 +188,34 @@ if($vtid != '' and $version != ''){
 			}
 		}	
 		echo $res;
-	}
-	else{
-		echo "ERROR: Vistrails didn't produce any image.\n";
-		echo "This is the output: \n".$result;
-	}
+	   }
+	   else{
+		  echo "ERROR: Vistrails didn't produce any image.\n";
+		  echo "This is the output: \n".$result;
+	   }
+    }
 }
 else{
 	echo "ERROR: Vistrails id or version not provided.\n";
 }
 
-function get_result_from_response($response) {
-	function contents($parser, $data){
-		global $resp_result;
-	    $result = $data;
-	}
+function get_version_from_response($xmlstring){
+    try{
+        $node = @new SimpleXMLElement($xmlstring);
+        return $node->params[0]->param[0]->value[0]->int[0];
+    } catch(Exception $e) {
+        echo "bad xml";
+    }
+}
 
-	function  start_tag($parser, $data){
-         //do nothing
-	} 
-
-	function end_tag($parser, $data){
-		//do nothing
-	}
-	$resp_result = '';
-	$xml_parser = xml_parser_create();
-	xml_set_element_handler($xml_parser, "start_tag", "end_tag");
-	xml_set_character_data_handler($xml_parser, "contents");
-	if(!(xml_parse($xml_parser, $response, True))){
-		die("Error on line " . xml_get_current_line_number($xml_parser));	
-	}
-	xml_parser_free($xml_parser);
-	return $resp_result;
+function clean_up($xmlstring){
+    try{
+        $node = @new SimpleXMLElement($xmlstring);
+   
+        return $node->params[0]->param[0]->value[0]->string[0];
+    } catch(Exception $e) {
+        echo "bad xml";
+    }
 }
 
 ?>
