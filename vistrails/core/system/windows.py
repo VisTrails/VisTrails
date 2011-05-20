71b4bfc41a6d5aaa03867f64c81780637219ed53
Huy T. Vo <hvo@sci.utah.edu>
2011-05-19 23:28:42 -0600

    <bugfix>Fixed crash when running VisTrails 64-bit on Windows with more than 4GB of RAM</bugfix>
    Used Win32 GlobalMemoryStatusEx instead of GlobalMemoryStatus to
    support VisTrails 64-bit on Windows.

--- vistrails/core/system/windows.py
+++ vistrails/core/system/windows.py
@@ -27,10 +27,10 @@ import subprocess
 import core.system
 
 try:
-    from ctypes import windll, Structure, c_ulong, byref, sizeof
+    from ctypes import windll, Structure, c_ulong, c_ulonglong, byref, sizeof
     importSuccess = True
     
-    class WIN32MEMORYSTATUS(Structure):
+    class WIN32MEMORYSTATUSEX(Structure):
         """ Structure that represents memory information returned by 
         Windows API
         
@@ -38,12 +38,13 @@ try:
         _fields_ = [
             ('dwLength', c_ulong),
             ('dwMemoryLoad', c_ulong),
-            ('dwTotalPhys', c_ulong),
-            ('dwAvailPhys', c_ulong),
-            ('dwTotalPageFile', c_ulong),
-            ('dwAvailPageFile', c_ulong),
-            ('dwTotalVirtual', c_ulong),
-            ('dwAvailVirtual', c_ulong)
+            ('dwTotalPhys', c_ulonglong),
+            ('dwAvailPhys', c_ulonglong),
+            ('dwTotalPageFile', c_ulonglong),
+            ('dwAvailPageFile', c_ulonglong),
+            ('dwTotalVirtual', c_ulonglong),
+            ('dwAvailVirtual', c_ulonglong),
+            ('dwAvailExtendedVirtual', c_ulonglong),
             ]
 
 except ImportError:
@@ -52,7 +53,7 @@ except ImportError:
 ##############################################################################
 def parse_meminfo():
     """ 
-    parse_meminfo() -> int
+    parse_meminfo() -> long
     Calls Windows 32 API GlobalMemoryStatus(Ex) to get memory information 
     It requires ctypes module
     
@@ -60,12 +61,12 @@ def parse_meminfo():
     try:
         kernel32 = windll.kernel32
 
-        result = WIN32MEMORYSTATUS()
-        result.dwLength = sizeof(WIN32MEMORYSTATUS)
-        kernel32.GlobalMemoryStatus(byref(result))
+        result = WIN32MEMORYSTATUSEX()
+        result.dwLength = sizeof(WIN32MEMORYSTATUSEX)
+        kernel32.GlobalMemoryStatusEx(byref(result))
     except:
         return -1
-    return int(result.dwTotalPhys / 1024) * 1L
+    return long(result.dwTotalPhys / 1024)
 
 def guess_total_memory():
     """ guess_total_memory() -> int 
