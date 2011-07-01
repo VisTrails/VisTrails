b6a9ac39501dda52e945523fa8b3215aaca275da
Emanuele Santos <emanuele@sci.utah.edu>
2011-07-01 10:37:15 -0300

    Changed headers of files in master branch to BSD license.

---
+++ scripts/update_to_mod_bsd_license.py
@@ -0,0 +1,291 @@
+###############################################################################
+##
+## Copyright (C) 2006-2011, University of Utah. 
+## All rights reserved.
+## Contact: vistrails@sci.utah.edu
+##
+## This file is part of VisTrails.
+##
+## "Redistribution and use in source and binary forms, with or without 
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice, 
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright 
+##    notice, this list of conditions and the following disclaimer in the 
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of the University of Utah nor the names of its 
+##    contributors may be used to endorse or promote products derived from 
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+
+# Finds all .py files recursively in current directory (.)
+# and updates to modified BSD license in the file header. 
+import re
+import os
+py_bsd_3_license = """###############################################################################
+##
+## Copyright (C) 2006-2011, University of Utah. 
+## All rights reserved.
+## Contact: vistrails@sci.utah.edu
+##
+## This file is part of VisTrails.
+##
+## "Redistribution and use in source and binary forms, with or without 
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice, 
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright 
+##    notice, this list of conditions and the following disclaimer in the 
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of the University of Utah nor the names of its 
+##    contributors may be used to endorse or promote products derived from 
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+"""
+xml_bsd_3_license = "<!--"+ py_bsd_3_license + "-->"
+php_bsd_3_license = """////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2006-2011, University of Utah. 
+// All rights reserved.
+// Contact: vistrails@sci.utah.edu
+//
+// This file is part of VisTrails.
+//
+// "Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the distribution.
+//  - Neither the name of the University of Utah nor the names of its 
+//    contributors may be used to endorse or promote products derived from 
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+//
+////////////////////////////////////////////////////////////////////////////"""
+sql_bsd_3_license = """--#############################################################################
+--
+-- Copyright (C) 2006-2011, University of Utah. 
+-- All rights reserved.
+-- Contact: vistrails@sci.utah.edu
+--
+-- This file is part of VisTrails.
+--
+-- "Redistribution and use in source and binary forms, with or without 
+-- modification, are permitted provided that the following conditions are met:
+--
+--  - Redistributions of source code must retain the above copyright notice, 
+--    this list of conditions and the following disclaimer.
+--  - Redistributions in binary form must reproduce the above copyright 
+--    notice, this list of conditions and the following disclaimer in the 
+--    documentation and/or other materials provided with the distribution.
+--  - Neither the name of the University of Utah nor the names of its 
+--    contributors may be used to endorse or promote products derived from 
+--    this software without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+-- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+-- PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+-- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+-- EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+-- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+-- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+-- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+-- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+-- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+--
+--#############################################################################"""
+py_old_license = """############################################################################
+##
+## Copyright (C) 2006-2010 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
+"""
+sql_old_license = """--#########################################################################
+--
+-- Copyright (C) 2006-2010 University of Utah. All rights reserved.
+--
+-- This file is part of VisTrails.
+--
+-- This file may be used under the terms of the GNU General Public
+-- License version 2.0 as published by the Free Software Foundation
+-- and appearing in the file LICENSE.GPL included in the packaging of
+-- this file.  Please review the following to ensure GNU General Public
+-- Licensing requirements will be met:
+-- http://www.opensource.org/licenses/gpl-license.php
+--
+-- If you are unsure which license is appropriate for your use (for
+-- instance, you are interested in developing a commercial derivative
+-- of VisTrails), please contact us at vistrails@sci.utah.edu.
+--
+-- This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+-- WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+--
+--##########################################################################"""
+xml_old_license = """<!--########################################################################
+##
+## Copyright (C) 2006-2010 University of Utah. All rights reserved.
+##
+## This file is part of VisTrails.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+#########################################################################-->"""
+
+php_old_license = """////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2006-2010 University of Utah. All rights reserved.
+//
+// This file is part of VisTrails.
+//
+// This file may be used under the terms of the GNU General Public
+// License version 2.0 as published by the Free Software Foundation
+// and appearing in the file LICENSE.GPL included in the packaging of
+// this file.  Please review the following to ensure GNU General Public
+// Licensing requirements will be met:
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// If you are unsure which license is appropriate for your use (for
+// instance, you are interested in developing a commercial derivative
+// of VisTrails), please contact us at vistrails@sci.utah.edu.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+////////////////////////////////////////////////////////////////////////////"""
+def process_files(files, old_license, new_license, type):
+    count = 0
+    not_found = []
+    gpl_found = []
+    for fname in files:
+        fin = open(fname)
+        all_lines = fin.read()
+        fin.close()
+        pos = all_lines.find(old_license)
+        pos_gen = all_lines.find("GNU General Public")
+        if pos > -1:
+            print "Updating: %s"%fname
+            newlines = all_lines.replace(old_license, new_license)
+            fout = file(fname, 'w')
+            fout.write(newlines)
+            fout.close()
+            count += 1
+        else:
+            if pos_gen > -1:
+                gpl_found.append(fname)
+            else:
+                if all_lines.find(new_license) == -1:
+                    print "Appending license to %s"%fname
+                    newlines = new_license+"\n"+all_lines
+                    fout = file(fname, 'w')
+                    fout.write(newlines)
+                    fout.close()
+                    count +=1
+    print count, " %s files updated"%type
+    print " found reference to gpl in the following files:"
+    for fname in gpl_found:
+        print "  ", fname
+
+py_files = []
+xml_files = []
+php_files = []
+sql_files = []
+others = []
+for (path, dnames, fnames) in os.walk('.'):
+    for fn in fnames:
+        if fn.endswith(".py"):
+            py_files.append(os.path.join(path, fn))
+        elif fn.endswith(".xml") or fn.endswith(".xsd"):
+            xml_files.append(os.path.join(path,fn))
+        elif fn.endswith(".php"):
+            php_files.append(os.path.join(path,fn))
+        elif fn.endswith(".sql"):
+            sql_files.append(os.path.join(path,fn))
+        else:
+            others.append(os.path.join(path,fn))
+
+print len(py_files), " python files found"
+process_files(py_files, py_old_license, py_bsd_3_license, 'python')
+
+print len(sql_files), " sql files found"
+process_files(sql_files, sql_old_license, sql_bsd_3_license, 'sql')
+
+print len(xml_files), " xml files found"
+process_files(xml_files, xml_old_license, xml_bsd_3_license, 'xml')
+
+print len(php_files), " php files found"
+process_files(php_files, php_old_license, php_bsd_3_license, 'php')
+
+#print "Ignored files: "
+#for f in others:
+#    print f
