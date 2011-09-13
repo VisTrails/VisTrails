VisTrails/VisTrails

commit 0262b0410051f2ebdac33b990b7b85a4fd1239d5

<bugfix>Fixed issues with executing queries</bugfix> <ticket>#486</ticket>
Fixed an issue where the execute button was not correctly displayed in
the query view.  There is some fun with determining the current "view"
in the query view because it can show the search canvas, or one of the
result views.

gui/query_view.py:
  - QueryController.reset_search: call vistrailChanged to reset the
    execute button

gui/vistrail_view.py:
  - QVistrailView.view_changed: use the currentWidget here, not the
    current tab, as for the query view, this is the inner view

vistrails/gui/vistrail_view.py
@@ -558,8 +558,8 @@ class QVistrailView(QtGui.QWidget):
     def view_changed(self):
         from gui.vistrails_window import _app
         _app.closeNotPinPalettes()
-        #view = self.stack.currentWidget()
-        view = self.get_current_tab()
+        view = self.stack.currentWidget()
+        # view = self.get_current_tab()
         print "changing tab from: ",self.current_tab, " to ", view
         print self.tab_to_stack_idx
         if view != self.current_tab:
@@ -580,9 +580,8 @@ class QVistrailView(QtGui.QWidget):
         else:
             print "tabs the same. do nothing"
         self.showCurrentViewPalettes()
-        real_view = self.stack.currentWidget()
-        if isinstance(real_view, QQueryView):
-            _app.notify("controller_changed", real_view.p_controller)
+        if isinstance(view, QQueryView):
+            _app.notify("controller_changed", view.p_controller)
             _app.notify("entry_klass_changed", QueryEntry)
         else:
             _app.notify("entry_klass_changed", ParameterEntry)

vistrails/gui/query_view.py
@@ -139,6 +139,7 @@ class QueryController(object):
             self.query_view.pipeline_view.controller.current_pipeline)
         self.query_view.set_display_view(self.query_view.VISUAL_SEARCH_VIEW)
         self.query_view.query_box.searchBox.clearSearch()
+        self.query_view.vistrailChanged()
 
         from gui.vistrails_window import _app
         _app.notify("search_changed", None)

commit 970ec4ecfb42f6c8cfafe7909ef9e9a8fc5bec78

<ticket>#456</ticket> <bugfix>Ignoring action updates when the view does not have the actions</bugfix>  - Updated git repository url for source release script.
--

There were two problems that caused the copy and
paste not work for detached views.

The first was that changing the focus among detached views of
the same vistrail was not triggering view changes. The second
was that the changes in commit 0262b04 caused the vistrail view
to ignore detached views when generating view changes.

To fix the second problem without breaking QQueryView, I've
created another method get_current_outer_tab() that will return
the right widget and that will be called only by
QVistrailView.view_changed()

--
gui/vistrails_window.py
 In class QVistrailsWindow:
  set_action_links()
     if the action is not present in the current window, ignore it
  set_action_defaults()
     if the action is not present in the current window, ignore it
  applicationFocusChanged()
     make sure to generate view changes even if the vistrail view
     hasn't changed so tab changes are processed correctly

gui/vistrail_view.py
  In class QVistrailView:
     detachedViewWasClosed()
       making sure to reset pipeline before calling self.view_changed()
     added new method get_current_outer_tab()

dist/source/make-vistrails-src-release.py
@@ -33,7 +33,7 @@ from StringIO import StringIO
 ####################
 
 # URL of the git repository to export
-GIT_URL = "git://vistrails.sci.utah.edu/vistrails.git"
+GIT_URL = "git://www.vistrails.org/vistrails.git"
 
 # Global arguments for calls to the git command
 GIT_ARGS = ""

vistrails/gui/vistrail_view.py
@@ -464,8 +464,8 @@ class QVistrailView(QtGui.QWidget):
         if self.controller.current_pipeline_view.parent() == view:
             self.controller.current_pipeline_view = None
             self.activateWindow()
-            self.view_changed()
             self.reset_tab_view_to_current()
+            self.view_changed()
         del self.detached_views[view]
         
     def updateTabsTooTip(self):
@@ -554,6 +554,15 @@ class QVistrailView(QtGui.QWidget):
                 widget = widget.get_current_view()
             return widget
         
+    def get_current_outer_tab(self):
+        window = QtGui.QApplication.activeWindow()
+        if window in self.detached_views.values():
+            return window.view   
+        else:
+            #if none of the detached views is active we will assume that the
+            #window containing this vistrail has focus
+            return self.stack.currentWidget()
+        
     def get_tab(self, stack_idx):
         widget = self.stack.widget(stack_idx)
         if type(widget) == QQueryView:
@@ -563,8 +572,8 @@ class QVistrailView(QtGui.QWidget):
     def view_changed(self):
         from gui.vistrails_window import _app
         _app.closeNotPinPalettes()
-        view = self.stack.currentWidget()
-        # view = self.get_current_tab()
+        #view = self.stack.currentWidget()
+        view = self.get_current_outer_tab()
         #print "changing tab from: ",self.current_tab, " to ", view
         #print self.tab_to_stack_idx
         if view != self.current_tab:

vistrails/gui/vistrails_window.py
@@ -1197,11 +1197,12 @@ class QVistrailsWindow(QVistrailViewWindow):
             window = obj.window()
             if isinstance(window, BaseView):
                 window = window.vistrail_view.window()
-            qaction = window.qactions[action]
-            method = get_method(qaction, check)
-            notification = (notification_id, method, True, view)
-            self.register_notification(*notification)
-            link_list.append(notification)
+            if action in window.qactions:
+                qaction = window.qactions[action]
+                method = get_method(qaction, check)
+                notification = (notification_id, method, True, view)
+                self.register_notification(*notification)
+                link_list.append(notification)
         self.action_links[id(obj)] = link_list
     
     def unset_action_links(self, obj):
@@ -1215,12 +1216,13 @@ class QVistrailsWindow(QVistrailViewWindow):
             window = window.vistrail_view.window()
         qactions = window.qactions
         for action, mlist in obj.action_defaults.iteritems():
-            qaction = qactions[action]
-            for (method, is_callback, value) in mlist:
-                if is_callback:
-                    getattr(qaction, method)(value())
-                else:
-                    getattr(qaction, method)(value)
+            if action in qactions:
+                qaction = qactions[action]
+                for (method, is_callback, value) in mlist:
+                    if is_callback:
+                        getattr(qaction, method)(value())
+                    else:
+                        getattr(qaction, method)(value)
             
     def set_name(self):
         widget = self.stack.currentWidget()
@@ -2354,27 +2356,26 @@ class QVistrailsWindow(QVistrailViewWindow):
                 view = self.get_current_view()
 #                print "view: ", view
                 if view and (view == current or view.isAncestorOf(current)):
-                    if (owner != self._focus_owner and 
-                        view != self._previous_vt_view):
+                    if owner != self._focus_owner:
                         self._previous_vt_view = view
-                        self._previous_view = view.get_current_tab()
                         self._focus_owner = owner
                         self.change_view(view)
-                        view.view_changed()
-                        view.reset_tab_view_to_current()
                         self.update_window_menu()
+                        self._previous_view = view.get_current_tab()
+                        view.reset_tab_view_to_current()
+                        view.view_changed()    
+                        
             elif isinstance(owner, QBaseViewWindow):
                 view = owner.get_current_view()
-#                print "view: ", view
-                if (view and owner != self._focus_owner and 
-                        view != self._previous_vt_view):
+                #print "view: ", view
+                if view and owner != self._focus_owner:
                     self._previous_vt_view = view
-                    self._previous_view = view.get_current_tab()
                     self._focus_owner = owner
                     self.change_view(view)
-                    view.view_changed()
-                    view.set_to_current(current)
                     self.update_window_menu()
+                    self._previous_view = view.get_current_tab()
+                    view.set_to_current(current)
+                    view.view_changed()
         else:
             self._focus_owner = None
 _app = None