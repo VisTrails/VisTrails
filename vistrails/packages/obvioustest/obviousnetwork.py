e126599739253ba92be447095d51288daee88ff4
P-L Hemery <hemery@lri.fr>
2011-10-26 14:46:30 +0200

    Add a draft module for obvious network.

---
+++ vistrails/packages/obvioustest/obviousnetwork.py
@@ -0,0 +1,35 @@
+import core.modules.module_registry
+from core.modules.vistrails_module import Module, ModuleError
+from obvious.data import Schema, Network, DataFactory
+from obvious.impl import SchemaImpl
+from obviousx.io.impl import GraphMLImport
+from javax.swing import JFrame
+from java.lang import System
+
+version = "0.0.1"
+name = "ObviousNetwork"
+identifier = "com.googlecode.obvious.data.obviousnetwork"
+
+class ObviousNetwork(Module):
+    
+    def compute(self):
+        nodeSchema = SchemaImpl()
+        edgeSchema = SchemaImpl()
+        file = self.getInputConnector("file")
+        dataFactoryName = self.getInputConnector("dataFactoryName")
+        System.setProperty("obvious.DataFactory", dataFactoryName)
+        factory = DataFactory.getInstance()
+        network = factory.createGraph(nodeSchema, edgeSchema)
+        graphmlimport = GraphMLImport(file, network, "source", "target", "nodeId")
+        filledNetwork = graphmlimport.loadGraph()
+        self.setResult("network", filledNetwork)
+    
+def initialize(*args, **keywords):
+    reg = core.modules.module_registry.registry
+    reg.addModule(ObviousNetwork)
+    reg.add_input_port(ObviousNetwork, "file",
+                       (core.modules.basic_modules.File, "a graphml file describing graph content"))
+    reg.add_input_port(ObviousNetwork, "dataFactoryName"
+                       (core.modules.basic_modules.String, "name of the datafactory"))
+    reg.add_output_port(ObviousNetwork, "network",
+                        (Network, "an Obvious network")
