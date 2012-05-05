4ef2b8639b6de9be179905fe95a33d903ec92315
David Koop <dakoop@cs.utah.edu>
2012-05-05 11:58:34 -0700

    <bugfix>Fix issue with caching group modules</bugfix> <ticket>#558</ticket> <feature>Allow modules in subpipelines to be cached</feature> <ticket>#559</ticket>
    Wrote a new signature function for the Group module which takes into account both the subpipeline and the input connections.  This allows groups to be properly cached (Ticket #558) and to allow interior pieces of groups to be cached as well (Ticket #559).  Basically, we write the upstream signature of the ports in a group to the signature of the interior InputPort module (we write it to _input_port_signature on that module).  Then, we recompute the signature of the subpipeline (a concatenation of the signatures of all of the sinks) using these new signatures.
    
    Note that there is still a difference between the group signature and the signature of the subpipeline the group replaced.  Thus, running a pipeline, grouping a region, and running the new pipeline will not use the old cache as desired.  This is mostly due to the fact that groups define InputPort and OutputPort modules that become part of the Group signature so the signatures inside the group become different than those outside.
    
    The is_cacheable method is still written so that any group with a non-cacheable module will be non-cacheable, but I believe we can remove this so that even groups with non-cacheable modules can be partially cacheable.  Also, we should see if these changes affect control flow modules.  This should also work for nested groups.
    
    core/modules/sub_module.py:
      - random_signature: moved out of initialize
      - input_port_signature: new method
      - group_signature: new method, computes the signature of the
        interior of the group with the external upstream connections and
        also allows caching in the subpipelines
      - initalize: change InputPort and Group signatures

--- vistrails/core/modules/sub_module.py
+++ vistrails/core/modules/sub_module.py
@@ -44,6 +44,8 @@ except ImportError:
     import sha
     sha_hash = sha.new
 
+from core.cache.hasher import Hasher
+from core.cache.utils import hash_list
 from core.modules import module_registry
 from core.modules.basic_modules import String, Boolean, Variant, NotCacheable
 from core.modules.vistrails_module import Module, InvalidOutput, new_module, \
@@ -369,18 +371,72 @@ def find_internal_abstraction_refs(pkg, vistrail, internal_version=-1L):
             abstractions.append((m.name, m.namespace))
     return abstractions
 
+def random_signature(pipeline, obj, chm):
+    hasher = sha_hash()
+    hasher.update(str(random.random()))
+    return hasher.digest()
+
+def input_port_signature(pipeline, obj, chm):
+    if hasattr(obj, '_input_port_signature') and \
+            obj._input_port_signature is not None:
+        return obj._input_port_signature
+    return random_signature(pipeline, obj, chm)
+
+def group_signature(pipeline, module, chm):
+    input_conns = {}
+    input_functions = {}
+    for from_module, c_id in pipeline.graph.edges_to(module.id):
+        conn = pipeline.connections[c_id]
+        if conn.destination.name not in input_conns:
+            input_conns[conn.destination.name] = []
+        input_conns[conn.destination.name].append((from_module, conn))
+    for function in module.functions:
+        if function.name not in input_functions:
+            input_functions[function.name] = []
+        input_functions[function.name].append(function)
+    covered_modules = dict((k, False) for k in module._input_remap)
+    for input_port_name, conn_list in input_conns.iteritems():
+        covered_modules[input_port_name] = True
+        input_module = module._input_remap[input_port_name]
+        upstream_sigs = [(pipeline.subpipeline_signature(m) +
+                          Hasher.connection_signature(c))
+                         for (m, c) in conn_list]
+        module_sig = Hasher.module_signature(input_module, chm)
+        sig = Hasher.subpipeline_signature(module_sig, upstream_sigs)
+        if input_port_name in input_functions:
+            function_sig = hash_list(input_functions[input_port_name], 
+                                     Hasher.function_signature, chm)
+            sig = Hasher.compound_signature([sig, function_sig])
+        input_module._input_port_signature = sig
+    for input_port_name, done in covered_modules.iteritems():
+        if done:
+            continue
+        covered_modules[input_port_name] = True
+        input_module = module._input_remap[input_port_name]
+        if input_port_name in input_functions:
+            module_sig = Hasher.module_signature(input_module, chm)
+            function_sig = hash_list(input_functions[input_port_name], 
+                                     Hasher.function_signature, chm)
+            sig = Hasher.compound_signature([module_sig, function_sig])
+        else:
+            sig = Hasher.module_signature(input_module, chm)
+        input_module._input_port_signature = sig
+
+    module.pipeline.refresh_signatures()
+
+    sig_list = []
+    sig_list.append(Hasher.module_signature(module, chm))
+    for m_id in module.pipeline.graph.sinks():
+        sig_list.append(module.pipeline.subpipeline_signature(m_id))
+    return Hasher.compound_signature(sig_list)
+
 ###############################################################################
 
 def initialize(*args, **kwargs):
     # These are all from sub_module.py!
     reg = module_registry.get_module_registry()
 
-    def random_signature(pipeline, obj, chm):
-        hasher = sha_hash()
-        hasher.update(str(random.random()))
-        return hasher.digest()
-
-    reg.add_module(InputPort, signatureCallable=random_signature)
+    reg.add_module(InputPort, signatureCallable=input_port_signature)
     reg.add_input_port(InputPort, "name", String, True)
     reg.add_input_port(InputPort, "optional", Boolean, True)
     reg.add_input_port(InputPort, "spec", String)
@@ -394,7 +450,7 @@ def initialize(*args, **kwargs):
     reg.add_input_port(OutputPort, "InternalPipe", Variant)
     reg.add_output_port(OutputPort, "ExternalPipe", Variant, True)
 
-    reg.add_module(Group)
+    reg.add_module(Group, signatureCallable=group_signature)
     reg.add_output_port(Group, "self", Group, True)
 
     reg.add_module(Abstraction, name="SubWorkflow")
