735e1b8ee6bcb788f2773a22e97bad813b2cdab3
tommy <rexissimus@yahoo.se>
2012-05-15 18:50:15 +0200

    <ticket>#370</ticket> PathChooser dialogs now uses the default data directory "dataDirectory" path when a path is not specified.
    core/system/__init__.py:
    vistrails_data_directory: *new
    Returns the current vistrails data directory
    This method was missing
    
    gui/modules/constant_configuration.py:
    - PathChooserToolButton.openChooser,
      FileChooserToolButton.openChooser,
      DirectoryChooserToolButton.openChooser,
      OutputPathChooserToolButton.openChooser:
    Use vistrails_data_directory() if none specified

--- vistrails/gui/modules/constant_configuration.py
+++ vistrails/gui/modules/constant_configuration.py
@@ -41,6 +41,7 @@ constants.
 from PyQt4 import QtCore, QtGui
 from core.utils import any, expression
 from core import system
+
 ############################################################################
 
 class ConstantWidgetMixin(object):
@@ -299,10 +300,11 @@ class PathChooserToolButton(QtGui.QToolButton):
             self.parent().update_parent()
     
     def openChooser(self):
+        text = self.lineEdit.text() or system.vistrails_data_directory()
         return QtGui.QFileDialog.getOpenFileName(self,
                                                  'Use Filename '
                                                  'as Value...',
-                                                 self.lineEdit.text(),
+                                                 text,
                                                  'All files '
                                                  '(*.*)')
 
@@ -378,10 +380,11 @@ class FileChooserToolButton(PathChooserToolButton):
                                        "Open a file chooser")
         
     def openChooser(self):
+        text = self.lineEdit.text() or system.vistrails_data_directory()
         return QtGui.QFileDialog.getOpenFileName(self,
                                                  'Use Filename '
                                                  'as Value...',
-                                                 self.lineEdit.text(),
+                                                 text,
                                                  'All files '
                                                  '(*.*)')
 
@@ -394,12 +397,13 @@ class DirectoryChooserToolButton(PathChooserToolButton):
     def __init__(self, parent=None, lineEdit=None):
         PathChooserToolButton.__init__(self, parent, lineEdit, 
                                        "Open a directory chooser")
-        
+
     def openChooser(self):
+        text = self.lineEdit.text() or system.vistrails_data_directory()
         return QtGui.QFileDialog.getExistingDirectory(self,
                                                       'Use Directory '
                                                       'as Value...',
-                                                      self.lineEdit.text())
+                                                      text)
 
 class DirectoryChooserWidget(PathChooserWidget):
     def create_browse_button(self):
@@ -411,9 +415,10 @@ class OutputPathChooserToolButton(PathChooserToolButton):
                                        "Open a path chooser")
     
     def openChooser(self):
+        text = self.lineEdit.text() or system.vistrails_data_directory()
         return QtGui.QFileDialog.getSaveFileName(self,
                                                  'Save Path',
-                                                 self.lineEdit.text(),
+                                                 text,
                                                  'All files (*.*)')
 
 class OutputPathChooserWidget(PathChooserWidget):
