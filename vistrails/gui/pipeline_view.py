db3a51bd276c25348d01ab14a1b31c9dfe017ddd
David Koop <dakoop@cs.utah.edu>
2012-06-01 10:51:04 -0700

    <bugfix>Module status brushes are checked first when setting the color of a module</bugfix> <ticket>#591</ticket>
    gui/pipeline_view.py:
      - QGraphicsModuleItem.setPainterState: push status brush to top of checks

--- vistrails/gui/pipeline_view.py
+++ vistrails/gui/pipeline_view.py
@@ -1179,7 +1179,9 @@ class QGraphicsModuleItem(QGraphicsItemInterface, QtGui.QGraphicsItem):
             else:
                 self.modulePen = CurrentTheme.MODULE_PEN
 
-        if self.customBrush:
+        if self.statusBrush:
+            self.moduleBrush = self.statusBrush
+        elif self.customBrush:
             self.moduleBrush = self.customBrush
         elif self.is_breakpoint:
             self.moduleBrush = CurrentTheme.BREAKPOINT_MODULE_BRUSH
@@ -1187,8 +1189,6 @@ class QGraphicsModuleItem(QGraphicsItemInterface, QtGui.QGraphicsItem):
             self.moduleBrush = CurrentTheme.GHOSTED_MODULE_BRUSH
         elif self.invalid:
             self.moduleBrush = CurrentTheme.INVALID_MODULE_BRUSH
-        elif self.statusBrush:
-            self.moduleBrush = self.statusBrush
         else:
             self.moduleBrush = CurrentTheme.MODULE_BRUSH
             
