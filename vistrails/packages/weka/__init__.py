6bb1d3532ef071b609d05d36c624d9b816b76487
Remi Rampin <remirampin@gmail.com>
2012-08-23 10:58:38 -0400

    Remove dependency on 'json' in the 'weka' package.

--- vistrails/packages/weka/__init__.py
+++ vistrails/packages/weka/__init__.py
@@ -13,7 +13,3 @@ configuration = ConfigurationObject(
         wekaDirectory='',
         wekaJar='weka.jar',
         wekaSrcJar='weka-src.jar')
-
-def package_requirements():
-    from core.requirements import require_python_module
-    require_python_module('json')
