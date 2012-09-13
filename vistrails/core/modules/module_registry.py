5d13c3e8292be0ff037219a397a39c0b55fb9563
David Koop <dkoop@poly.edu>
2012-09-13 16:26:49 -0700

    <bugfix>MissingPackageVersion exceptions need to be tied to specific modules</bugfix>
    Revert earlier change that assumed that MissingPackageVersion exceptions could be collapsed when they exist for more than one module.  The upgrade logic uses these exceptions on a per-module basis.
    
    core/modules/module_registry.py:
      - MissingPackageVersion._module_id: **removed** property

--- vistrails/core/modules/module_registry.py
+++ vistrails/core/modules/module_registry.py
@@ -342,13 +342,6 @@ class MissingPackageVersion(ModuleRegistryException):
         return "Missing version %s of package %s" % \
             (self._package_version, self._identifier)
 
-    def _get_module_id(self):
-        return None
-    def _set_module_id(self, m_id):
-        # do not set
-        pass
-    _module_id = property(_get_module_id, _set_module_id)
-
 class MissingModuleVersion(ModuleRegistryException):
     def __init__(self, identifier, name, namespace, module_version, 
                  package_version=None, module_id=None):
