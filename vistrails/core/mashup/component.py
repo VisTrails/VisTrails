0ca888fa33d38af63ee2ebf9113d07db21459146
Emanuele Santos <emanuelesantos@gmail.com>
2012-09-27 12:52:11 -0300

    Added NYU-Poly copyright to source files.

--- vistrails/core/mashup/component.py
+++ vistrails/core/mashup/component.py
@@ -1,5 +1,6 @@
 ###############################################################################
 ##
+## Copyright (C) 2011-2012, NYU-Poly.
 ## Copyright (C) 2006-2011, University of Utah. 
 ## All rights reserved.
 ## Contact: contact@vistrails.org
