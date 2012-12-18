e9e1717a573374c8b4a32a84a05ab1fec4f3df60
Tommy Ellqvist <tellqvis@poly.edu>
2012-12-18 18:00:52 +0100

    Removed fixedSpreadsheetCells related code in spreadsheet_sheet.py
    The added resizing code caused a core dump on Mac in some
    cases, and these calls were not needed anyway.
    
    packages/spreadsheet/spreadsheet_sheet.py:
    Reverted to before adding fixedSpreadsheetCells related code.

--- vistrails/packages/spreadsheet/spreadsheet_sheet.py
+++ vistrails/packages/spreadsheet/spreadsheet_sheet.py
@@ -41,7 +41,6 @@
 ################################################################################
 from PyQt4 import QtCore, QtGui
 from spreadsheet_helpers import CellHelpers, CellResizer
-from core.configuration import get_vistrails_configuration
 
 ################################################################################
 
@@ -67,9 +66,6 @@ class StandardWidgetHeaderView(QtGui.QHeaderView):
         self.setClickable(True)
         self.setHighlightSections(True)
         self.fitToViewport = False
-        if getattr(get_vistrails_configuration(),'fixedSpreadsheetCells',False):
-            self.setResizeMode(self.ResizeToContents)
-        
         if orientation==QtCore.Qt.Vertical:
             self.setDefaultAlignment(QtCore.Qt.AlignHCenter |
                                      QtCore.Qt.AlignVCenter)
@@ -241,12 +237,7 @@ class StandardWidgetSheet(QtGui.QTableWidget):
         self.helpers = CellHelpers(parent, CellResizer(self))
         self.setRowCount(rows)
         self.setColumnCount(cols)
-        fix = getattr(get_vistrails_configuration(),'fixedSpreadsheetCells',False)
         self.setFitToWindow(True)
-        if fix:
-            self.resizeColumnsToContents()
-            self.resizeRowsToContents()
-            self.setFitToWindow(False)
         self.connect(self,
                      QtCore.SIGNAL('cellActivated(int, int, bool)'),
                      self.selectCell)
