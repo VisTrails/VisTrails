6aaa72017fa81ec7dd36370dd47bfd2f4b6d57c5
David Koop <dkoop@poly.edu>
2013-02-01 14:56:22 -0700

    <bugfix>Fix ordering in function remap</bugfix> <ticket>#651</ticket>
    core/upgradeworkflow.py:
      - UpgradeWorkflowHandler.replace_generic: add function operations *after* the add module operation

--- vistrails/core/upgradeworkflow.py
+++ vistrails/core/upgradeworkflow.py
@@ -326,6 +326,7 @@ class UpgradeWorkflowHandler(object):
                 new_spec.name = spec_name
                 new_module.add_port_spec(new_spec)
 
+        function_ops = []
         for function in old_module.functions:
             if function.name not in function_remap:
                 function_name = function.name
@@ -335,7 +336,7 @@ class UpgradeWorkflowHandler(object):
                     # don't add the function back in
                     continue                    
                 elif type(remap) != type(""):
-                    ops.extend(remap(function, new_module))
+                    function_ops.extend(remap(function, new_module))
                     continue
                 else:
                     function_name = remap
@@ -354,6 +355,7 @@ class UpgradeWorkflowHandler(object):
 
         # add the new module
         ops.append(('add', new_module))
+        ops.extend(function_ops)
 
         create_new_connection = UpgradeWorkflowHandler.create_new_connection
 
