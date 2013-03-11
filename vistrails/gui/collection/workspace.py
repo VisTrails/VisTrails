VisTrails/VisTrails

commit b553987185103b12fdc784ca944198c61bdc0b2f

<bugfix>Workspace: Sometimes selecting an open vistrail does not select it</bugfix>
gui/collection/workspace.py:
- QVistrailList.connect_current_changed: *new
- QVistrailList.disconnect_current_changed: *new
Used to temporary disable the signal when updating
- QVistrailList.setSelected:
  Disconnect the current_changed signal and set current item

vistrails/gui/collection/workspace.py
@@ -860,20 +860,17 @@ class QVistrailList(QtGui.QTreeWidget):
         self.setSortingEnabled(True)
         self.sortItems(0, QtCore.Qt.AscendingOrder)
 
-        self.connect(self, 
-                     QtCore.SIGNAL("currentItemChanged(QTreeWidgetItem*,"
-                                   "QTreeWidgetItem*)"),
-                     self.item_changed)
-
         self.connect(self,
                      QtCore.SIGNAL('itemDoubleClicked(QTreeWidgetItem *, int)'),
                      self.item_selected)
+        
         self.setIconSize(QtCore.QSize(16,16))
 
         self.connect(self,
                      QtCore.SIGNAL('itemPressed(QTreeWidgetItem *,int)'),
                      self.onItemPressed)
         self.updateHideExecutions()
+        self.connect_current_changed()
 
     def setup_closed_files(self):
         self.closedFilesItem = QtGui.QTreeWidgetItem(['My Vistrails'])
@@ -891,6 +888,19 @@ class QVistrailList(QtGui.QTreeWidget):
             item.paramExplorationsItem.setHidden(
                              not item.paramExplorationsItem.childCount())
             
+
+    def connect_current_changed(self):
+        self.connect(self, 
+                     QtCore.SIGNAL("currentItemChanged(QTreeWidgetItem*,"
+                                   "QTreeWidgetItem*)"),
+                     self.item_changed)
+
+    def disconnect_current_changed(self):
+        self.disconnect(self, 
+                        QtCore.SIGNAL("currentItemChanged(QTreeWidgetItem*,"
+                                      "QTreeWidgetItem*)"),
+                        self.item_changed)
+    
     def show_search_results(self):
         self.searchResultsItem = QtGui.QTreeWidgetItem(['Search Results'])
         self.addTopLevelItem(self.searchResultsItem)
@@ -1461,8 +1471,7 @@ class QVistrailList(QtGui.QTreeWidget):
         self.setSelected(vistrail_window)
 
     def setSelected(self, view):
-        for item in self.selectedItems():
-            item.setSelected(False)
+        self.disconnect_current_changed()
 
         def setBold(parent_item):
             for i in xrange(parent_item.childCount()):
@@ -1473,13 +1482,14 @@ class QVistrailList(QtGui.QTreeWidget):
                 item.setFont(0, font)
                 if window:
                     item.setText(0, window.get_name())
-                # item.setSelected(view == item.window 
-                #                  if window and view else False)
+                if window and view and view == window:
+                    self.setCurrentItem(item)
                 
         if not self.openFilesItem.isHidden():
             setBold(self.openFilesItem)
         elif self.searchMode:
             setBold(self.searchResultsItem)
+        self.connect_current_changed()
             
     def item_changed(self, item, prev_item):
         if not item:

commit 24b8feb1f766bc6b6e4a539649bbe7f4f0c5f1f3

<bugfix>Opened vistrails were not selected</bugfix>
* This was broken by commit b553987185
* This also caused strange selection behaviour

gui/collection/workspace.py:
- QVistrailList.connect_current_changed, disconnect_current_changed:
  Also connect itemClicked so that already selected items can be selected
- QVistrailList.setSelected:
  Do not call setCurrentItem

vistrails/gui/collection/workspace.py
@@ -832,8 +832,8 @@ class QVistrailList(QtGui.QTreeWidget):
         self.search = None
         self.setColumnCount(1)
         self.setHeaderHidden(True)
-        self.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
-        self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.setSelectionMode(self.SingleSelection)
+        self.setSelectionBehavior(self.SelectItems)
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
         self.setDropIndicatorShown(True)
@@ -887,18 +887,26 @@ class QVistrailList(QtGui.QTreeWidget):
             item.mashupsItem.setHidden(not item.mashupsItem.childCount())
             item.paramExplorationsItem.setHidden(
                              not item.paramExplorationsItem.childCount())
-            
 
     def connect_current_changed(self):
+        # using currentItemChanged makes sure a drag selects the dragged-from
+        # vistrail
         self.connect(self, 
                      QtCore.SIGNAL("currentItemChanged(QTreeWidgetItem*,"
                                    "QTreeWidgetItem*)"),
+                     self.item_changed)
+        # using item_clicked makes sure even selected items can be clicked
+        self.connect(self, 
+                     QtCore.SIGNAL("itemClicked(QTreeWidgetItem*,int)"),
                      self.item_changed)
 
     def disconnect_current_changed(self):
         self.disconnect(self, 
                         QtCore.SIGNAL("currentItemChanged(QTreeWidgetItem*,"
                                       "QTreeWidgetItem*)"),
+                        self.item_changed)
+        self.disconnect(self, 
+                        QtCore.SIGNAL("itemClicked(QTreeWidgetItem*,int)"),
                         self.item_changed)
     
     def show_search_results(self):
@@ -1471,9 +1479,11 @@ class QVistrailList(QtGui.QTreeWidget):
         self.setSelected(vistrail_window)
 
     def setSelected(self, view):
+        """ Highlights the vistrail item for the selected item"""
         self.disconnect_current_changed()
 
         def setBold(parent_item):
+            """ """
             for i in xrange(parent_item.childCount()):
                 item = parent_item.child(i)
                 font = item.font(0)
@@ -1482,8 +1492,6 @@ class QVistrailList(QtGui.QTreeWidget):
                 item.setFont(0, font)
                 if window:
                     item.setText(0, window.get_name())
-                if window and view and view == window:
-                    self.setCurrentItem(item)
                 
         if not self.openFilesItem.isHidden():
             setBold(self.openFilesItem)