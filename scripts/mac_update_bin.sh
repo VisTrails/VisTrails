b7bf545373ad349b8291d0849e27066ea1d4ecc3
David Koop <dkoop@poly.edu>
2013-04-17 13:29:43 -0700

    Update the mac_update_bin script again to remove the old lower-level symlinks as these can interfere with the error-handling for imports.
    scripts/mac_update_bin.sh:
      - remove the api, core, db, gui, packages, and tests dirs/symlinks if they exist

--- scripts/mac_update_bin.sh
+++ scripts/mac_update_bin.sh
@@ -33,10 +33,11 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ##
 ###############################################################################
-DIR_NAME="vistrails"
+ROOT_DIR_NAME="vistrails"
 BIN_PATH_25="Contents/Resources/lib/python2.5"
 BIN_PATH_26="Contents/Resources/lib/python2.6"
 BIN_PATH_27="Contents/Resources/lib/python2.7"
+OLD_DIRS="api core db gui packages tests"
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
@@ -55,16 +56,25 @@ then
     BIN_PATH=$BIN_PATH_25
 fi
 
-if [ -e "$2/$BIN_PATH/$DIR_NAME" ]
+# remove the old lower-level symlinks if they exist
+for dir in $OLD_DIRS
+do
+    if [ -e "$2/$BIN_PATH/$dir" ]
+    then
+        rm -r $2/$BIN_PATH/$dir
+    fi
+done
+
+if [ -e "$2/$BIN_PATH/$ROOT_DIR_NAME" ]
 then
-    rm -r $2/$BIN_PATH/$DIR_NAME
+    rm -r $2/$BIN_PATH/$ROOT_DIR_NAME
 fi
-ln -s -f -F $1/$DIR_NAME $2/$BIN_PATH/$DIR_NAME
+ln -s -f -F $1/$ROOT_DIR_NAME $2/$BIN_PATH/$ROOT_DIR_NAME
 
 if [ -e "$2/$BIN_PATH/../../vistrails.py" ]
 then
     rm $2/$BIN_PATH/../../vistrails.py
 fi
-ln -s -f -F $1/$DIR_NAME/vistrails.py $2/$BIN_PATH/../../vistrails.py
+ln -s -f -F $1/$ROOT_DIR_NAME/vistrails.py $2/$BIN_PATH/../../vistrails.py
 
 exit 0
