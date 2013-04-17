200ea7df4edec9c1f186a2605c4f0668b7b6752b
David Koop <dkoop@poly.edu>
2013-04-17 10:14:05 -0700

    Update mac_update_bin.sh script to support new vistrails prefix.
    scripts/mac_update_bin.sh:
      - only need to make sure the vistrails directory and vistrails.py files are linked now

--- scripts/mac_update_bin.sh
+++ scripts/mac_update_bin.sh
@@ -33,11 +33,10 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ##
 ###############################################################################
-SRC_PATH="vistrails"
+DIR_NAME="vistrails"
 BIN_PATH_25="Contents/Resources/lib/python2.5"
 BIN_PATH_26="Contents/Resources/lib/python2.6"
 BIN_PATH_27="Contents/Resources/lib/python2.7"
-DIRS="api core db gui packages tests"
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
@@ -56,19 +55,16 @@ then
     BIN_PATH=$BIN_PATH_25
 fi
 
-for dir in $DIRS
-do
-    if [ -e "$2/$BIN_PATH/$dir" ]
-    then
-    	rm -r $2/$BIN_PATH/$dir
-    fi
-    ln -s -f -F $1/$SRC_PATH/$dir $2/$BIN_PATH/$dir
-done
+if [ -e "$2/$BIN_PATH/$DIR_NAME" ]
+then
+    rm -r $2/$BIN_PATH/$DIR_NAME
+fi
+ln -s -f -F $1/$DIR_NAME $2/$BIN_PATH/$DIR_NAME
 
 if [ -e "$2/$BIN_PATH/../../vistrails.py" ]
 then
     rm $2/$BIN_PATH/../../vistrails.py
 fi
-ln -s -f -F $1/$SRC_PATH/vistrails.py $2/$BIN_PATH/../../vistrails.py
+ln -s -f -F $1/$DIR_NAME/vistrails.py $2/$BIN_PATH/../../vistrails.py
 
 exit 0
