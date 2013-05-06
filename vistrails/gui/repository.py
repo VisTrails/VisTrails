f7f8cb876726689186141c9c4d20162a9f48e69d
Remi Rampin <remirampin@gmail.com>
2013-05-06 12:33:53 -0400

    Fixes mk[sd]temp() usage in QRepositoryPushWidget
    The directory logic didn't seem to be needed.

--- vistrails/gui/repository.py
+++ vistrails/gui/repository.py
@@ -421,9 +421,7 @@ class QRepositoryPushWidget(QtGui.QWidget):
         self.update_push_information()
         try:
             # create temp file
-            self.directory = tempfile.mkdtemp(prefix='vt_tmp')
-            (fd, filename) = tempfile.mkstemp(suffix='.vt', prefix='vt_tmp',
-                                              dir=self.directory)
+            (fd, filename) = tempfile.mkstemp(suffix='.vt', prefix='vt_tmp')
             os.close(fd)
 
             # writing tmp vt and switching back to orginal vt
@@ -501,10 +499,8 @@ class QRepositoryPushWidget(QtGui.QWidget):
                     updated_file = result.read()
 
                     # create temp file of updated vistrail
-                    self.directory = tempfile.mkdtemp(prefix='vt_tmp')
                     (fd, updated_filename) = tempfile.mkstemp(suffix='.vtl',
-                                                              prefix='vtl_tmp',
-                                                              dir=self.directory)
+                                                              prefix='vtl_tmp')
                     os.close(fd)
                     updated_vt = open(updated_filename, 'w')
                     updated_vt.write(updated_file)
@@ -528,7 +524,6 @@ class QRepositoryPushWidget(QtGui.QWidget):
 
                     os.remove(updated_filename)
                     os.remove(updated_filename[:-1])
-                    os.rmdir(self.directory)
 
                     self._repository_status['details'] = \
                             "Update to repository was successful"
