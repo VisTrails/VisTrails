100a220ddb5bef60d6f9cdbaf35e746e731677d7
Remi Rampin <remirampin@gmail.com>
2013-05-16 17:55:44 -0400

    Reorganizes tabledata using make_modules_dict()

--- vistrails/packages/tabledata/read/__init__.py
+++ vistrails/packages/tabledata/read/__init__.py
@@ -1,5 +1,7 @@
+from vistrails.core.modules.utils import make_modules_dict
+
 from read_numpy import _modules as numpy_modules
 from read_csv import _modules as csv_modules
 
 
-_modules = numpy_modules + csv_modules
+_modules = make_modules_dict(numpy_modules, csv_modules, namespace='read')
