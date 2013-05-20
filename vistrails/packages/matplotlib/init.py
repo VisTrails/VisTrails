02a9a276bc8f16b35a604569926ad6867ca80383
Remi Rampin <remirampin@gmail.com>
2013-05-20 17:03:45 -0400

    Adds linux-debian bundle names for pkg matplotlib

--- vistrails/packages/matplotlib/init.py
+++ vistrails/packages/matplotlib/init.py
@@ -47,7 +47,8 @@ from vistrails.core.vistrail.operation import AddOp
 
 from vistrails.core.bundles import py_import
 try:
-    mpl_dict = {'linux-ubuntu': 'python-matplotlib',
+    mpl_dict = {'linux-debian': 'python-matplotlib',
+                'linux-ubuntu': 'python-matplotlib',
                 'linux-fedora': 'python-matplotlib'}
     matplotlib = py_import('matplotlib', mpl_dict)
     matplotlib.use('Qt4Agg', warn=False)
