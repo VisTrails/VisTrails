VisTrails/VisTrails

commit 7120176e99e1a1704eb293c2909719f8b12f248d

Disables the QModuleInfo on read-only pipelines
vistrails/gui/vistrail_view.py
  - Moves the setReadOnlyMode() call on the QPipelineView to before it is
    added, so that set_to_current has the correct mode.
vistrails/gui/pipeline_view.py
  - When set_to_current() is called, we set the QModuleInfo to the same
    read only mode as us.
vistrails/gui/module_info.py
  - Adds a setReadOnly() method that disables the PortsList's and
    QModuleAnnotationTable.
vistrails/gui/module_annotation.py
vistrails/gui/ports_pane.py
  - Adds a setReadOnly() method used to disable the widget.

vistrails/gui/pipeline_view.py
@@ -62,6 +62,7 @@ from vistrails.gui.controlflow_assist import QControlFlowAssistDialog
 from vistrails.gui.graphics_view import (QInteractiveGraphicsScene,
                                QInteractiveGraphicsView,
                                QGraphicsItemInterface)
+from vistrails.gui.module_info import QModuleInfo
 from vistrails.gui.module_palette import QModuleTreeWidget
 from vistrails.gui.theme import CurrentTheme
 from vistrails.gui.utils import getBuilderWindow
@@ -3191,6 +3192,7 @@ class QPipelineView(QInteractiveGraphicsView, BaseView):
             # controller.current_pipeline_view = self.scene()
 
     def set_to_current(self):
+        QModuleInfo.instance().setReadOnly(self.scene().read_only_mode)
         if self.controller.current_pipeline_view is not None:
             self.disconnect(self.controller,
                             QtCore.SIGNAL('versionWasChanged'),

vistrails/gui/module_annotation.py
@@ -96,6 +96,7 @@ class QModuleAnnotationTable(QtGui.QTableWidget):
         
         """
         QtGui.QTableWidget.__init__(self, 1, 2, parent)
+        self.read_only = False
         self.setHorizontalHeaderLabels(QtCore.QStringList() << 'Key' << 'Value')
         self.horizontalHeader().setResizeMode(QtGui.QHeaderView.Interactive)
         self.horizontalHeader().setMovable(False)
@@ -112,6 +113,11 @@ class QModuleAnnotationTable(QtGui.QTableWidget):
 
     def set_controller(self, controller):
         self.controller = controller
+
+    def setReadOnly(self, read_only):
+        if read_only != self.read_only:
+            self.read_only = read_only
+            self.setEnabled(not read_only and self.module is not None)
 
     def updateModule(self, module=None):
         """ updateModule() -> None
@@ -136,7 +142,7 @@ class QModuleAnnotationTable(QtGui.QTableWidget):
                     item = QtGui.QTableWidgetItem(annotation.value)
                     self.setItem(curRow, 1, item)
                     curRow += 1
-            self.setEnabled(True)
+            self.setEnabled(not self.read_only)
         else:
             self.setRowCount(1)
             self.setEnabled(False)

vistrails/gui/module_info.py
@@ -47,6 +47,7 @@ class QModuleInfo(QtGui.QWidget, QVistrailsPaletteInterface):
         self.controller = None
         self.module = None
         self.pipeline_view = None # pipeline_view
+        self.read_only = False
 
     def build_widget(self):
         name_label = QtGui.QLabel("Name:")
@@ -122,6 +123,12 @@ class QModuleInfo(QtGui.QWidget, QVistrailsPaletteInterface):
         layout.setAlignment(QtCore.Qt.AlignTop)
         self.setLayout(layout)
         self.setWindowTitle('Module Information')
+
+    def setReadOnly(self, read_only):
+        if read_only != self.read_only:
+            self.read_only = read_only
+            for widget in self.ports_lists + [self.annotations]:
+                widget.setReadOnly(read_only)
 
     def set_controller(self, controller):
         self.controller = controller

vistrails/gui/ports_pane.py
@@ -344,6 +344,9 @@ class PortsList(QtGui.QTreeWidget):
         self.port_spec_items = {}
         self.entry_klass = ParameterEntry
 
+    def setReadOnly(self, read_only):
+        self.setEnabled(not read_only)
+
     def set_entry_klass(self, entry_klass):
         self.entry_klass = entry_klass
         self.update_module(self.module)

vistrails/gui/vistrail_view.py
@@ -413,15 +413,13 @@ class QVistrailView(QtGui.QWidget):
             for m_id in items[0]:
                 module = pipelineView.current_pipeline.modules[m_id]
                 if module.is_group() or module.is_abstraction():
-                    newPipelineView = self.add_pipeline_view()
+                    newPipelineView = self.add_pipeline_view(True)
                     newPipelineView.controller.current_pipeline_view = \
                         newPipelineView.scene()
                     module.pipeline.ensure_connection_specs()
                     newPipelineView.scene().setupScene(module.pipeline)
                     newPipelineView.scene().current_pipeline = module.pipeline
-                    newPipelineView.scene().fitToView(newPipelineView, True)
-                    newPipelineView.setReadOnlyMode(True)
-            
+
     def create_view(self, klass, add_tab=True):
         view = klass(self)
         view.set_vistrail_view(self)
@@ -715,19 +713,20 @@ class QVistrailView(QtGui.QWidget):
             else:
                 _app.notify("controller_changed", self.controller)
             self.reset_version_view()
-            
-    def create_pipeline_view(self):
+
+    def create_pipeline_view(self, read_only=False):
         view = self.create_view(QPipelineView)
-        self.connect(view.scene(), QtCore.SIGNAL('moduleSelected'), 
+        view.setReadOnlyMode(read_only)
+        self.connect(view.scene(), QtCore.SIGNAL('moduleSelected'),
                      self.gen_module_selected(view))
         view.set_controller(self.controller)
         view.set_to_current()
         self.set_notification('module_done_configure', view.done_configure)
         #self.switch_to_tab(view.tab_idx)
         return view
-    
-    def add_pipeline_view(self):
-        view = self.create_pipeline_view()
+
+    def add_pipeline_view(self, read_only=False):
+        view = self.create_pipeline_view(read_only)
         self.switch_to_tab(view.tab_idx)
         return view
 

commit 4bdf4705a66912a81c0345cf60d2f38a25738951

Restores read-only mode removed by previous merge
In 5b374ef72d, Tommy removed the setReadOnly() call while fixing
conflicts. This broke the feature introduced by 7120176e99.

vistrails/gui/pipeline_view.py
@@ -64,6 +64,7 @@ from vistrails.gui.controlflow_assist import QControlFlowAssistDialog
 from vistrails.gui.graphics_view import (QInteractiveGraphicsScene,
                                QInteractiveGraphicsView,
                                QGraphicsItemInterface)
+from vistrails.gui.module_info import QModuleInfo
 from vistrails.gui.module_palette import QModuleTreeWidget
 from vistrails.gui.theme import CurrentTheme
 from vistrails.gui.utils import getBuilderWindow
@@ -3272,6 +3273,7 @@ class QPipelineView(QInteractiveGraphicsView, BaseView):
             # controller.current_pipeline_view = self.scene()
 
     def set_to_current(self):
+        QModuleInfo.instance().setReadOnly(self.scene().read_only_mode)
         self.controller.set_pipeline_view(self)
 
     def get_long_title(self):