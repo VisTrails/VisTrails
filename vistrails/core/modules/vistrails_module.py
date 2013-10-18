VisTrails/VisTrails

commit c805e6296eba4b51b8aa916f3f83ecb06160c5df

Implements not stopping on errors

vistrails/core/modules/vistrails_module.py
@@ -78,7 +78,13 @@ class ModuleBreakpoint(Exception):
             inputs[p] = self.module.getInputListFromPort(p)
 
         return inputs
-        
+
+class ModuleHadError(Exception):
+    """Exception occurring when a module that failed gets updated again.
+
+    It is caught by the interpreter that doesn't log it.
+    """
+
 class ModuleError(Exception):
 
     """Exception representing a VisTrails module runtime error. This
@@ -245,6 +251,7 @@ Designing New Modules
         self.inputPorts = {}
         self.outputPorts = {}
         self.upToDate = False
+        self.ran = False
         self.setResult("self", self) # every object can return itself
         self.logging = _dummy_logging
 
@@ -346,14 +353,20 @@ context."""
         modules. Report to the logger if available
         
         """
-        self.logging.begin_update(self)
-        self.updateUpstream()
-        if self.suspended:
-            return
         if self.upToDate:
             if not self.computed:
                 self.logging.update_cached(self)
                 self.computed = True
+            return
+        if self.ran:
+            if self.had_error:
+                raise ModuleHadError(self)
+            return
+        self.ran = True
+        self.had_error = True # Unset later in this method
+        self.logging.begin_update(self)
+        self.updateUpstream()
+        if self.suspended:
             return
         self.logging.begin_compute(self)
         try:
@@ -387,6 +400,7 @@ context."""
         if self.annotate_output:
             self.annotate_output_values()
         self.upToDate = True
+        self.had_error = False
         self.logging.end_update(self)
         self.logging.signalSuccess(self)
 

vistrails/core/interpreter/cached.py
@@ -40,7 +40,7 @@ import vistrails.core.db.io
 from vistrails.core.log.controller import DummyLogController
 from vistrails.core.modules.basic_modules import identifier as basic_pkg
 from vistrails.core.modules.vistrails_module import ModuleConnector, \
-    ModuleError, ModuleBreakpoint, ModuleErrors
+    ModuleHadError, ModuleError, ModuleBreakpoint, ModuleErrors
 from vistrails.core.utils import DummyView
 from vistrails.core.vistrail.annotation import Annotation
 from vistrails.core.vistrail.vistrail import Vistrail
@@ -139,6 +139,7 @@ class CachedInterpreter(vistrails.core.interpreter.base.BaseInterpreter):
         actions = fetch('actions', None)
         done_summon_hooks = fetch('done_summon_hooks', [])
         module_executed_hook = fetch('module_executed_hook', [])
+        stop_on_error = fetch('stop_on_error', True)
 
         if len(kwargs) > 0:
             raise VistrailsInternalError('Wrong parameters passed '
@@ -296,6 +297,7 @@ class CachedInterpreter(vistrails.core.interpreter.base.BaseInterpreter):
         module_suspended_hook = fetch('module_suspended_hook', [])
         done_summon_hooks = fetch('done_summon_hooks', [])
         clean_pipeline = fetch('clean_pipeline', False)
+        stop_on_error = fetch('stop_on_error', True)
         # parent_exec = fetch('parent_exec', None)
 
         if len(kwargs) > 0:
@@ -439,18 +441,20 @@ class CachedInterpreter(vistrails.core.interpreter.base.BaseInterpreter):
         for obj in persistent_sinks:
             try:
                 obj.update()
+                continue
+            except ModuleHadError:
+                pass
             except ModuleErrors, mes:
                 for me in mes.module_errors:
                     me.module.logging.end_update(me.module, me.msg)
                     errors[me.module.id] = me
-                break
             except ModuleError, me:
                 me.module.logging.end_update(me.module, me.msg, me.errorTrace)
                 errors[me.module.id] = me
-                break
             except ModuleBreakpoint, mb:
                 mb.module.logging.end_update(mb.module)
                 errors[mb.module.id] = mb
+            if stop_on_error:
                 break
 
         if self.done_update_hook:
@@ -606,6 +610,7 @@ class CachedInterpreter(vistrails.core.interpreter.base.BaseInterpreter):
         actions = fetch('actions', None)
         done_summon_hooks = fetch('done_summon_hooks', [])
         module_executed_hook = fetch('module_executed_hook', [])
+        stop_on_error = fetch('stop_on_error', True)
 
         if len(kwargs) > 0:
             raise VistrailsInternalError('Wrong parameters passed '

vistrails/core/vistrail/controller.py
@@ -2476,7 +2476,9 @@ class VistrailController(object):
     
     def execute_workflow_list(self, vistrails):
         """execute_workflow_list(vistrails: list) -> (results, bool)"""
-        
+
+        stop_on_error = getattr(get_vistrails_configuration(),
+                                'stopOnError')
         interpreter = get_default_interpreter()
         changed = False
         results = []
@@ -2501,6 +2503,7 @@ class VistrailController(object):
                       'reason': reason,
                       'sinks': sinks,
                       'extra_info': extra_info,
+                      'stop_on_error': stop_on_error,
                       }    
             if self.get_vistrail_variables():
                 kwargs['vistrail_variables'] = \

commit 9dbf4cc8767a7240826c2023fa5fdfcb6c649d1e

Fixes marking cached modules
This had been broken by c805e629 when introducing "don't stop on error".

vistrails/core/modules/vistrails_module.py
@@ -253,6 +253,7 @@ Designing New Modules
         self.outputPorts = {}
         self.upToDate = False
         self.ran = False
+        self.had_error = False
         self.setResult("self", self) # every object can return itself
         self.logging = _dummy_logging
 
@@ -354,21 +355,21 @@ context."""
         modules. Report to the logger if available
         
         """
+        if self.ran and self.had_error:
+            raise ModuleHadError(self)
+        elif self.ran and self.computed:
+            return
+        self.ran = True
+        self.logging.begin_update(self)
+        self.updateUpstream()
+        if self.suspended:
+            return
         if self.upToDate:
             if not self.computed:
                 self.logging.update_cached(self)
                 self.computed = True
             return
-        if self.ran:
-            if self.had_error:
-                raise ModuleHadError(self)
-            return
-        self.ran = True
         self.had_error = True # Unset later in this method
-        self.logging.begin_update(self)
-        self.updateUpstream()
-        if self.suspended:
-            return
         self.logging.begin_compute(self)
         try:
             if self.is_breakpoint:
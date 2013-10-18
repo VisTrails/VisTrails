9dbf4cc8767a7240826c2023fa5fdfcb6c649d1e
Remi Rampin <remirampin@gmail.com>
2013-10-18 11:32:49 -0400

    Fixes marking cached modules
    This had been broken by c805e629 when introducing "don't stop on error".

--- vistrails/core/modules/vistrails_module.py
+++ vistrails/core/modules/vistrails_module.py
@@ -253,6 +253,7 @@ Designing New Modules
         self.outputPorts = {}
         self.upToDate = False
         self.ran = False
+        self.had_error = False
         self.setResult("self", self) # every object can return itself
         self.logging = _dummy_logging
 
@@ -354,21 +355,21 @@ context."""
         modules. Report to the logger if available
         
         """
+        if self.ran and self.had_error:
+            raise ModuleHadError(self)
+        elif self.ran and self.computed:
+            return
+        self.ran = True
+        self.logging.begin_update(self)
+        self.updateUpstream()
+        if self.suspended:
+            return
         if self.upToDate:
             if not self.computed:
                 self.logging.update_cached(self)
                 self.computed = True
             return
-        if self.ran:
-            if self.had_error:
-                raise ModuleHadError(self)
-            return
-        self.ran = True
         self.had_error = True # Unset later in this method
-        self.logging.begin_update(self)
-        self.updateUpstream()
-        if self.suspended:
-            return
         self.logging.begin_compute(self)
         try:
             if self.is_breakpoint:
