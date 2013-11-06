65126e8939442a7ba02b7656dbc79c22fecfc808
Remi Rampin <remirampin@gmail.com>
2013-11-06 12:39:59 -0500

    Disables test and warns about bugged pytz version

--- vistrails/packages/tabledata/convert/convert_dates.py
+++ vistrails/packages/tabledata/convert/convert_dates.py
@@ -1,6 +1,8 @@
 import datetime
+from distutils.version import LooseVersion
 import re
 import time
+import warnings
 
 from vistrails.core.modules.vistrails_module import Module, ModuleError
 from vistrails.core.bundles import py_import
@@ -100,6 +102,13 @@ def make_timezone(s):
             raise ValueError("can't understand timezone %r (maybe you should "
                              "install pytz?)" % s)
         else:
+            ver = LooseVersion(pytz.__version__)
+            if ver < LooseVersion('2013'):
+                warnings.warn(
+                        "You are using an old version of pytz (%s). You might "
+                        "run into some issues with daylight saving handling." %
+                        pytz.__version__,
+                        category=UserWarning)
             try:
                 return pytz.timezone(s)
             except KeyError:
@@ -443,6 +452,9 @@ class TestStringsToDates(unittest.TestCase):
             import pytz
         except ImportError:
             self.skipTest("pytz is not available")
+        if LooseVersion(pytz.__version__) < LooseVersion('2013'):
+            self.skipTest("pytz version is known to cause issues (%s)" %
+                          pytz.__version__)
 
         dates = ['2013-01-20 9:25', '2013-01-20 09:31', '2013-06-02 19:05']
         in_fmt = '%Y-%m-%d %H:%M'
