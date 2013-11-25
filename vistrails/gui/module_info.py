71147be4b5efc3ff3f2d36314730788719cc5702
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2013-11-25 16:54:45 +0100

    Improvements to focus and view switching
    builderWindow.get_current_view no longer tries to guess the view
    and just returns the current view
    
    builderWindow.applicationFocusChanged now detect focus changes correctly by also being triggered by mouse clicks
    
    Detached views and pipelines now works correctly. Modules can be dropped in the active pipeline in any view, but only the active view will show connection snapping.
    
    Detached views can now use toolbars docked in the main view correctly.
    
    Detached views can now be re-attached.
    
    ---
    
    gui/module_info.py:
    - QModuleInfo.name_editing_finished:
    Updating annotation can cause a focus change so we need to prevent the method from being called a second time
    
    gui/module_palette.py:
    - QModuleTreeWidget.startDrag:
    Use controller.current_pipeline_view because current pipeline may be detached
    
    gui/pipeline_view.py:
    - QPipelineScene:
    Only show port snapping if this is the current pipeline
    Only allow dropping if this is the current pipeline in any view
    
    gui/vistrails_window.py:
    - QVistrailsWindow.__init__:
      Call applicationFocusChanged on MouseButtonPress to catch all view changes
    - QVistrailsWindow.dock_palettes:
      For some reason dropping to a detached view does not work before the module palette has been docked once. This fix will cause detached palettes to be docked to the main window when detaching a view, but I could find no other fix.
    - QVistrailsWindow.get_current_view:
      This method caused many problems and now just returns self.current_view
    - QVistrailsWindow.get_next_view: *new
      Returns one of the available views if one exists. This is used to select a vistrail when a vistrail is closed.
    - QVistrailsWindow.update_window_menu:
      Added Re-attach view action
    - QVistrailsWindow.applicationFocusChanged:
      Rewritten to handle the simplified get_current_view()
    
    Ticket: 776

--- vistrails/gui/module_info.py
+++ vistrails/gui/module_info.py
@@ -48,6 +48,7 @@ class QModuleInfo(QtGui.QWidget, QVistrailsPaletteInterface):
         self.module = None
         self.pipeline_view = None # pipeline_view
         self.read_only = False
+        self.is_updating = False
 
     def build_widget(self):
         name_label = QtGui.QLabel("Name:")
@@ -171,25 +172,27 @@ class QModuleInfo(QtGui.QWidget, QVistrailsPaletteInterface):
             # self.module_id.setEnabled(True)
 
     def name_editing_finished(self):
-        if self.module is not None:
+        # updating module may trigger a second call so we check for that
+        if self.is_updating or self.module is None:
+            return
+        try:
+            self.is_updating = True
             old_text = ''
             if self.module.has_annotation_with_key('__desc__'):
                 old_text = self.module.get_annotation_by_key('__desc__').value
             new_text = str(self.name_edit.text()).strip()
             if not new_text:
                 if old_text:
-                    #print 'delete annotation'
                     self.controller.delete_annotation('__desc__', 
                                                       self.module.id)
             elif old_text != new_text:
-                #print 'add annotation', old_text, new_text
                 self.controller.add_annotation(('__desc__', new_text), 
                                                self.module.id)
-                
-
             scene = self.controller.current_pipeline_scene
             scene.recreate_module(self.controller.current_pipeline, 
                                   self.module.id)
+        finally:
+            self.is_updating = False
             
     def configure(self):
         from vistrails.gui.vistrails_window import _app
