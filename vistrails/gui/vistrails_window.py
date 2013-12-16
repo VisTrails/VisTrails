63641f31883bf6f67d6d675d3f65a34f0226b966
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2013-12-16 17:14:14 +0100

    Fixed vistrail always being executed when opened
    gui/vistrails_window.py:
    - open_vistrail_from_locator:
      Check if mashupversion is set before setting execute flag

--- vistrails/gui/vistrails_window.py
+++ vistrails/gui/vistrails_window.py
@@ -1666,7 +1666,8 @@ class QVistrailsWindow(QVistrailViewWindow):
                 mashuptrail = locator._mshptrail
             if hasattr(locator, '_mshpversion'):
                 mashupversion = locator._mshpversion
-                execute = True
+                if mashupversion:
+                    execute = True
             self.open_vistrail_without_prompt(locator, version, 
                                               mashuptrail=mashuptrail,
                                               mashupVersion=mashupversion,
