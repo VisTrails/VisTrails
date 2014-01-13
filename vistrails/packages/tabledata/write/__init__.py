5e39ce62ba13cd6f27a9b80f3c8f9c126a0be3ab
Remi Rampin <remirampin@gmail.com>
2014-01-13 18:15:56 -0500

    Adds a WriteExcelSpreadsheet module

--- vistrails/packages/tabledata/write/__init__.py
+++ vistrails/packages/tabledata/write/__init__.py
@@ -9,6 +9,7 @@ else:
     from write_numpy import _modules as numpy_modules
 
 from write_csv import _modules as csv_modules
+from write_excel import _modules as excel_modules
 
 
 _modules = make_modules_dict(numpy_modules, csv_modules, excel_modules,
--- vistrails/packages/tabledata/write/write_excel.py
+++ vistrails/packages/tabledata/write/write_excel.py
@@ -1,1 +1,50 @@
-_modules = []
+from vistrails.core.bundles.pyimport import py_import
+from vistrails.core import debug
+from vistrails.core.modules.vistrails_module import Module, ModuleError
+from vistrails.packages.tabledata.common import Table
+
+
+def get_xlwt():
+    try:
+        return py_import('xlwt', {
+                'pip': 'xlwt',
+                'linux-debian': 'python-xlwt',
+                'linux-ubuntu': 'python-xlwt',
+                'linux-fedora': 'python-xlwt'})
+    except ImportError: # pragma: no cover
+        return None
+
+
+class WriteExcelSpreadsheet(Module):
+    """Writes a table to an Excel spreadsheet file.
+    """
+    _input_ports = [('table', Table)]
+    _output_ports = [('file', '(org.vistrails.vistrails.basic:File)')]
+
+    def compute(self):
+        table = self.get_input('table')
+        rows = table.rows
+
+        xlwt = get_xlwt()
+        if xlwt is None: # pragma: no cover
+            raise ModuleError(self, "xlwt is not available")
+
+        workbook = xlwt.Workbook()
+        sheet = workbook.add_sheet('Sheet1')
+
+        fileobj = self.interpreter.filePool.create_file(suffix='.xls')
+        fname = fileobj.name
+
+        for c in xrange(table.columns):
+            column = table.get_column(c)
+            for r, e in enumerate(column):
+                sheet.write(r, c, e)
+            if r != rows:
+                debug.warning("WriteExcelSpreadsheet wrote %d lines instead "
+                              "of expected %d" % (r, rows))
+
+        workbook.save(fname)
+        self.set_output('file', fileobj)
+
+
+_modules = [WriteExcelSpreadsheet]
