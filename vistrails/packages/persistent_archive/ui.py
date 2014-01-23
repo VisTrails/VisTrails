53fbf2bd6abeed524ce3c31d7c8ac68bd68bbf02
Remi Rampin <remirampin@gmail.com>
2014-01-23 12:51:26 -0500

    Adds "Go to Vistrails" button in viewer
    Uses very hacky code to select the module in the pipeline view.

--- vistrails/packages/persistent_archive/ui.py
+++ vistrails/packages/persistent_archive/ui.py
@@ -1,10 +1,85 @@
+from PyQt4 import QtCore, QtGui
+
+import file_archive.viewer
 from file_archive.viewer import StoreViewerWindow
+
+from vistrails.core.application import get_vistrails_application
+from vistrails.core.db.locator import FileLocator
 
 from .common import get_default_store
 
 
+class VistrailsViewerWindow(StoreViewerWindow):
+    WINDOW_TITLE = "Persistent archive viewer"
+
+    def _create_buttons(self):
+        buttons = super(VistrailsViewerWindow, self)._create_buttons()
+
+        open_vt_button = QtGui.QPushButton("Go to Vistrail")
+        self.connect(open_vt_button, QtCore.SIGNAL('clicked()'),
+                     self._open_vt)
+        buttons.append(('vt', open_vt_button))
+
+        return buttons
+
+    def _selection_changed(self):
+        super(VistrailsViewerWindow, self)._selection_changed()
+        items = self._result_tree.selectedItems()
+        for t, button in self._buttons:
+            if t == 'vt':
+                button.setEnabled(
+                        len(items) == 1 and
+                        self._find_vt(items[0]) is not None)
+
+    @staticmethod
+    def _find_vt(item):
+        if isinstance(item, file_archive.viewer.MetadataItem):
+            item = item.parent()
+        metadata = item.entry.metadata
+        try:
+            workflow = metadata['vistrails_workflow']
+            module_id = metadata['vistrails_module_id']
+        except KeyError:
+            return None
+        return (workflow, module_id)
+
+    def _open_vt(self):
+        items = self._result_tree.selectedItems()
+        item = items[0]
+        vt = self._find_vt(item)
+        if vt is None:
+            return
+        workflow, module_id = vt
+
+        if ':' in workflow:
+            filename, version = workflow.rsplit(':', 1)
+            try:
+                version = int(version)
+            except ValueError:
+                filename, version = workflow, None
+        else:
+            filename, version = workflow, None
+
+        app = get_vistrails_application()
+        if (not app.is_running_gui() or not hasattr(app, 'builderWindow') or
+                app.builderWindow is None):
+            return
+
+        view = app.builderWindow.open_vistrail(FileLocator(filename),
+                                               version=version)
+        if module_id is not None:
+            from vistrails.gui.pipeline_view import QGraphicsModuleItem
+
+            scene = view.controller.current_pipeline_view.scene()
+            for module_item in (i for i in scene.items()
+                                  if isinstance(i, QGraphicsModuleItem)):
+                if module_item.module.id == module_id:
+                    module_item.setSelected(True)
+                    break
+
+
 store = get_default_store()
-viewer = StoreViewerWindow(store)
+viewer = VistrailsViewerWindow(store)
 
 
 def show_viewer():
