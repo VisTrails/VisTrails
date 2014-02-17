VisTrails/VisTrails

commit 2674fe23f8e64bf6a6cae09e1be20407eb1955eb

<feature>Improve and generalize widget configuration specification</feature>
There are three major changes here:

(1) Constant widgets are now all specified as arguments to
ModuleRegistry.add_module.  This includes "default" widgets, widgets
for the query interface, and widgets for the parameter exploration
interface.  This should also support other types of widgets like those
used for Mashups, although this has not yet been considered.  Each
widget is specified with (a) a "widget_use"---default, query,
paramexp, and (b) a "widget_type" which is linked to
PortSpec.entry_type so that the display of any given port can be
customized. Both (a) and (b) default to "default" if neither is
specified.

(2) Constant widgets now obey inheritance.  If a superclass has
specified widgets, those widgets are now inherited by the subclass.  A
user may override the inheritance as normal.

(3) Specifying the widgets uses defaulted namedtuples to make code
more understandable.  The list of constantWidgets contains **all**
widgets, but the ConstantWidgetConfig namedtuple makes it possible to
write clearer code.  Furthermore, "subclasses" QueryWidgetConfig and
ParamExpWidgetConfig make those differences clearer by defaulting
widget_use to the corresponding use.

The plan is to expand vistrails.core.modules.config to add namedtuples
for PortSpecConfig, ModuleConfig so that the "shorthand", inline
Module definition is clearer.

Note: get_widget_class is preserved and is used when no default
constant widget is found in constantWidgets, but
get_query_widget_class, get_param_explore_widget_list, and the newly
introduced get_widget_class_of_type methods are all **removed**.  This
should not cause problems as I do not think anyone outside of us has
used them.

vistrails/core/modules/basic_modules.py:
  - Constant.get_widget_class_of_type: **removed**
  - Constant.get_query_widget_class: **removed**
  - Constant.get_param_explore_widget_class: **removed**

vistrails/gui/variable_dropbox.py
@@ -50,7 +50,7 @@ from vistrails.core.modules import module_registry
 from vistrails.core.modules.basic_modules import Constant
 from vistrails.core.vistrail.vistrailvariable import VistrailVariable
 from vistrails.gui.common_widgets import QPromptWidget
-from vistrails.gui.modules import get_widget_class
+from vistrails.gui.modules.utils import get_widget_class
 from vistrails.gui.modules.constant_configuration import StandardConstantWidget, \
     FileChooserToolButton
 from vistrails.gui.module_palette import QModuleTreeWidget

vistrails/core/modules/basic_modules.py
@@ -38,6 +38,8 @@ import vistrails.core.cache.hasher
 from vistrails.core.modules.module_registry import get_module_registry
 from vistrails.core.modules.vistrails_module import Module, new_module, \
      Converter, NotCacheable, ModuleError
+from vistrails.core.modules.config import ConstantWidgetConfig, \
+    QueryWidgetConfig, ParamExpWidgetConfig
 import vistrails.core.system
 from vistrails.core.utils import InstanceObject
 from vistrails.core import debug
@@ -132,18 +134,6 @@ class Constant(Module):
         return None
 
     @staticmethod
-    def get_widget_class_of_type(widget_type):
-        return None
-
-    @staticmethod
-    def get_query_widget_class():
-        return None
-
-    @staticmethod
-    def get_param_explore_widget_list():
-        return []
-
-    @staticmethod
     def query_compute(value_a, value_b, query_method):
         if query_method == '==' or query_method is None:
             return (value_a == value_b)
@@ -154,10 +144,7 @@ class Constant(Module):
 def new_constant(name, py_conversion=None, default_value=None, validation=None,
                  widget_type=None,
                  str_conversion=None, base_class=Constant,
-                 compute=None, query_widget_type=None,
-                 query_compute=None,
-                 param_explore_widget_list=None,
-                 widget_types_list=None):
+                 compute=None, query_compute=None):
     """new_constant(name: str, 
                     py_conversion: callable,
                     default_value: python_type,
@@ -166,12 +153,7 @@ def new_constant(name, py_conversion=None, default_value=None, validation=None,
                     str_conversion: callable,
                     base_class: class,
                     compute: callable,
-                    query_widget_type: (path, name) tuple or QWidget type,
-                    query_compute: static callable,
-                    param_explore_widget_list: 
-                        list((path, name) tuple or QWidget type)
-                    widget_types_list:
-                        list((path, name) tuple or QWidget type)) -> Module
+                    query_compute: static callable) -> Module
 
     new_constant dynamically creates a new Module derived from
     Constant with given py_conversion and str_conversion functions, a
@@ -217,26 +199,7 @@ def new_constant(name, py_conversion=None, default_value=None, validation=None,
         @staticmethod
         def get_widget_class():
             return widget_type
-        d['get_widget_class'] = get_widget_class
-    if query_widget_type is not None:
-        @staticmethod
-        def get_query_widget_class():
-            return query_widget_type
-        d['get_query_widget_class'] = get_query_widget_class
-    if param_explore_widget_list is not None:
-        @staticmethod
-        def get_param_explore_widget_list():
-            return param_explore_widget_list
-        d['get_param_explore_widget_list'] = get_param_explore_widget_list
-    if widget_types_list is not None:
-        @staticmethod
-        def get_widget_class_of_type(w_type):
-            for (t, widget) in widget_types_list:
-                if w_type == t:
-                    return widget
-            return None
-        d['get_widget_class_of_type'] = get_widget_class_of_type
-            
+        d['get_widget_class'] = get_widget_class            
 
     m = new_module(base_class, name, d)
     m._input_ports = [('value', m)]
@@ -289,30 +252,17 @@ def string_compare(value_a, value_b, query_method):
     return False
 
 Boolean = new_constant('Boolean' , staticmethod(bool_conv),
-                       False, staticmethod(lambda x: isinstance(x, bool)),
-                       widget_type=('vistrails.gui.modules.constant_configuration', 
-                                    'BooleanWidget'))
+                       False, staticmethod(lambda x: isinstance(x, bool)))
 Float   = new_constant('Float'   , staticmethod(float), 0.0, 
                        staticmethod(lambda x: isinstance(x, (int, long, float))),
-                       query_widget_type=('vistrails.gui.modules.query_configuration',
-                                          'NumericQueryWidget'),
-                       query_compute=numeric_compare,
-                       param_explore_widget_list=[('vistrails.gui.modules.paramexplore',
-                                                   'FloatExploreWidget')])
+                       query_compute=numeric_compare)
 Integer = new_constant('Integer' , staticmethod(int_conv), 0, 
                        staticmethod(lambda x: isinstance(x, (int, long))),
                        base_class=Float,
-                       query_widget_type=('vistrails.gui.modules.query_configuration',
-                                          'NumericQueryWidget'),
-                       query_compute=numeric_compare,
-                       param_explore_widget_list=[('vistrails.gui.modules.paramexplore',
-                                                   'IntegerExploreWidget')])
+                       query_compute=numeric_compare)
 String  = new_constant('String'  , staticmethod(str), "", 
                        staticmethod(lambda x: isinstance(x, str)),
-                       query_widget_type=('vistrails.gui.modules.query_configuration',
-                                          'StringQueryWidget'),
-                       query_compute=string_compare,
-                       widget_types_list=[('multiline', ('vistrails.gui.modules.constant_configuration', 'MultiLineStringWidget'))])
+                       query_compute=string_compare)
 
 ##############################################################################
 
@@ -359,11 +309,6 @@ class Path(Constant):
 #         self.setResult("isfile", os.path.isfile(n))
 #         self.setResult("isdir", os.path.isdir(n))
         
-    @staticmethod
-    def get_widget_class():
-        return ("vistrails.gui.modules.constant_configuration", 
-                "PathChooserWidget")
-
 Path.default_value = Path()
 
 class File(Path):
@@ -389,11 +334,6 @@ class File(Path):
         self.set_results(n)
         self.setResult("local_filename", n)
         self.setResult("self", self)
-
-    @staticmethod
-    def get_widget_class():
-        return ("vistrails.gui.modules.constant_configuration", 
-                "FileChooserWidget")
 
 File.default_value = File()
     
@@ -437,11 +377,6 @@ class Directory(Path):
                 output_list.append(dir_item)
         self.setResult('itemList', output_list)
             
-    @staticmethod
-    def get_widget_class():
-        return ("vistrails.gui.modules.constant_configuration", 
-                "DirectoryChooserWidget")
-
 Directory.default_value = Directory()
 
 def path_parameter_hasher(p):
@@ -488,11 +423,6 @@ class OutputPath(Path):
         n = self.get_name()
         self.set_results(n)
         
-    @staticmethod
-    def get_widget_class():
-        return ("vistrails.gui.modules.constant_configuration", 
-                "OutputPathChooserWidget")
-
 OutputPath.default_value = OutputPath()
 
 class FileSink(NotCacheable, Module):
@@ -623,27 +553,7 @@ class Color(Constant):
 
     @staticmethod
     def to_string(r, g, b):
-        return "%s,%s,%s" % (r,g,b)
-
-    @staticmethod
-    def get_widget_class():
-        return ("vistrails.gui.modules.constant_configuration", "ColorWidget")
-
-    @staticmethod
-    def get_widget_class_of_type(widget_type):
-        if widget_type.startswith('enum'):
-            return ("vistrails.gui.modules.constant_configuration", 
-                    "ColorEnumWidget")
-        return None
-        
-    @staticmethod
-    def get_query_widget_class():
-        return ("vistrails.gui.modules.query_configuration", "ColorQueryWidget")
-
-    @staticmethod
-    def get_param_explore_widget_list():
-        return [('vistrails.gui.modules.paramexplore', 'RGBExploreWidget'),
-                ('vistrails.gui.modules.paramexplore', 'HSVExploreWidget')]
+        return "%s,%s,%s" % (r,g,b)        
 
     @staticmethod
     def query_compute(value_a, value_b, query_method):
@@ -1174,8 +1084,12 @@ def init_constant(m):
     reg.add_module(m)
     reg.add_input_port(m, "value", m)
     reg.add_output_port(m, "value", m)
-    
+
 def initialize(*args, **kwargs):
+    constant_config_path = "vistrails.gui.modules.constant_configuration"
+    query_config_path = "vistrails.gui.modules.query_configuration"
+    paramexp_config_path = "vistrails.gui.modules.paramexplore"
+
     reg = get_module_registry()
 
     # !!! is_root should only be set for Module !!!
@@ -1188,42 +1102,67 @@ def initialize(*args, **kwargs):
 
     reg.add_module(Constant, abstract=True)
 
-    reg.add_module(Boolean)
-    reg.add_module(Float)
-    reg.add_module(Integer)
+    reg.add_module(Boolean,
+                   constantWidget=('%s:BooleanWidget' % \
+                                   constant_config_path))
+    reg.add_module(Float,
+                   constantWidgets=[
+                       QueryWidgetConfig('%s:NumericQueryWidget' % \
+                                            query_config_path),
+                       ParamExpWidgetConfig('%s:FloatExploreWidget' % \
+                                            paramexp_config_path)])
+    reg.add_module(Integer,
+                   constantWidgets=[
+                       QueryWidgetConfig('%s:NumericQueryWidget' % \
+                                         query_config_path),
+                       ParamExpWidgetConfig('%s:IntegerExploreWidget' % \
+                                            paramexp_config_path)])
+
     reg.add_module(String,
-                   configureWidgetType=("vistrails.gui.modules.string_configure",
-                                        "TextConfigurationWidget"))
+                   configureWidgetType="vistrails.gui.modules.string_configure:TextConfigurationWidget",
+                   constantWidgets=[
+                       ConstantWidgetConfig('%s:MultiLineStringWidget' % \
+                                            constant_config_path, 
+                                            widget_type='multiline'),
+                       QueryWidgetConfig('%s:StringQueryWidget' % \
+                                         query_config_path)])
     
     reg.add_output_port(Constant, "value_as_string", String)
     reg.add_output_port(String, "value_as_string", String, True)
 
-    reg.add_module(List,
+    reg.add_module(List, 
                    configureWidgetType=(
-                           "vistrails.gui.modules.list_configuration",
-                           "ListConfigurationWidget"))
+            "vistrails.gui.modules.list_configuration:ListConfigurationWidget"))
     reg.add_input_port(List, "head", Module)
     reg.add_input_port(List, "tail", List)
 
-    reg.add_module(Path)
+    reg.add_module(Path,
+                   constantWidget=("%s:PathChooserWidget" % \
+                                   constant_config_path))
     reg.add_input_port(Path, "value", Path)
     reg.add_output_port(Path, "value", Path)
     reg.add_input_port(Path, "name", String, True)
 
-    reg.add_module(File, constantSignatureCallable=path_parameter_hasher)
+    reg.add_module(File, constantSignatureCallable=path_parameter_hasher,
+                   constantWidget=("%s:FileChooserWidget" % \
+                                   constant_config_path))
     reg.add_input_port(File, "value", File)
     reg.add_output_port(File, "value", File)
     reg.add_output_port(File, "self", File, True)
     reg.add_input_port(File, "create_file", Boolean, True)
     reg.add_output_port(File, "local_filename", String, True)
 
-    reg.add_module(Directory, constantSignatureCallable=path_parameter_hasher)
+    reg.add_module(Directory, constantSignatureCallable=path_parameter_hasher,
+                   constantWidget=("%s:DirectoryChooserWidget" % \
+                                   constant_config_path))
     reg.add_input_port(Directory, "value", Directory)
     reg.add_output_port(Directory, "value", Directory)
     reg.add_output_port(Directory, "itemList", List)
     reg.add_input_port(Directory, "create_directory", Boolean, True)
 
-    reg.add_module(OutputPath)
+    reg.add_module(OutputPath,
+                   constantWidget=("%s:OutputPathChooserWidget" % \
+                                   constant_config_path))
     reg.add_output_port(OutputPath, "value", OutputPath)
 
     reg.add_module(FileSink)
@@ -1244,7 +1183,20 @@ def initialize(*args, **kwargs):
     reg.add_input_port(WriteFile, 'suffix', String, True, defaults='[""]')
     reg.add_output_port(WriteFile, 'out_value', File)
 
-    reg.add_module(Color)
+    reg.add_module(Color,
+                   constantWidgets=[
+                       '%s:ColorWidget' % constant_config_path, 
+                       ConstantWidgetConfig('%s:ColorEnumWidget' % \
+                                            constant_config_path, 
+                                            widget_type='enum'),
+                       QueryWidgetConfig('%s:ColorQueryWidget' % \
+                                            query_config_path),
+                       ParamExpWidgetConfig('%s:RGBExploreWidget' % \
+                                            paramexp_config_path,
+                                            widget_type='rgb'),
+                       ParamExpWidgetConfig('%s:HSVExploreWidget' % \
+                                            paramexp_config_path,
+                                            widget_type='hsv')])
     reg.add_input_port(Color, "value", Color)
     reg.add_output_port(Color, "value", Color)
 
@@ -1252,13 +1204,11 @@ def initialize(*args, **kwargs):
     reg.add_input_port(StandardOutput, "value", Module)
 
     reg.add_module(Tuple, 
-                   configureWidgetType=("vistrails.gui.modules.tuple_configuration",
-                                        "TupleConfigurationWidget"))
+                   configureWidgetType="vistrails.gui.modules.tuple_configuration:TupleConfigurationWidget")
     reg.add_output_port(Tuple, 'self', Tuple)
 
     reg.add_module(Untuple, 
-                   configureWidgetType=("vistrails.gui.modules.tuple_configuration",
-                                        "UntupleConfigurationWidget"))
+                   configureWidgetType="vistrails.gui.modules.tuple_configuration:UntupleConfigurationWidget")
     reg.add_input_port(Untuple, 'tuple', Tuple)
 
     reg.add_module(ConcatenateString)

vistrails/core/modules/config.py
@@ -33,4 +33,38 @@
 ##
 ###############################################################################
 
-pass
+from collections import namedtuple as _namedtuple
+
+def namedtuple(typename, field_names, default_values=None):
+    T = _namedtuple(typename, field_names)
+    if default_values is not None:
+        T.__new__.__defaults__ = default_values
+    return T
+
+def subnamedtuple(typename, cls, default_values=None):
+    return namedtuple(typename, cls._fields, default_values)
+
+ConstantWidgetConfig = namedtuple('ConstantWidgetConfig',
+                                  ['widget', 'widget_type', 'widget_use'],
+                                  (None, None))
+
+QueryWidgetConfig = subnamedtuple('QueryWidgetConfig', ConstantWidgetConfig, 
+                                  (None, 'query'))
+ParamExpWidgetConfig = subnamedtuple('ParamExpWidgetConfig', 
+                                     ConstantWidgetConfig, (None, 'paramexp'))
+
+# class QueryWidgetConfig(ConstantWidgetConfig):
+#     pass
+# QueryWidgetConfig.__new__.__defaults__ = (None, 'query')
+
+# class ParamExpWidgetConfig(ConstantWidgetConfig):
+#     pass
+# ParamExpWidgetConfig.__new__.__defaults__ = (None, 'paramexp')
+    
+# QueryWidgetConfig.= namedtuple('QueryWidgetConfig',
+#                                   ['widget', 'widget_type', 'widget_use'],
+#                                   (None, 'query'))
+
+# ParamExpWidgetConfig = namedtuple('ParamExpWidgetConfig',
+#                                   ['widget', 'widget_type', 'widget_use'],
+#                                   (None, 'paramexp'))

vistrails/core/modules/module_descriptor.py
@@ -138,6 +138,7 @@ class ModuleDescriptor(DBModuleDescriptor):
             self._widget_item = None
             self._is_hidden = False
             self._namespace_hidden = False
+            self._widget_classes = {}
             self.children = []
             # The ghost attributes represent the original values
             # for the descriptor of an upgraded package subworkflow
@@ -162,6 +163,8 @@ class ModuleDescriptor(DBModuleDescriptor):
             self._hasher_callable = other._hasher_callable
             self._widget_item = other._widget_item
             self._is_hidden = other._is_hidden
+            self._widget_classes = dict((k,copy.copy(v)) for k, v in \
+                                         other._widget_classes.iteritems())
             self._namespace_hidden = other._namespace_hidden
             self.ghost_identifier = other.ghost_identifier
             self.ghost_package_version = other.ghost_package_version
@@ -248,6 +251,26 @@ class ModuleDescriptor(DBModuleDescriptor):
 
     def configuration_widget(self):
         return self._configuration_widget
+
+    def set_constant_config_widget(self, widget_class, widget_use, 
+                                   widget_type):
+        if widget_use not in self._widget_classes:
+            self._widget_classes[widget_use] = {}
+        self._widget_classes[widget_use][widget_type] = widget_class
+
+    def has_constant_config_widget(self, widget_use, widget_type):
+        return widget_use in self._widget_classes and \
+            widget_type in self._widget_classes[widget_use]
+
+    def get_constant_config_widget(self, widget_use, widget_type):
+        if self.has_constant_config_widget(widget_use, widget_type):
+            return self._widget_classes[widget_use][widget_type]
+        return None
+
+    def get_all_constant_config_widgets(self, widget_use):
+        if widget_use in self._widget_classes:
+            return self._widget_classes[widget_use]
+        return {}
 
     def set_module_color(self, color):
         if color:

vistrails/core/modules/module_registry.py
@@ -42,6 +42,7 @@ import uuid
 from vistrails.core import debug, get_vistrails_application
 from vistrails.core.data_structures.graph import Graph
 import vistrails.core.modules
+from vistrails.core.modules.config import ConstantWidgetConfig
 import vistrails.core.modules.vistrails_module
 from vistrails.core.modules.module_descriptor import ModuleDescriptor
 from vistrails.core.modules.package import Package
@@ -1030,6 +1031,7 @@ class ModuleRegistry(DBRegistry):
         kwargs:
           name=None,
           configureWidgetType=None,
+          constantWidget(s)=None,
           signatureCallable=None,
           moduleColor=None,
           moduleFringe=None,
@@ -1090,6 +1092,14 @@ class ModuleRegistry(DBRegistry):
         the given constant.  If this is not None, then the added
         module must be a subclass of Constant.
 
+        If constantWidget(s) is not None, the registry will use the
+        specified widget(s) or import path string(s) of the form
+        "<module>:<class>" to import the widget.  A tuple allows a
+        user to specify the widget_type (e.g. "default", "enum", etc.)
+        as the second item and the widget_use (e.g. "default",
+        "query", "paramexp").  If the plural form is used, we expect a
+        list, the singular form is used for a single item.
+
         If hide_namespace is True, the ModulePalette will not display
         the namespace for that module.  If hide_descriptor is True,
         the ModulePalette will not display that module in its list
@@ -1132,6 +1142,8 @@ class ModuleRegistry(DBRegistry):
             return r
         name = fetch('name', module.__name__)
         configureWidgetType = fetch('configureWidgetType', None)
+        constantWidget = fetch('constantWidget', None)
+        constantWidgets = fetch('constantWidgets', None)
         signatureCallable = fetch('signatureCallable', None)
         constantSignatureCallable = fetch('constantSignatureCallable', None)
         moduleColor = fetch('moduleColor', None)
@@ -1211,6 +1223,35 @@ class ModuleRegistry(DBRegistry):
             # FIXME, currently only allow one per hash, no versioning
             hash_key = (identifier, name, namespace)
             self._constant_hasher_map[hash_key] = constantSignatureCallable
+        
+        if constantWidget:
+            if constantWidgets is not None:
+                constantWidgets = constantWidget + constantWidgets
+            else:
+                constantWidgets = [constantWidget]
+        if constantWidgets:
+            try:
+                basic_pkg = get_vistrails_basic_pkg_id()
+                c = self.get_descriptor_by_name(basic_pkg, 'Constant').module
+            except ModuleRegistryException:
+                msg = "Constant not found - can't set constantSignatureCallable"
+                raise VistrailsInternalError(msg)
+            if not issubclass(module, c):
+                raise TypeError("To set constantWidgets, module " +
+                                "must be a subclass of Constant")
+            for widget_t in constantWidgets:
+                if isinstance(widget_t, tuple):
+                    widget_t = ConstantWidgetConfig(*widget_t)
+                else:
+                    widget_t = ConstantWidgetConfig(widget_t)
+                if widget_t.widget is not None:
+                    print 'setting widget:', widget_t.widget, \
+                        widget_t.widget_type, widget_t.widget_use
+                    self.set_constant_config_widget(descriptor, 
+                                                    widget_t.widget,
+                                                    widget_t.widget_type, 
+                                                    widget_t.widget_use)
+
         descriptor.set_module_color(moduleColor)
 
         if moduleFringe:
@@ -1904,12 +1945,52 @@ class ModuleRegistry(DBRegistry):
 
     def get_configuration_widget(self, identifier, name, namespace):
         descriptor = self.get_descriptor_by_name(identifier, name, namespace)
-        klass = descriptor.configuration_widget()
-        if isinstance(klass, tuple):
-            (path, klass_name) = klass
-            module = __import__(path, globals(), locals(), [klass_name])
-            klass = getattr(module, klass_name)            
-        return klass
+        cls = descriptor.configuration_widget()
+        path = None
+        if isinstance(cls, tuple):
+            (path, cls_name) = cls
+        elif isinstance(cls, basestring):
+            [path, cls_name] = cls.split(':')[:2]
+        if path is not None:
+            module = __import__(path, globals(), locals(), [cls_name])
+            cls = getattr(module, cls_name)            
+        return cls
+
+    def get_constant_config_params(self, widget_type=None, widget_use=None):
+        if widget_type is None:
+            widget_type = 'default'
+        if widget_use is None:
+            widget_use = 'default'
+        return (widget_type, widget_use)
+
+    def get_constant_config_widget(self, descriptor, widget_type=None, 
+                                   widget_use=None):
+        widget_type, widget_use = self.get_constant_config_params(widget_type,
+                                                                  widget_use)
+        for desc in self.get_module_hierarchy(descriptor):
+            if desc.has_constant_config_widget(widget_use, widget_type):
+                return desc.get_constant_config_widget(widget_use, widget_type)
+        return None
+
+    def get_all_constant_config_widgets(self, descriptor, widget_use=None):
+        widget_use = self.get_constant_config_params(None, widget_use)[1]
+        widgets = {}
+        for desc in reversed(self.get_module_hierarchy(descriptor)):
+            widgets.update(desc.get_all_constant_config_widgets(widget_use))
+        return widgets.values()
+
+    def set_constant_config_widget(self, descriptor, widget_class, 
+                                   widget_type=None, widget_use=None):
+        widget_type, widget_use = self.get_constant_config_params(widget_type,
+                                                                  widget_use)
+        basic_pkg = get_vistrails_basic_pkg_id()
+        constant_desc = self.get_descriptor_by_name(basic_pkg, 'Constant')
+        if not self.is_descriptor_subclass(descriptor, constant_desc):
+            raise Exception('Descriptor "%s" must be a subclass of Constant '
+                            'to use a constant configuration widget.' % \
+                            descriptor.sigstring)
+        descriptor.set_constant_config_widget(widget_class,
+                                              widget_use, widget_type)
 
     def is_descriptor_subclass(self, sub, super):
         """is_descriptor_subclass(sub : ModuleDescriptor, 

vistrails/gui/mashups/alias_inspector.py
@@ -51,7 +51,7 @@ from PyQt4.QtCore import pyqtSignal, pyqtSlot
 from vistrails.core.mashup.alias import Alias
 from vistrails.core.modules.module_registry import get_module_registry
 from vistrails.core.system import get_vistrails_basic_pkg_id
-from vistrails.gui.modules import get_widget_class
+from vistrails.gui.modules.utils import get_widget_class
 from vistrails.gui.modules.constant_configuration import StandardConstantWidget
 from vistrails.gui.theme import CurrentTheme
 from vistrails.gui.utils import show_warning

vistrails/gui/mashups/mashups_widgets.py
@@ -35,7 +35,7 @@
 from PyQt4 import QtCore, QtGui
 from vistrails.core.system import get_vistrails_basic_pkg_id
 from vistrails.gui.theme import CurrentTheme
-from vistrails.gui.modules import get_widget_class
+from vistrails.gui.modules.utils import get_widget_class
 from vistrails.gui.modules.constant_configuration import ConstantWidgetMixin, \
     StandardConstantWidget
 from vistrails.core.modules.module_registry import get_module_registry

commit e396349ec37c07ec02e95995c898833d50699f74

Fixed broken vistrail variables dropbox
Now updated to use the syntax introduced by commit 2674fe23f8e6.

gui/variable_dropbox.py:
  get_widget_class() now takes a descriptor as argument

vistrails/gui/variable_dropbox.py
@@ -338,7 +338,7 @@ class QVariableInputForm(QtGui.QGroupBox):
         p = ModuleParam(type=descriptor.name, identifier=descriptor.identifier,
                         namespace=descriptor.namespace)
         p.strValue = var_strValue
-        widget_type = get_widget_class(descriptor.module)
+        widget_type = get_widget_class(descriptor)
         self.widget = widget_type(p, self)
         self.label = QDragVariableLabel(p.type)
         self.layout().addWidget(self.label, 0, 0)