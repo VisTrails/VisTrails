64a775988587c927aaa3dab6eb02bed88b68f7e8
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2014-03-28 22:55:43 +0100

    Added annotations and control parameters to visual diff
    Handle annotations and control parameters in the visual diff computations.
    We could consider making the annotations optional, since it is a non-functional
    part of the module.
    
    core/db/io.py:
    Forward new diff parameters
    
    core/vistrail/vistrail.py:
    Forward new visual diff parameters
    Also fixed and added test for returning connections with diff
    
    db/services/vistrail.py:
    Added cparams and annotations when computing the diff
    
    gui/vis_diff.py:
    Added tables for cparams and annotations

--- vistrails/core/db/io.py
+++ vistrails/core/db/io.py
@@ -125,12 +125,14 @@ def get_workflow_diff(vt_pair_1, vt_pair_2):
     """
 
     from vistrails.core.vistrail.pipeline import Pipeline
-    (v1, v2, pairs, heuristic_pairs, v1_only, v2_only, param_changes, \
-         _, _, _, _) = \
-         vistrails.db.services.vistrail.getWorkflowDiff(vt_pair_1, vt_pair_2, True)
+    (v1, v2, pairs, heuristic_pairs, v1_only, v2_only, param_changes,
+     cparam_changes, annot_changes, _, _, _, _) = \
+         vistrails.db.services.vistrail.getWorkflowDiff(vt_pair_1, vt_pair_2,
+                                                        True)
     Pipeline.convert(v1)
     Pipeline.convert(v2)
-    return (v1, v2, pairs, heuristic_pairs, v1_only, v2_only, param_changes)
+    return (v1, v2, pairs, heuristic_pairs, v1_only, v2_only, param_changes,
+            cparam_changes, annot_changes)
 
 def get_workflow_diff_with_connections(vt_pair_1, vt_pair_2):
     """get_workflow_diff_with_connections
@@ -139,13 +141,15 @@ def get_workflow_diff_with_connections(vt_pair_1, vt_pair_2):
     """
 
     from vistrails.core.vistrail.pipeline import Pipeline
-    (v1, v2, m_pairs, m_heuristic, v1_only, v2_only, param_changes, \
-         c_pairs, c_heuristic, c1_only, c2_only) = \
-         vistrails.db.services.vistrail.getWorkflowDiff(vt_pair_1, vt_pair_2, False)
+    (v1, v2, m_pairs, m_heuristic, v1_only, v2_only, param_changes,
+     cparam_changes, annot_changes, c_pairs, c_heuristic, c1_only, c2_only) =\
+         vistrails.db.services.vistrail.getWorkflowDiff(vt_pair_1, vt_pair_2,
+                                                        False)
     Pipeline.convert(v1)
     Pipeline.convert(v2)
-    return (v1, v2, m_pairs, m_heustric, v1_only, v2_only, param_changes,
-            c_pairs, c_heuristic, c1_only, c2_only)
+    return (v1, v2, m_pairs, m_heuristic, v1_only, v2_only, param_changes,
+            cparam_changes, annot_changes, c_pairs, c_heuristic, c1_only,
+            c2_only)
 
 def getPathAsAction(vt, v1, v2, do_copy=False):
     a = vistrails.db.services.vistrail.getPathAsAction(vt, v1, v2, do_copy)
