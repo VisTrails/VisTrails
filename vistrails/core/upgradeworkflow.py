984a1b38aaf97d5a432cfe27ff2dc3ae13075ad8
Remi Rampin <remirampin@gmail.com>
2014-04-22 13:02:29 -0400

    Fixes testsuite failure because of upgrade package
    Not sure why that fixes it, but git-bisect told me
    792611a1413c062e055ecad5aaa8e294df92551f was the cause, so here goes.
    Issue #845 still needs a real fix.

--- vistrails/core/upgradeworkflow.py
+++ vistrails/core/upgradeworkflow.py
@@ -815,6 +815,7 @@ class TestUpgradePackageRemap(unittest.TestCase):
         from vistrails.core.application import get_vistrails_application
 
         app = get_vistrails_application()
+        app.new_vistrail()
 
         try:
             pm = get_package_manager()
@@ -883,6 +884,7 @@ class TestUpgradePackageRemap(unittest.TestCase):
         from vistrails.core.application import get_vistrails_application
 
         app = get_vistrails_application()
+        app.new_vistrail()
         default_upgrade_on = app.temp_configuration.upgradeOn
         default_upgrade_delay = app.temp_configuration.upgradeDelay
         app.temp_configuration.upgradeOn = True
