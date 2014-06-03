a7e5a736abbb9adaf4ba14de3d0a77570061035f
David Koop <dakoop@nyu.edu>
2014-06-02 20:32:52 -0700

    Enable translation from 0.2 to 0.3 package
    Showcases need for more high-level upgrade mechanisms than simple
    module_remap.  Also adds marker title support and provides a legacy
    mode for GMapSymbols.

--- vistrails/packages/gmaps/vis.py
+++ vistrails/packages/gmaps/vis.py
@@ -27,6 +27,29 @@ def convert_color(c):
     c = c.tuple
     return GMapColor(c[0] * 255.0, c[1] * 255.0, c[2] * 255.0)
 
+class TitlesMixin(object):
+    def get_titles(self, table=None, default_col=None):
+        if table is None:
+            table = self.get_input("table")
+        title_col_idx = self.force_get_input('titleColIdx')
+        title_col_name = self.force_get_input('titleColName')
+        print "title_col_idx:", title_col_idx
+        print "title_col_name:", title_col_name
+
+        if (title_col_idx is None and
+                title_col_name is None and 
+                default_col is not None and
+                default_col < table.columns):
+                # only allow default if in range
+                title_col_idx = default_col
+
+        if title_col_idx is not None or title_col_name is not None:
+            title_idx = choose_column(table.columns, table.names, 
+                                      title_col_name, title_col_idx)
+            title_col = table.get_column(title_idx)
+            return title_col
+        return None
+
 class GMapVis(Module, OptionsMixin):
     _input_ports = [IPort('table', 'tabledata:Table'),
                     IPort('latitudeColIdx', 'basic:Integer', optional=True,
@@ -42,14 +65,20 @@ class GMapVis(Module, OptionsMixin):
         if table is None:
             table = self.get_input("table")
         
-        lat_col_idx = self.get_input('latitudeColIdx') # default 0
+        lat_col_idx = self.force_get_input('latitudeColIdx')
         lat_col_name = self.force_get_input('latitudeColName')
-        lng_col_idx = self.get_input('longitudeColIdx') # default 1
+        lng_col_idx = self.force_get_input('longitudeColIdx')
         lng_col_name = self.force_get_input('longitudeColName')
-        lat_idx = choose_column(table.columns, table.names, 
-                                lat_col_name, lat_col_idx)
-        lng_idx = choose_column(table.columns, table.names, 
-                                lng_col_name, lng_col_idx)
+        if lat_col_idx is None and lat_col_name is None:
+            lat_idx = self.get_input('latitudeColIdx') # default 0
+        else:
+            lat_idx = choose_column(table.columns, table.names, 
+                                    lat_col_name, lat_col_idx)
+        if lng_col_idx is None and lng_col_name is None:
+            lng_idx = self.get_input('longitudeColIdx') # default 1
+        else:
+            lng_idx = choose_column(table.columns, table.names, 
+                                    lng_col_name, lng_col_idx)
         lat_col = table.get_column(lat_idx, True)
         lng_col = table.get_column(lng_idx, True)
         center = (sum(float(x) for x in lat_col)/len(lat_col),
@@ -59,25 +88,34 @@ class GMapVis(Module, OptionsMixin):
             positions.append(GMapLatLng(lat_col[i], lng_col[i]))
         return (positions, center)
 
-class GMapMarkers(GMapVis):
+class GMapMarkers(GMapVis, TitlesMixin):
     TEMPLATE = Template("""
   var positions = $marker_data;
   var options = $marker_options;
+  var titles = $marker_titles;
 
   for (var i=0; i < positions.length; i++) {
     marker = new google.maps.Marker({"position": positions[i],
                                       "map": map});
     marker.setOptions(options);
+    if (titles) {
+      marker.setTitle(titles[i]);
+    }
   }
 """)
     SPECS = ['flat']
-    _input_ports = [IPort("flat", "basic:Boolean", optional=True)]
+    _input_ports = [IPort("flat", "basic:Boolean", optional=True),
+                    IPort('titleColIdx', 'basic:Integer', optional=True),
+                    IPort('titleColName', 'basic:String', optional=True)]
     
     def compute(self):
         (positions, center) = self.get_positions()
         marker_options = self.get_options(self.SPECS)
+        titles = self.get_titles()
+        print "got titles:", titles
         data = {"marker_options": marker_options,
-                "marker_data": positions}
+                "marker_data": positions,
+                "marker_titles": titles}
         
         vis_data = GMapVisData([], self.TEMPLATE, data, center)
         self.set_output("self", vis_data)
@@ -85,14 +123,16 @@ class GMapMarkers(GMapVis):
 class GMapValueVis(GMapVis):
     _input_ports = [IPort('valueColIdx', 'basic:Integer', optional=True,
                           default=2),
-                    IPort('valueColName', 'basic:String', optional=True)]
+                    IPort('valueColName', 'basic:String', optional=True),]
     _settings = ModuleSettings(abstract=True)
     def get_values(self, table=None):
         if table is None:
             table = self.get_input("table")
-        value_col_idx = self.get_input("valueColIdx") # default 2
+        value_col_idx = self.force_get_input("valueColIdx")
         value_col_name = self.force_get_input("valueColName")
-        if value_col_idx is not None or value_col_name is not None:
+        if value_col_idx is None and value_col_name is None:
+            value_idx = self.get_input("valueColIdx") # default 2
+        else:
             value_idx = choose_column(table.columns, table.names, 
                                       value_col_name, value_col_idx)
         value_col = table.get_column(value_idx, True)
@@ -127,7 +167,7 @@ class GMapCircles(GMapValueVis):
     def compute(self):
         (positions, center) = self.get_positions()
         values = self.get_values()
-        circle_data = [[positions[i], float(values[i])/200.0] 
+        circle_data = [[positions[i], float(values[i])/200.0]
                        for i in xrange(len(positions))]
         circle_options = self.get_options(self.SPECS)
         data = {"circle_options": circle_options,
@@ -135,29 +175,37 @@ class GMapCircles(GMapValueVis):
         vis_data = GMapVisData([], self.TEMPLATE, data, center)
         self.set_output("self", vis_data)
 
-class GMapSymbols(GMapValueVis):
+class GMapSymbols(GMapValueVis, TitlesMixin):
     TEMPLATE = Template("""
   var data = $symbol_data;
+  var titles = $symbol_titles;
 
   for (var i=0; i < data.length; i++) {
-    var options = $symbol_options;
-    options["fillColor"] = data[i][1];
-    marker = new google.maps.Marker({"position": data[i][0],
-                                     "map": map,
-                                     "icon": options});
+    var marker_options = {"position": data[i][0],
+                          "map": map};
+    if (titles) {
+      marker_options["title"] = titles[i];
+    }
+    if ($use_values) {
+      var icon_options = $symbol_options;
+      icon_options["fillColor"] = data[i][1];
+      marker_options["icon"] = icon_options;
+    }
+    marker = new google.maps.Marker(marker_options);
   }  
 """)
 
     SPECS = [('strokeColor', convert_color, True),
              ('fillStartColor', None, True),
              ('fillEndColor', None, True),
-             'strokeWeight', 
+             ('strokeWeight', None, True),
              'strokeOpacity',
              ('fillOpacity', None, True),
-             'scale']
+             ('scale', None, True)]
     _input_ports = [IPort("strokeColor", "basic:Color", optional=True,
                           default=InstanceObject(tuple=(0,0,0))),
-                    IPort("strokeWeight", "basic:Integer", optional=True),
+                    IPort("strokeWeight", "basic:Integer", optional=True,
+                          default=1),
                     IPort("strokeOpacity", "basic:Float", optional=True),
                     IPort("fillStartColor", "basic:Color", optional=True,
                           default=InstanceObject(tuple=(1,1,1))),
@@ -165,47 +213,71 @@ class GMapSymbols(GMapValueVis):
                           default=InstanceObject(tuple=(1,0,0))),
                     IPort("fillOpacity", "basic:Float", optional=True,
                           default=1.0),
-                    IPort("scale", "basic:Float", optional=True)]
+                    IPort("scale", "basic:Float", optional=True,
+                          default=5.0),
+                    IPort('titleColIdx', 'basic:Integer', optional=True),
+                    IPort('titleColName', 'basic:String', optional=True),
+                    IPort("allowLegacy", "basic:Boolean", optional=True,
+                          default=False)]
 
     def compute(self):
         (positions, center) = self.get_positions()
-        values = [float(x) for x in self.get_values()]
-        symbol_options = self.get_options(self.SPECS)
-        symbol_options["path"] = \
+        legacy = self.get_input("allowLegacy")
+        use_values = True
+        try:
+            values = [float(x) for x in self.get_values()]
+        except ValueError, e:
+            # LEGACY SUPPORT
+            if legacy:
+                use_values = False
+            else:
+                raise ModuleError(self, "Must provide values column")
+        if not use_values:
+            symbol_data = positions
+            symbol_options = {}
+        else:
+            symbol_options = self.get_options(self.SPECS)
+            symbol_options["path"] = \
                             RawJavaScriptText("google.maps.SymbolPath.CIRCLE")
-        min_value = min(values)
-        max_value = max(values)
+            min_value = min(values)
+            max_value = max(values)
 
-        # if we have black or white, we want hue to match the other side
-        def white_or_black(c):
-            return ((c[0] < 1e-8 and c[1] < 1e-8 and c[2] < 1e-8) or
-                    (c[0] > 1-1e-8 and c[1] > 1-1e-8 and c[2] > 1-1e-8))
-        start_c = symbol_options.pop("fillStartColor").tuple
-        end_c = symbol_options.pop("fillEndColor").tuple
-        start_wb = white_or_black(start_c)
-        end_wb = white_or_black(end_c)
-        start_c = list(colorsys.rgb_to_hsv(*start_c))
-        end_c = list(colorsys.rgb_to_hsv(*end_c))
-        if start_wb:
-            start_c[0] = end_c[0]
-        elif end_wb:
-            end_c[0] = start_c[0]
-        
-        symbol_data = []
-        for i in xrange(len(positions)):
-            val = values[i]
-            norm_val = (val - min_value) / (max_value - min_value)
-            color = []
-            for j in xrange(len(start_c)):
-                color.append((1.0 - norm_val) * start_c[j] + 
-                             norm_val * end_c[j])
-            color = colorsys.hsv_to_rgb(*color)
-            symbol_data.append([positions[i],
-                                GMapColor(255 * color[0],
-                                          255 * color[1],
-                                          255 * color[2])])
+            # if we have black or white, we want hue to match the other side
+            def white_or_black(c):
+                return ((c[0] < 1e-8 and c[1] < 1e-8 and c[2] < 1e-8) or
+                        (c[0] > 1-1e-8 and c[1] > 1-1e-8 and c[2] > 1-1e-8))
+            start_c = symbol_options.pop("fillStartColor").tuple
+            end_c = symbol_options.pop("fillEndColor").tuple
+            start_wb = white_or_black(start_c)
+            end_wb = white_or_black(end_c)
+            start_c = list(colorsys.rgb_to_hsv(*start_c))
+            end_c = list(colorsys.rgb_to_hsv(*end_c))
+            if start_wb:
+                start_c[0] = end_c[0]
+            elif end_wb:
+                end_c[0] = start_c[0]
+
+            symbol_data = []
+            for i in xrange(len(positions)):
+                val = values[i]
+                if max_value - min_value < 1e-8:
+                    norm_val = 1.0
+                else:
+                    norm_val = (val - min_value) / (max_value - min_value)
+                color = []
+                for j in xrange(len(start_c)):
+                    color.append((1.0 - norm_val) * start_c[j] + 
+                                 norm_val * end_c[j])
+                color = colorsys.hsv_to_rgb(*color)
+                symbol_data.append([positions[i],
+                                    GMapColor(255 * color[0],
+                                              255 * color[1],
+                                              255 * color[2])])
+        symbol_titles = self.get_titles(default_col=(3 if legacy else None))
         data = {"symbol_data": symbol_data,
-                "symbol_options": symbol_options}
+                "symbol_options": symbol_options,
+                "symbol_titles": symbol_titles,
+                "use_values": use_values}
         vis_data = GMapVisData([], self.TEMPLATE, data, center)
         self.set_output("self", vis_data)
 
