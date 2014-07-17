95caab6e05e516f339685e67ee84281bc097f9a8
Remi Rampin <remirampin@gmail.com>
2014-07-17 15:33:39 -0400

    Starts work on preludes

--- vistrails/core/scripting.py
+++ vistrails/core/scripting.py
@@ -50,21 +50,22 @@ def write_workflow_to_python(pipeline, filename):
     text = []
     all_vars = set()
     modules = dict()
+    preludes = []
 
     reg = get_module_registry()
 
-    # Walk through the pipeline
+    # ########################################
+    # Walk through the pipeline to get all the codes
+    #
     for module_id in pipeline.graph.vertices_topological_sort():
         module = pipeline.modules[module_id]
-        print("Writing module %s %d" % (module.name, module_id))
-
-        # Annotation, used to rebuild the pipeline
-        text.append("# MODULE name=%r, id=%s\n" % (module.name, module_id))
+        print("Processing module %s %d" % (module.name, module_id))
 
         desc = module.module_descriptor
         module_class = desc.module
 
         # Gets the code
+        code_preludes = []
         if not hasattr(module_class, 'to_python_script'):
             debug.critical("Module %s cannot be converted to Python")
             code = ("# <Missing code>\n"
@@ -85,7 +86,42 @@ def write_workflow_to_python(pipeline, filename):
             # Call the module to get the base code
             code = module_class.to_python_script(module,
                                                  input_vars, output_vars)
+            if isinstance(code, tuple):
+                code, code_preludes = code
             print("Got code:\n%r" % (code,))
+
+        modules[module_id] = code
+        preludes.extend(code_preludes)
+
+    # ########################################
+    # Processes the preludes and writes the beginning of the file
+    #
+    # Adds all imported modules to the list of symbols
+    for prelude in preludes:
+        all_vars.update(prelude.imported_pkgs)
+
+    # Removes collisions
+    prelude_renames = {}
+    for prelude in preludes:
+        prelude_renames.update(prelude.avoid_collisions(all_vars))
+
+    # Writes the preludes
+    for prelude in preludes:
+        text.append('# Prelude')
+        text.append(unicode(prelude))
+        text.append('')
+
+    # ########################################
+    # Walk through the pipeline a second time to generate the full script
+    #
+    for module_id in pipeline.graph.vertices_topological_sort():
+        module = pipeline.modules[module_id]
+        print("Writing module %s %d" % (module.name, module_id))
+
+        # Annotation, used to rebuild the pipeline
+        text.append("# MODULE name=%r, id=%s" % (module.name, module_id))
+
+        code = modules[module_id]
 
         old_all_vars = set(all_vars)
         if isinstance(code, basestring):
@@ -155,16 +191,118 @@ def write_workflow_to_python(pipeline, filename):
         text.append(unicode(code))
         print("Total new vars: %r" % (all_vars - old_all_vars,))
 
-        # Stores this for connections to downstream modules
-        modules[module_id] = code
-
     print("Writing to file")
     f = io.open(filename, 'w', encoding='utf-8')
     f.write('\n'.join(text))
+    f.write('\n')
     f.close()
 
 
-class Script(object):
+class BaseScript(object):
+    def __init__(self, source):
+        if isinstance(source, RedBaron):
+            self.source = source
+        else:
+            self.source = RedBaron(utf8(source))
+        while self.source and self.source[0].Endl:
+            del self.source[0]
+        while self.source and self.source[-1].Endl:
+            del self.source[-1]
+
+    def rename(self, renames):
+        counts = {}
+        for node in self.source.find_all('NameNode'):
+            v = node.value
+            if v in renames:
+                node.value = renames[v]
+                counts[v] = counts.get(v, 0) + 1
+        if not counts:
+            print("rename():\n    no renames")
+        else:
+            print("rename():")
+            for v, c in counts.iteritems():
+                print("    %s: %d" % (v, c))
+
+    def __unicode__(self):
+        return self.source.dumps()
+
+
+class Prelude(BaseScript):
+    """A piece of code that should be inserted at the beginning.
+    """
+    # Defined variables are the symbols that are created by this prelude code
+    # The scripts that use this prelude will want to use these
+    # We rename them if they clash with other preludes; in that case we rename
+    # in the same way in each script that use the prelude
+    defined_vars = None
+    # Imported packages are symbols that are created by importing a package of
+    # the same name (top-level only)
+    # For example, 'import vistrails.api' creates a symbol 'vistrails'
+    # We don't rename these and it doesn't matter if they collide
+    imported_pkgs = None
+
+    def __init__(self, source, prelude_id=None):
+        BaseScript.__init__(self, source)
+        if prelude_id is None:
+            self.prelude_id = id(source)
+        else:
+            self.prelude_id = prelude_id
+
+        self.defined_vars = set()
+        self.imported_pkgs = set()
+        # Variable assignments
+        for assign in self.source.find_all('AssignmentNode'):
+            # a = 2 -> 'a' is a defined variable
+            if assign.target.NameNode:
+                self.defined_vars.add(assign.target.value)
+        # Imports
+        for imp in self.source.find_all('ImportNode'):
+            for v in imp.value:
+                if not v.dotted_as_name:
+                    continue
+                # import a.b as c -> 'c' is a defined variable
+                if v.target:
+                    self.defined_vars.add(v.target)
+                # import a.b -> 'b' is an imported package
+                else:
+                    self.imported_pkgs.add(v.value.value[0])
+        for imp in self.source.find_all('FromImportNode'):
+            for v in imp.targets:
+                # from a import b as c -> 'c' is a defined variable
+                if v.target:
+                    self.defined_vars.add(v.target)
+                # from a import b -> 'b' is a defined variable
+                else:
+                    self.defined_vars.add(v.value)
+
+    def avoid_collisions(self, all_vars):
+        """Renames the variables to avoid collisions.
+        """
+        renames = {}
+        for name in self.defined_vars:
+            if name in all_vars:
+                # Collision!
+                nname = make_unique(name, all_vars, self.defined_vars)
+                renames[name] = nname
+                self.defined_vars.discard(name)
+                self.defined_vars.add(nname)
+            else:
+                all_vars.add(name)
+        self.rename(renames)
+        return renames
+
+    def __hash__(self):
+        return hash(self.prelude_id)
+
+    def __eq__(self, other):
+        return (isinstance(other, Prelude) and
+                self.prelude_id == other.prelude_id)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Script(BaseScript):
     """Wrapper for a piece of Python script.
 
     This holds a piece of code plus information on how the inputs/outputs
@@ -181,10 +319,7 @@ class Script(object):
     internal_vars = None
 
     def __init__(self, source, inputs, outputs):
-        if isinstance(source, RedBaron):
-            self.source = source
-        else:
-            self.source = RedBaron(utf8(source))
+        BaseScript.__init__(self, source)
         self.inputs = inputs
         self.outputs = outputs
 
@@ -294,24 +429,7 @@ class Script(object):
     def get_output(self, port):
         return self.outputs[port]
 
-    def __unicode__(self):
-        return self.source.dumps()
-
     def __repr__(self):
         return "Script('''\n%s\n''',\ninputs: %r,\noutputs: %r\n)" % (
                 self.source.dumps(),
                 self.inputs, self.outputs)
-
-    def rename(self, renames):
-        counts = {}
-        for node in self.source.find_all('NameNode'):
-            v = node.value
-            if v in renames:
-                node.value = renames[v]
-                counts[v] = counts.get(v, 0) + 1
-        if not counts:
-            print("rename():\n    no renames")
-        else:
-            print("rename():")
-            for v, c in counts.iteritems():
-                print("    %s: %d" % (v, c))
