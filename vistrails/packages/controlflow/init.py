6dbef8723f03f2683567725ff59e0d31be4ce4d6
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2014-09-02 17:50:06 +0200

    Added missing Results port upgrade for Map and Filter
    They are now of type List but upgrade only existed for Map on master
    
    Bugfix: Map and Filter upgrades were missing

--- vistrails/packages/controlflow/init.py
+++ vistrails/packages/controlflow/init.py
@@ -210,6 +210,16 @@ def handle_module_upgrade_request(controller, module_id, pipeline):
                     },
                 }),
             ],
+            'Map': [
+                (None, '0.2.4', None, {
+                    'src_port_remap': {'Result': 'Result'}
+                }),
+            ],
+            'Filter': [
+                (None, '0.2.4', None, {
+                    'src_port_remap': {'Result': 'Result'}
+                }),
+            ],
         }
 
     return UpgradeWorkflowHandler.remap_module(controller,
