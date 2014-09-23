VisTrails/VisTrails

commit cccf158c3ff24813fd12d5197368207e06a319fc

Details to be added later

vistrails/db/services/locator.py
@@ -27,9 +27,11 @@ from db.services import io
 from db.services.io import SaveBundle
 from db.domain import DBVistrail
 import urllib
+import urlparse
+import cgi
 from db import VistrailsDBException
 from core import debug
-from core.system import get_elementtree_library
+from core.system import get_elementtree_library, systemType
 ElementTree = get_elementtree_library()
 import hashlib
 from time import strptime
@@ -71,6 +73,57 @@ class BaseLocator(object):
     def parse(element):
         pass #Parse an XML object representing a locator and returns a Locator
     
+    @staticmethod
+    def from_url(url):
+        # FIXME works only with absolute paths right now...
+        # FIXME are ? or / in filenames problematic?
+        if '://' in url:
+            scheme, rest = url.split('://', 1)
+        else:
+            scheme = 'file'
+            rest = url
+        if scheme == 'db':
+            return DBLocator.from_url(url)
+        elif scheme == 'file':
+            filename = urlparse.urlsplit('http://' + rest)[2]
+            if filename.endswith('.vt'):
+                return ZIPFileLocator.from_url(url)
+            elif filename.endswith('.xml'):
+                return XMLFileLocator.from_url(url)
+        return None
+
+    @staticmethod
+    def parse_args(arg_str):
+        args = {}
+        parsed_dict = cgi.parse_qs(arg_str)
+        if 'type' in parsed_dict:
+            args['obj_type'] = parsed_dict['type'][0]
+        if 'id' in parsed_dict:
+            args['obj_id'] = parsed_dict['id'][0]
+        args['version_node'] = None
+        args['version_tag'] = None
+        if 'workflow' in parsed_dict:
+            workflow_arg = parsed_dict['workflow'][0]
+            try:
+                args['version_node'] = int(workflow_arg)
+            except ValueError:
+                args['version_tag'] = workflow_arg
+        return args
+
+    @staticmethod
+    def generate_args(args):
+        generate_dict = {}
+        if 'obj_type' in args and args['obj_type']:
+            generate_dict['type'] = args['obj_type']
+        if 'obj_id' in args and args['obj_id']:
+            generate_dict['id'] = args['obj_id']
+        if 'version_node' in args and args['version_node']:
+            generate_dict['workflow'] = args['version_node']
+        elif 'version_tag' in args and args['version_tag']:
+            generate_dict['workflow'] = args['version_tag']
+        return urllib.urlencode(generate_dict)
+
+
     def _get_name(self):
         return None # Returns a name that will be displayed for the object
     name = property(_get_name)
@@ -89,15 +142,17 @@ class BaseLocator(object):
         pass # Implement nonequality
 
 class XMLFileLocator(BaseLocator):
-    def __init__(self, filename, version_node=None, version_tag=''):
+    def __init__(self, filename, **kwargs):
         self._name = filename
-        self._vnode = version_node
-        self._vtag = version_tag
+        self._vnode = kwargs.get('version_node', None)
+        self._vtag = kwargs.get('version_tag', '')
+     
         config = core.configuration.get_vistrails_configuration()
         if config:
             self._dot_vistrails = config.dotVistrails
         else:
             self._dot_vistrails = core.system.default_dot_vistrails()
+        self.kwargs = kwargs
 
     def load(self, type):
         fname = self._find_latest_temporary()
@@ -137,6 +192,26 @@ class XMLFileLocator(BaseLocator):
     def _get_short_name(self):
         return os.path.splitext(os.path.basename(self._name))[0]
     short_name = property(_get_short_name)
+
+    @staticmethod
+    def from_url(url):
+        if '://' in url:
+            scheme, rest = url.split('://', 1)
+        else:
+            scheme = 'file'
+            rest = url
+        url = 'http://' + rest
+        (_, _, path, args_str, _) = urlparse.urlsplit(url)
+        # "/c:/path" needs to be "c:/path"
+        if systemType in ['Windows', 'Microsoft']:
+            path = path[1:]
+        kwargs = BaseLocator.parse_args(args_str)
+        return XMLFileLocator(path, **kwargs)
+
+    def to_url(self):
+        args_str = BaseLocator.generate_args(self.kwargs)
+        url_tuple = ('file', '', os.path.abspath(self._name), args_str, '')
+        return urlparse.urlunsplit(url_tuple)
 
     def encode_name(self, filename):
         """encode_name(filename) -> str
@@ -281,9 +356,29 @@ class XMLFileLocator(BaseLocator):
 class ZIPFileLocator(XMLFileLocator):
     """Files are compressed in zip format. The temporaries are
     still in xml"""
-    def __init__(self, filename, version_node=None, version_tag=None):
-        XMLFileLocator.__init__(self, filename, version_node, version_tag)
+    def __init__(self, filename, **kwargs):
+        XMLFileLocator.__init__(self, filename, **kwargs)
         self.tmp_dir = None
+
+    @staticmethod
+    def from_url(url):
+        if '://' in url:
+            scheme, rest = url.split('://', 1)
+        else:
+            scheme = 'file'
+            rest = url
+        url = 'http://' + rest
+        (_, _, path, args_str, _) = urlparse.urlsplit(url)
+        # "/c:/path" needs to be "c:/path"
+        if systemType in ['Windows', 'Microsoft']:
+            path = path[1:]
+        kwargs = BaseLocator.parse_args(args_str)
+        return ZIPFileLocator(path, **kwargs)
+
+    def to_url(self):
+        args_str = BaseLocator.generate_args(self.kwargs)
+        url_tuple = ('file', '', os.path.abspath(self._name), args_str, '')
+        return urlparse.urlunsplit(url_tuple)
 
     def load(self, type):
         fname = self._find_latest_temporary()
@@ -376,20 +471,20 @@ class DBLocator(BaseLocator):
     cache_connections = {}
     
     def __init__(self, host, port, database, user, passwd, name=None,
-                 obj_id=None, obj_type=None, connection_id=None,
-                 version_node=None, version_tag=''):
+                 **kwargs):
         self._host = host
-        self._port = port
+        self._port = int(port)
         self._db = database
         self._user = user
         self._passwd = passwd
         self._name = name
-        self._obj_id = obj_id
-        self._obj_type = obj_type
-        self._conn_id = connection_id
-        self._vnode = version_node
-        self._vtag = version_tag
         self._hash = ''
+        self.kwargs = kwargs
+        self._obj_id = self.kwargs.get('obj_id', None)
+        self._obj_type = self.kwargs.get('obj_type', None)
+        self._conn_id = self.kwargs.get('connection_id', None)
+        self._vnode = self.kwargs.get('version_node', None)
+        self._vtag = self.kwargs.get('version_tag', None)
 
     def _get_host(self):
         return self._host

vistrails/core/db/locator.py
@@ -28,11 +28,28 @@ from core.system import vistrails_default_file_type, get_elementtree_library, \
 from core.external_connection import ExtConnectionList, DBConnection
 from core.thumbnails import ThumbnailCache
 from db.services.locator import XMLFileLocator as _XMLFileLocator, \
-    DBLocator as _DBLocator, ZIPFileLocator as _ZIPFileLocator
+    DBLocator as _DBLocator, ZIPFileLocator as _ZIPFileLocator, \
+    BaseLocator as _BaseLocator
 from db.services.io import SaveBundle
 from PyQt4 import QtCore
 import core.configuration
 ElementTree = get_elementtree_library()
+
+class BaseLocator(_BaseLocator):
+    @staticmethod
+    def convert_locator(locator):
+        if locator.__class__ == _XMLFileLocator:
+            locator.__class__ = XMLFileLocator
+        elif locator.__class__ == _ZIPFileLocator:
+            locator.__class__ = ZIPFileLocator
+        elif locator.__class__ == _DBLocator:
+            locator.__class__ = DBLocator
+            
+    @staticmethod
+    def from_url(url):
+        locator = _BaseLocator.from_url(url)
+        BaseLocator.convert_locator(locator)
+        return locator
 
 class CoreLocator(object):
     @staticmethod
@@ -161,12 +178,10 @@ class DBLocator(_DBLocator, CoreLocator):
     keyChain = getKeyChain()
     
     def __init__(self, host, port, database, user, passwd, name=None,
-                 obj_id=None, obj_type=None, connection_id=None,
-                 version_node=None, version_tag=''):
+                 **kwargs):
         
         _DBLocator.__init__(self, host, port, database, user, passwd, name,
-                            obj_id, obj_type, connection_id, version_node,
-                            version_tag)
+                            **kwargs)
         self.ext_connection_id = -1
 
     def load(self, klass=None):
@@ -350,8 +365,8 @@ class DBLocator(_DBLocator, CoreLocator):
     
 class ZIPFileLocator(_ZIPFileLocator, CoreLocator):
 
-    def __init__(self, filename, version_node=None, version_tag=''):
-        _ZIPFileLocator.__init__(self, filename, version_node, version_tag)
+    def __init__(self, filename, **kwargs):
+        _ZIPFileLocator.__init__(self, filename, **kwargs)
 
     def load(self, klass=None):
         from core.vistrail.vistrail import Vistrail

vistrails/core/collection/__init__.py
@@ -1,0 +1,265 @@
+import glob
+import os
+import sqlite3
+
+from entity import Entity
+from vistrail import VistrailEntity
+from workflow import WorkflowEntity
+from workflow_exec import WorkflowExecEntity
+from thumbnail import ThumbnailEntity
+
+from core.db.locator import ZIPFileLocator, DBLocator, FileLocator, BaseLocator
+from core.db.io import load_vistrail
+import core.system
+import db.services.io
+from core.configuration import get_vistrails_configuration
+from core import debug
+
+schema = ["create table entity(id integer primary key, type integer, "
+          "name text, user integer, mod_time text, create_time text, "
+          "size integer, description text, url text)",
+          "create table entity_children(parent integer, child integer)"]
+
+class Collection(object):
+    entity_types = dict((x.type_id, x) 
+                        for x in [VistrailEntity, WorkflowEntity, 
+                                  WorkflowExecEntity, ThumbnailEntity])
+
+    def __init__(self, database=None):
+        if database is None:
+            self.database = ':memory:'
+        else:
+            self.database = database
+
+        self.entities = {}
+        self.deleted_entities = {}
+        self.listeners = [] # listens for entity creation removal
+        self.max_id = 0
+        
+        if not os.path.exists(self.database):
+            debug.log("'%s' does not exist. Trying to create" % self.database)
+            self.conn = sqlite3.connect(self.database)
+            try:
+                cur = self.conn.cursor()
+                [cur.execute(s) for s in schema]
+                self.conn.commit()
+            except Exception, e:
+                debug.critical("Could not create vistrail index schema",
+                               str(e))
+        else:
+            self.conn = sqlite3.connect(self.database)
+                
+            
+        self.load_entities()
+
+    #Singleton technique
+    _instance = None
+    class CollectionSingleton():
+        def __call__(self, *args, **kw):
+            if Collection._instance is None:
+                config = get_vistrails_configuration()
+                if config:
+                    self.dotVistrails = config.dotVistrails
+                else:
+                    self.dotVistrails = core.system.default_dot_vistrails()
+
+                config = get_vistrails_configuration()
+                path = os.path.join(self.dotVistrails, "index.db")
+                obj = Collection(path)
+                Collection._instance = obj
+            return Collection._instance
+        
+    getInstance = CollectionSingleton()
+
+    def add_listener(self, c):
+        """ Add objects that listen to entity creation/removal
+            Object may implement the following methods
+             entityCreated(self, entity)
+             entityDeleted(self, entity)
+        """
+        self.listeners.append(c)
+
+    def clear(self):
+        cur = self.conn.cursor()
+        cur.execute('delete from entity;')
+        cur.execute('delete from entity_children;')
+        self.conn.commit()
+
+    def load_entities(self):
+        cur = self.conn.cursor()
+        cur.execute("select max(id) from entity;")
+        for row in cur.fetchall():
+            n = row[0]
+            self.max_id = n if n is not None else 0
+        
+        cur.execute("select * from entity;")
+        for row in cur.fetchall():
+            entity = self.load_entity(*row)
+            if entity is not None:
+                self.entities[entity.id] = entity
+
+        # now need to map children to correct places
+        cur.execute("select * from entity_children;")
+        for row in cur.fetchall():
+            if row[0] in self.entities and row[1] in self.entities:
+                self.entities[row[0]].children.append(self.entities[row[1]])
+                self.entities[row[1]].parent = self.entities[row[0]]
+
+    def save_entities(self):
+        for entity in self.deleted_entities.itervalues():
+            self.db_delete_entity(entity)
+        self.deleted_entities = {}
+        
+        for entity in self.entities.itervalues():
+            if entity.was_updated:
+                self.save_entity(entity)
+            
+    def load_entity(self, *args):
+        if args[1] in Collection.entity_types:
+            entity = Collection.entity_types[args[1]].load(*args)
+            return entity
+        else:
+            debug.critical("Cannot find entity type '%s'" % args[1])
+        return None
+
+    def add_entity(self, entity):
+        """ Adds entities to memory recursively """
+        if entity.id is None:
+            self.max_id += 1
+            entity.id = self.max_id
+        entity.was_updated = True
+        self.entities[entity.id] = entity
+        for child in entity.children:
+            child.parent = entity
+            self.add_entity(child)
+        for o in self.listeners:
+            if hasattr(o, "entityCreated"):
+                o.entityCreated(entity)
+        
+        cur = self.conn.cursor()
+        entity_tuple = entity.save()
+        cur.execute("insert or replace into entity values(%s)" % \
+                    ','.join(('?',) * len(entity_tuple)), entity_tuple)
+        entity.was_updated = False
+        # self.conn.commit()
+        cur.execute('delete from entity_children where parent=?', (entity.id,))
+        cur.executemany("insert into entity_children values (?, ?)", 
+                        ((entity.id, child.id) for child in entity.children))
+
+    def commit(self):
+        self.save_entities()
+        self.conn.commit()
+
+    def delete_entity(self, entity):
+        """ Delete entity from memory recursively"""
+        self.deleted_entities[entity.id] = entity
+        del self.entities[entity.id]
+        for child in entity.children:
+            self.delete_entity(child)
+        for o in self.listeners:
+            if hasattr(o, "entityDeleted"):
+                o.entityCreated(entity)
+
+    def db_delete_entity(self, entity):
+        cur = self.conn.cursor()
+        if entity.id is not None:
+            cur.execute("delete from entity where id=?", (entity.id,))
+            cur.execute("delete from entity_children where parent=?", (entity.id,))
+            cur.execute("delete from entity_children where child=?", (entity.id,))
+
+    def create_workflow_entity(self, workflow):
+        entity = WorkflowEntity(workflow)
+        self.add_entity(entity)
+        return entity
+
+    def create_vistrail_entity(self, vistrail):
+        entity = VistrailEntity(vistrail)
+        self.add_entity(entity)
+        return entity
+
+    def update_from_database(self, db_locator):
+        # db_conn = db_locator.get_connection()
+        config = (('host', db_locator._host),
+                  ('port', int(db_locator._port)),
+                  ('db', db_locator._db),
+                  ('user', db_locator._user),
+                  ('passwd', db_locator._passwd))
+        rows = db.services.io.get_db_object_list(dict(config), 'vistrail')
+        for row in rows:
+            if row[0] in [1,]:
+                continue
+            kwargs = {'obj_type': 'vistrail', 'obj_id': row[0]}
+            locator = DBLocator(*[x[1] for x in config], **kwargs)
+            (vistrail, abstractions, thumbnails) = load_vistrail(locator)
+            vistrail.abstractions = abstractions
+            vistrail.thumbnails = thumbnails
+
+            entity = self.create_vistrail_entity(vistrail)
+
+    def update_from_directory(self, directory):
+        filenames = glob.glob(os.path.join(directory, '*.vt'))
+        for filename in filenames:
+            print 'processing ', filename
+            locator = FileLocator(filename)
+            url = locator.to_url()
+            self.updateVistrail(url)
+        self.commit()
+
+    def hasUrl(self, url):
+        """ Check if entity with this url exist in index """
+        for e in self.entities.itervalues():
+            if e.url == url:
+                return True
+        return False
+
+    def urlExists(self, url):
+        """ Check if entity with this url exist """
+        locator = BaseLocator.from_url(url)
+        if locator.is_valid():
+                return True
+        return False
+
+    def updateVistrail(self, url, vistrail = None):
+        """ Update the specified url. Delete or reload as necessary. """
+        locator = BaseLocator.from_url(url)
+        entities = [e for e in self.entities.itervalues() if e.url == url]
+        entity = entities[0] if len(entities) else None
+        if entity:
+                self.delete_entity(entity)
+        if locator.is_valid():
+            if not vistrail:
+                (vistrail, abstractions, thumbnails) = load_vistrail(locator)
+                vistrail.abstractions = abstractions
+                vistrail.thumbnails = thumbnails
+            entity = self.create_vistrail_entity(vistrail)
+            self.add_entity(entity)
+
+def main():
+    from db.services.locator import BaseLocator
+    import sys
+    sys.path.append('/home/tommy/git/vistrails/vistrails')
+
+    # vistrail = load_vistrail(ZIPFileLocator('/vistrails/examples/spx.vt'))[0]
+#    db_locator = DBLocator('vistrails.sci.utah.edu', 3306,
+#                           'vistrails', 'vistrails', '8edLj4',
+#                           obj_id=9, obj_type='vistrail')
+    # vistrail = load_vistrail(db_locator)[0]
+    c = Collection('/home/tommy/git/vistrails/vistrails/core/collection/test.db')
+    c.clear()
+    c.update_from_directory('/home/tommy/git/vistrails/examples')
+#    c.update_from_database(db_locator)
+
+    # entity = c.create_vistrail_entity(vistrail)
+    c.entities = {}
+    c.load_entities()
+#    print c.entities[2].url
+#    locator = BaseLocator.from_url(c.entities[2].url)
+#    c.entities[1].description = 'blah blah blah'
+#    c.save_entity(c.entities[1])
+#    print locator.to_url()
+    # c.load_entities()
+
+#    print BaseLocator.from_url('/vistrails/examples/spx.xml').to_url()
+
+if __name__ == '__main__':
+    main()

vistrails/core/collection/schema.sql
@@ -1,0 +1,3 @@
+create table entity(id integer primary key, type integer, name text, user integer, mod_time text, create_time text, size integer, description text, url text);
+create table entity_children(parent integer, child integer);
+create table type_map(id integer, type string);

vistrails/core/collection/thumbnail.py
@@ -1,0 +1,39 @@
+import os
+import stat
+from time import localtime
+from datetime import datetime
+import urlparse
+from core.thumbnails import ThumbnailCache
+
+from entity import Entity
+
+class ThumbnailEntity(Entity):
+    type_id = 4
+
+    def __init__(self, thumbnail=None):
+        Entity.__init__(self)
+        self.id = None
+        self.update(thumbnail)
+
+    @staticmethod
+    def load(*args):
+        entity = ThumbnailEntity()
+        Entity.load(entity, *args)
+        return entity
+
+    def update(self, thumbnail):
+        self.thumbnail = thumbnail
+        if self.thumbnail is not None:
+            # store in cache if not already there
+            cache = ThumbnailCache.getInstance()
+            cache._copy_thumbnails([thumbnail])
+            self.name = os.path.basename(thumbnail)
+            statinfo = os.stat(self.thumbnail)
+            self.user = statinfo[stat.ST_UID]
+            self.size = statinfo[stat.ST_SIZE]
+            time = datetime(*localtime(statinfo[stat.ST_MTIME])[:6]).strftime('%d %b %Y %H:%M:%S')
+            self.mod_time = ''
+            self.create_time = time
+            self.description = ""
+            self.url = 'test'
+            self.was_updated = True

vistrails/core/collection/vistrail.py
@@ -1,0 +1,195 @@
+from PyQt4 import QtGui
+from PyQt4.QtCore import QString
+import xml.sax.saxutils
+
+import os
+import urlparse
+
+from entity import Entity
+from workflow import WorkflowEntity
+from workflow_exec import WorkflowExecEntity
+from thumbnail import ThumbnailEntity
+import core.db.io
+from core.db.locator import DBLocator
+from core.log.log import Log
+
+class VistrailEntity(Entity):
+    type_id = 1
+
+    def __init__(self, vistrail=None):
+        Entity.__init__(self)
+        self.id = None
+        self.update(vistrail)
+
+    @staticmethod
+    def load(*args):
+        entity = VistrailEntity()
+        Entity.load(entity, *args)
+        return entity
+    
+    def create_workflow_entity(self, workflow, action):
+        entity = WorkflowEntity(workflow)
+        self.children.append(entity)
+        if self.vistrail.has_notes(action.id):
+            notes = xml.sax.saxutils.unescape(self.vistrail.get_notes(action.id))
+            fragment = QtGui.QTextDocumentFragment.fromHtml(QString(notes))
+            plain_notes = str(fragment.toPlainText())
+            entity.description = plain_notes
+        else:
+            entity.description = ''
+        entity.user = action.user
+        entity.mod_time = action.date
+        entity.create_time = action.date
+        scheme, rest = self.url.split('://', 1)
+        url = 'http://' + rest
+        url_tuple = urlparse.urlsplit(url)
+        query_str = url_tuple[3]
+        if query_str == '':
+            query_str = 'workflow=%s' % action.id
+        else:
+            query_str += '&workflow=%s' % action.id
+        url_tuple = (scheme, url_tuple[1], url_tuple[2], query_str,
+                     url_tuple[4])
+        entity.url = urlparse.urlunsplit(url_tuple)
+        # entity.url = self.url + '?workflow_id=%s' % action.id
+        return entity
+
+    def update(self, vistrail):
+        self.vistrail = vistrail
+        if self.vistrail is not None:
+            wf_entity_map = {}
+            latestVersion = self.vistrail.actionMap[self.vistrail.get_latest_version()]
+            firstVersion = self.vistrail.actionMap[1] \
+                if 1 in self.vistrail.actionMap else latestVersion
+
+            self.name = self.vistrail.locator.short_name
+            self.user = latestVersion.user
+            self.mod_time = latestVersion.date
+            self.create_time = firstVersion.date
+            self.size = len(self.vistrail.actionMap)
+            self.description = ""
+            self.url = self.vistrail.locator.to_url()
+            self.was_updated = True
+
+            for id, tag in self.vistrail.get_tagMap().iteritems():
+                if id not in self.vistrail.actionMap:
+                    continue
+                action = self.vistrail.actionMap[id]
+                workflow = self.vistrail.getPipeline(id)
+                tag = self.vistrail.get_action_annotation(id, "__tag__")
+                if tag:
+                    workflow.name = tag.value
+                # if workflow already exists, we want to update it...
+                # spin through self.children and look for matching
+                # workflow entity?
+                # self.children.append(WorkflowEntity(workflow))
+                wf_entity_map[id] = \
+                    self.create_workflow_entity(workflow, action)
+
+            log = None
+            if vistrail.db_log_filename is not None:
+                log = core.db.io.open_log(vistrail.db_log_filename, True)
+            elif vistrail.log is not None and \
+                hasattr(self.vistrail.locator,"_host"):
+                locator = DBLocator(self.vistrail.locator._host,
+                                    self.vistrail.locator._port,
+                                    self.vistrail.locator._db,
+                                    self.vistrail.locator._user,
+                                    self.vistrail.locator._passwd,
+                                    obj_id=vistrail.log,
+                                    obj_type='log')
+                log = locator.load(Log)
+
+            if log is not None:
+                for wf_exec in log.workflow_execs:
+                    version_id = wf_exec.parent_version
+                    if version_id not in wf_entity_map:
+
+                        # FIXME add new workflow entity for this version
+                        if version_id not in self.vistrail.actionMap:
+                            continue
+                        action = self.vistrail.actionMap[version_id]
+                        workflow = self.vistrail.getPipeline(version_id)
+                        wf_entity = \
+                            self.create_workflow_entity(workflow, action)
+
+                        wf_entity_map[version_id] = wf_entity
+                    else:
+                        wf_entity = wf_entity_map[version_id]
+                    entity = WorkflowExecEntity(wf_exec)
+
+                    scheme, rest = self.url.split('://', 1)
+                    url = 'http://' + rest
+                    url_tuple = urlparse.urlsplit(url)
+                    query_str = url_tuple[3]
+                    if query_str == '':
+                        query_str = 'workflow_exec=%s' % wf_exec.id
+                    else:
+                        query_str += '&workflow_exec=%s' % wf_exec.id
+                    url_tuple = (scheme, url_tuple[1], url_tuple[2], query_str,
+                                 url_tuple[4])
+                    entity.url = urlparse.urlunsplit(url_tuple)
+                    wf_entity.children.append(entity)
+
+            thumbnail_map = {}
+            for thumbnail in self.vistrail.thumbnails:
+                thumbnail_map[os.path.basename(thumbnail)] = thumbnail
+
+            for action in self.vistrail.actionMap.itervalues():
+                thumbnail = self.vistrail.get_thumbnail(action.id)
+                if thumbnail is not None and thumbnail in thumbnail_map:
+                    entity = ThumbnailEntity(thumbnail_map[thumbnail])
+                    if action.id in wf_entity_map:
+                        wf_entity_map[action.id].children.append(entity)
+                    else:
+                        # there is a thumbnail but the action is not important
+                        #print "No such action:", action.id                    
+                        pass
+
+#     # returns string
+#     def get_name(self):
+#         return self.vistrail.name
+    
+#     # returns datetime
+#     def get_mod_time(self):
+#         return self.vistrail.get_most_recent_action().py_date
+
+#     # returns datetime
+#     def get_create_time(self):
+#         return self.vistrail.get_first_action().py_date
+    
+#     # returns string
+#     # FIXME: perhaps this should be a User object at some point
+#     def get_user(self):
+#         return self.vistrail.get_most_recent_action().py_date
+    
+#     # returns tuple (<entity_type>, <entity_id>)
+#     def get_id(self):
+#         return (self.vistrail.vtType, self.vistrail.id)
+
+#     # returns integer
+#     def get_size(self):
+#         return len(self.vistrail.actionMap)
+    
+#     # returns possibly empty list of Entity objects
+#     def get_children(self):
+#         return len(self.vistrail.tagMap)
+
+#     # returns list of strings representing paths
+#     # FIXME: should this be urls for database access?
+#     def get_image_fnames(self):
+#         return []
+    
+    # returns boolean, True if search input is satisfied else False
+    def match(self, search):
+        # try match on self
+        
+
+        # if no match on self, check for a match on all children
+        for child in self.get_children():
+            if child.match(search):
+                return True
+
+    def open(self):
+        locator = BaseLocator.from_url(self.url)
+        core.open_locator(locator)

commit 7fad01699503e8068fd81e2d877d24031a876560

Fixes DBLocator#parse() broken by cccf158c
"Details to be added later".

I'm unsure whether these "parse" methods are actually used.

vistrails/db/services/locator.py
@@ -938,7 +938,7 @@ class DBLocator(BaseLocator):
                     name = str(n.firstChild.nodeValue).strip(" \n\t")
                     #print host, port, database, name, vt_id
                     return DBLocator(host, port, database,
-                                     user, passwd, name, vt_id, None)
+                                     user, passwd, name, obj_id=vt_id)
             return None
         else:
             return None