61a5d306d6cc949225e9db2a4b405352ded3b1f9
Remi Rampin <remirampin@gmail.com>
2014-10-03 15:39:35 -0400

    Makes xlrd optional

--- vistrails/packages/tabledata/read/__init__.py
+++ vistrails/packages/tabledata/read/__init__.py
@@ -6,11 +6,16 @@ try:
 except ImportError: # pragma: no cover
     numpy_modules = []
 else:
-    from read_numpy import _modules as numpy_modules
+    from .read_numpy import _modules as numpy_modules
 
-from read_csv import _modules as csv_modules
-from read_excel import _modules as excel_modules
-from read_json import _modules as json_modules
+from .read_excel import get_xlrd
+if get_xlrd():
+    from .read_excel import _modules as excel_modules
+else: # pragma: no cover
+    excel_modules = []
+
+from .read_csv import _modules as csv_modules
+from .read_json import _modules as json_modules
 
 _modules = make_modules_dict(numpy_modules, csv_modules, excel_modules,
                              json_modules,
