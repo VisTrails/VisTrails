4b8ed861f851d77cec1aadd5c99c0a48241c173c
Remi Rampin <remirampin@gmail.com>
2014-11-11 17:17:36 -0500

    Fixes paths in pypi.sh

--- scripts/dist/pypi.sh
+++ scripts/dist/pypi.sh
@@ -3,14 +3,14 @@
 # This script automatically builds a source package ("sdist") and wheel
 # ("bdist_wheel") of VisTrails
 
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/../.."
 
 if [ -z "$1" ]; then
     echo "Usage: $(basename $0) <target_directory> [--no-set-version]" >&2
     exit 1
 fi
 if [ "$2" != "--no-set-version" ]; then
-    if ! python dist/common/set_version.py setup.py; then
+    if ! python scripts/dist/common/set_version.py setup.py; then
         echo "Setting version number in setup.py failed" >&2
         exit 1
     fi
