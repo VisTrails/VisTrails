6b3e5e099a5f631a51cf030bb2953addbd22a255
Remi Rampin <remirampin@gmail.com>
2015-01-12 15:32:06 -0500

    Adds missing import

--- vistrails/core/interpreter/noncached.py
+++ vistrails/core/interpreter/noncached.py
@@ -33,6 +33,7 @@
 ##
 ###############################################################################
 import vistrails.core.interpreter.base
+import vistrails.core.interpreter.cached
 import vistrails.core.interpreter.utils
 import vistrails.core.vistrail.pipeline
 import atexit
