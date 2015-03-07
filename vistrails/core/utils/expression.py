VisTrails/VisTrails

commit 62b10b9c9dba4d10813cddecd4fc1c692887aa3d

Replaces eval() with safer literal_eval()

vistrails/core/utils/expression.py
@@ -34,6 +34,8 @@
 ###############################################################################
 """ Helper functions for parsing and evaluating expressions """
 
+from ast import literal_eval
+
 ################################################################################
 
 def evaluate_expressions(expressions):
@@ -45,8 +47,8 @@ def evaluate_expressions(expressions):
     # FIXME: eval should pretty much never be used
     (base, exps) = parse_expression(str(expressions))
     for e in exps:
-        try:                        
-            base = base[:e[0]] + unicode(eval(e[1],None,None)) + base[e[0]:]
+        try:
+            base = base[:e[0]] + unicode(literal_eval(e[1])) + base[e[0]:]
         except Exception:
             base = base[:e[0]] + '$' + e[1] + '$' + base[e[0]:]
     return base

vistrails/core/interpreter/base.py
@@ -32,12 +32,8 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ##
 ###############################################################################
-from vistrails.core.data_structures.graph import Graph
-from vistrails.core.utils import expression
-from vistrails.core.utils import trace_method
 from vistrails.core import debug
 import copy
-import parser
 
 ##############################################################################
 
@@ -145,17 +141,19 @@ class BaseInterpreter(object):
 
     def evaluate_exp(self, atype, base, exps, aliases):
         # FIXME: eval should pretty much never be used
-        import datetime        
-        for e in exps: base = (base[:e[0]] +
-                               str(eval(e[1],
-                                        {'datetime':locals()['datetime']},
-                                        aliases)) +
-                               base[e[0]:])
+        import datetime
+        for e in exps:
+            base = (
+                    base[:e[0]] + str(eval(
+                        e[1],
+                        {'datetime': locals()['datetime']},
+                        aliases)) +
+                    base[e[0]:])
         if not atype in ['string', 'String']:
             if base=='':
                 base = '0'
             try:
-                base = eval(base,None,None)
+                base = eval(base)
             except Exception:
                 pass
         return base

vistrails/core/modules/basic_modules.py
@@ -47,6 +47,7 @@ from vistrails.core.utils import InstanceObject
 from vistrails.core import debug
 
 from abc import ABCMeta
+from ast import literal_eval
 from itertools import izip
 import os
 import pickle
@@ -843,7 +844,7 @@ class List(Constant):
 
     @staticmethod
     def translate_to_python(v):
-        return eval(v)
+        return literal_eval(v)
 
     @staticmethod
     def translate_to_string(v, dims=None):
@@ -887,7 +888,7 @@ class List(Constant):
 # Dictionary
                     
 def dict_conv(v):
-    v_dict = eval(v)
+    v_dict = literal_eval(v)
     return v_dict
 
 def dict_compute(self):

vistrails/core/modules/module_registry.py
@@ -33,7 +33,7 @@
 ##
 ###############################################################################
 from itertools import izip, chain
-import ast
+from ast import literal_eval
 import collections
 import copy
 import os
@@ -1429,7 +1429,7 @@ class ModuleRegistry(DBRegistry):
             if defaults is not None:
                 new_defaults = []
                 if isinstance(defaults, basestring):
-                    defaults = ast.literal_eval(defaults)
+                    defaults = literal_eval(defaults)
                 if not isinstance(defaults, list):
                     raise ValueError('Defaults for port "%s" must be a list' %
                                      name)
@@ -1448,13 +1448,13 @@ class ModuleRegistry(DBRegistry):
             if values is not None:
                 new_values = []
                 if isinstance(values, basestring):
-                    values = ast.literal_eval(values)
+                    values = literal_eval(values)
                 if not isinstance(values, list):
                     raise ValueError('Values for port "%s" must be a list '
                                      'of lists' % name)
                 for i, values_list in enumerate(values):
                     if isinstance(values_list, basestring):
-                        values_list = ast.literal_eval(values_list)
+                        values_list = literal_eval(values_list)
                     if values_list is not None:
                         if not isinstance(values_list, list):
                             raise ValueError('Values for port "%s" must be '

vistrails/core/paramexplore/paramexplore.py
@@ -47,7 +47,7 @@ from vistrails.core.modules.paramexplore import IntegerLinearInterpolator, \
    FloatLinearInterpolator, RGBColorInterpolator, HSVColorInterpolator,\
    UserDefinedFunctionInterpolator
 
-
+from ast import literal_eval
 import unittest
 import copy
 
@@ -102,7 +102,7 @@ class ParameterExploration(DBParameterExploration):
     
     def get_dims(self):
         try:
-            return eval(self._dims)
+            return literal_eval(self._dims)
         except Exception:
             return []
     def set_dims(self, d):
@@ -114,7 +114,7 @@ class ParameterExploration(DBParameterExploration):
 
     def get_layout(self):
         try:
-            return eval(self._layout)
+            return literal_eval(self._layout)
         except Exception:
             return {}
     def set_layout(self, l):
@@ -160,25 +160,25 @@ class ParameterExploration(DBParameterExploration):
                 if param.interpolator == 'Linear Interpolation':
                     # need to figure out type
                     if port_spec_item.module == "Integer":
-                        i_range = eval(text)
+                        i_range = literal_eval(text)
                         p_min = int(i_range[0])
                         p_max =int(i_range[1])
                         values = IntegerLinearInterpolator(p_min, p_max,
                                                      count).get_values()
                     if port_spec_item.module == "Float":
-                        i_range = eval(text)
+                        i_range = literal_eval(text)
                         p_min = float(i_range[0])
                         p_max =float(i_range[1])
                         values = FloatLinearInterpolator(p_min, p_max,
                                                      count).get_values()
                 elif param.interpolator == 'RGB Interpolation':
-                    i_range = eval(text)
+                    i_range = literal_eval(text)
                     p_min = str(i_range[0])
                     p_max =str(i_range[1])
                     values = RGBColorInterpolator(p_min, p_max,
                                                      count).get_values()
                 elif param.interpolator == 'HSV Interpolation':
-                    i_range = eval(text)
+                    i_range = literal_eval(text)
                     p_min = str(i_range[0])
                     p_max =str(i_range[1])
                     values = HSVColorInterpolator(p_min, p_max,
@@ -186,7 +186,7 @@ class ParameterExploration(DBParameterExploration):
                 elif param.interpolator == 'List':
                     p_module = port_spec_item.descriptor.module
                     values = [p_module.translate_to_python(m)
-                              for m in eval(text)]
+                              for m in literal_eval(text)]
                 elif param.interpolator == 'User-defined Function':
                     p_module = port_spec_item.descriptor.module
                     values = UserDefinedFunctionInterpolator(p_module,

vistrails/core/vistrail/controller.py
@@ -2014,13 +2014,6 @@ class VistrailController(object):
                                                     abs_name, None, 
                                                     module_version, 
                                                     is_global, avail_fnames)
-            #if desc.version != module_version:
-                #print "upgraded version", module_version, "of", abs_name, "(namespace: %s)"%abstraction_uuid, "to version", desc.version, "and namespace", desc.namespace
-#        else:
-#            if upgrade_version is not None:
-#                print "version", old_version, "of", abs_name, "(namespace: %s)"%abstraction_uuid, "already in registry as upgraded version", module_version
-#            else:
-#                print "version", module_version, "of", abs_name, "(namespace: %s)"%abstraction_uuid, "already in registry"
         return desc
     
     def unload_abstractions(self):
@@ -2035,52 +2028,6 @@ class VistrailController(object):
                 except Exception:
                     pass
         self._loaded_abstractions.clear()
-
-        # for abs_fname, abs_vistrail in self._loaded_abstractions.iteritems():
-        #     abs_desc_info = abs_vistrail.get_annotation('__abstraction_descriptor_info__')
-        #     if abs_desc_info is not None:
-        #         abs_desc_info = eval(abs_desc_info.value)
-        #         # Don't unload package abstractions that have been
-        #         # upgraded by this controller (during a manual version
-        #         # upgrade) because that would also unload the version
-        #         # in the module palette
-        #         if abs_desc_info[2] == abs_vistrail.get_annotation('__abstraction_uuid__').value:
-        #             continue
-        #     abs_name = parse_abstraction_name(abs_fname)
-        #     abs_namespace = abs_vistrail.get_annotation('__abstraction_uuid__').value
-        #     try:
-        #         descriptor = self.get_abstraction_descriptor(abs_name, abs_namespace)
-        #         print "removing all versions of", abs_name, "from registry (namespace: %s)"%abs_namespace
-        #         while descriptor is not None:
-        #             reg = core.modules.module_registry.get_module_registry()
-        #             reg.delete_module(abstraction_pkg, abs_name, abs_namespace)
-        #             descriptor = self.get_abstraction_descriptor(abs_name, abs_namespace)
-        #     except:
-        #         # No versions of the abstraction exist in the registry now
-        #         pass
-        # self._loaded_abstractions.clear()
-
-#    def update_abstraction(self, abstraction, new_actions):
-#        module_version = abstraction.internal_version
-#        if isinstance(module_version, basestring):
-#            module_version = int(module_version)
-#        abstraction_uuid = \
-#            abstraction.vistrail.get_annotation('__abstraction_uuid__').value
-#        upgrade_action = self.create_upgrade_action(new_actions) 
-#        
-#        a = (abstraction.vistrail, 
-#             module_version)
-#        
-#        desc = self.get_abstraction_desc(abstraction.name, abstraction_uuid,
-#                                         new_version)
-#        if desc is None:
-#            # desc = self.add_abstraction_to_registry(abstraction.vistrail,
-#            # abstraction.
-#            pass
-#        # FIXME finish this!
-                                         
-                                         
-        
 
     def manage_package_names(self, vistrail, package):
         vistrail = copy.copy(vistrail)

vistrails/core/vistrail/port_spec.py
@@ -41,12 +41,11 @@ from vistrails.core.system import get_vistrails_basic_pkg_id, \
     get_module_registry
 from vistrails.core.utils import enum, VistrailsInternalError
 from vistrails.core.vistrail.port_spec_item import PortSpecItem
-from vistrails.db.domain import DBPortSpec
+from vistrails.db.domain import DBPortSpec, IdScope
 
+from ast import literal_eval
 import unittest
 import copy
-from vistrails.db.domain import IdScope
-import vistrails.core
 
 PortEndPoint = enum('PortEndPoint',
                     ['Invalid', 'Source', 'Destination'])
@@ -287,19 +286,19 @@ class PortSpec(DBPortSpec):
         if defaults is None:
             defaults = []
         elif isinstance(defaults, basestring):
-            defaults = eval(defaults)
+            defaults = literal_eval(defaults)
         if labels is None:
             labels = []
         elif isinstance(labels, basestring):
-            labels = eval(labels)
+            labels = literal_eval(labels)
         if values is None:
             values = []
         elif isinstance(values, basestring):
-            values = eval(values)
+            values = literal_eval(values)
         if entry_types is None:
             entry_types = []
         elif isinstance(entry_types, basestring):
-            entry_types = eval(entry_types)
+            entry_types = literal_eval(entry_types)
         attrs = [defaults, labels, values, entry_types]
         if items:
             self.set_items(items, *attrs)

vistrails/core/vistrail/port_spec_item.py
@@ -32,13 +32,16 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ##
 ###############################################################################
+
+from ast import literal_eval
 import copy
+import unittest
+
 from vistrails.core.modules.utils import parse_port_spec_item_string, \
     create_port_spec_item_string
 from vistrails.core.system import get_module_registry
-from vistrails.db.domain import DBPortSpecItem, IdScope
+from vistrails.db.domain import DBPortSpecItem
 
-import unittest
 
 _MissingPackage = None
 def get_MissingPackage():
@@ -182,8 +185,7 @@ class PortSpecItem(DBPortSpecItem):
 
     def _get_values(self):
         if self._values is None:
-            # don't use eval here...
-            self._values = eval(self.db_values)
+            self._values = literal_eval(self.db_values)
         return self._values
     def _set_values(self, values):
         if not isinstance(values, basestring):
@@ -191,8 +193,7 @@ class PortSpecItem(DBPortSpecItem):
             self.db_values = str(values)
         else:
             self.db_values = values
-            # don't use eval here...
-            self._values = eval(values)
+            self._values = literal_eval(values)
     values = property(_get_values, _set_values)
 
     def _get_spec_tuple(self):

commit 626555cd0633cd066e25f0aa5d949151629bc38f

Fixes evaluate_expressions()
Broken by 62b10b9c.

vistrails/core/utils/expression.py
@@ -37,8 +37,6 @@
 
 from __future__ import division
 
-from ast import literal_eval
-
 ################################################################################
 
 def evaluate_expressions(expressions):
@@ -51,7 +49,7 @@ def evaluate_expressions(expressions):
     (base, exps) = parse_expression(str(expressions))
     for e in exps:
         try:
-            base = base[:e[0]] + unicode(literal_eval(e[1])) + base[e[0]:]
+            base = base[:e[0]] + unicode(eval(e[1], None, None)) + base[e[0]:]
         except Exception:
             base = base[:e[0]] + '$' + e[1] + '$' + base[e[0]:]
     return base