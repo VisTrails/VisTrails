d7f3be17579ee6c82b6b3995e1e299a45843ff81
Remi Rampin <remirampin@gmail.com>
2015-03-20 15:02:53 -0400

    Switch from package_data to a MANIFEST.in

--- setup.py
+++ setup.py
@@ -11,41 +11,6 @@ for rootdir, dirs, files in os.walk('vistrails'):
         packages.append(rootdir.replace('\\', '.').replace('/', '.'))
 
 
-def list_files(d, root):
-    files = []
-    for e in os.listdir(os.path.join(root, d)):
-        if os.path.isdir(os.path.join(root, d, e)):
-            files.extend(list_files('%s/%s' % (d, e), root))
-        elif not e.endswith('.pyc'):
-            files.append('%s/%s' % (d, e))
-    return files
-
-
-package_data = {
-    'vistrails.core.collection': ['schema.sql', 'test.db'],
-    'vistrails.core': list_files('resources', 'vistrails/core'),
-    'vistrails.db': ['specs/all.xml'],
-    'vistrails.gui': list_files('resources/images', 'vistrails/gui') + ['resources/vistrails-mime.xml'],
-    'vistrails.packages.analytics': ['*.vt'], # FIXME : what is this?
-    'vistrails.packages.CLTools': ['icons/*.png', 'test_files/*'],
-    'vistrails.packages.persistence': ['schema.sql'],
-    'vistrails.packages.tabledata': ['test_files/*'],
-    'vistrails.tests': list_files('resources', 'vistrails/tests'),
-    }
-for version in os.listdir('vistrails/db/versions'):
-    if not version.startswith('v'):
-        continue
-    package_data['vistrails.db.versions.%s' % version] = [
-        'schemas/sql/vistrails.sql',
-        'schemas/sql/vistrails_drop.sql',
-        'schemas/xml/log.xsd',
-        'schemas/xml/vistrail.xsd',
-        'schemas/xml/vtlink.xsd',
-        'schemas/xml/workflow.xsd',
-        'specs/all.xml',
-]
-
-
 description = """
 VisTrails is an open-source data analysis and visualization tool. It provides a comprehensive provenance infrastructure that maintains detailed history information about the steps followed and data derived in the course of an exploratory task: VisTrails maintains provenance of data products, of the computational processes that derive these products and their executions.
 
@@ -60,7 +25,7 @@ Who we are: http://www.vistrails.org/index.php/People
 setup(name='vistrails',
       version='2.1.5',
       packages=packages,
-      package_data=package_data,
+      include_package_data=True,
       entry_points={
         'console_scripts': [
           'vistrails = vistrails.run:main']},
