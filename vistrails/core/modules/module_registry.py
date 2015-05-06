VisTrails/VisTrails

commit d6779b8599ac50fbac1a45fbab366f7ca4bf16d4

Fixes lots of issues found by my IDE

vistrails/core/modules/module_registry.py
@@ -739,34 +739,32 @@ class ModuleRegistry(DBRegistry):
         package_version = package_version or ''
         module_version = module_version or ''
 
-        try:
-            package = self.packages[identifier]
-            if package_version:
-                package_version_key = (identifier, package_version)
+        if package_version:
+            package_version_key = (identifier, package_version)
+            try:
                 package = self.package_versions[package_version_key]
-            if not module_version:
+            except KeyError:
+                raise MissingPackageVersion(identifier, package_version)
+        else:
+            try:
+                package = self.packages[identifier]
+            except KeyError:
+                raise MissingPackage(identifier)
+        if not module_version:
+            try:
                 descriptor = package.descriptors[(name, namespace)]
-            else:
-                descriptor_version_key = (name, namespace, module_version)
+            except KeyError:
+                raise MissingModule(identifier, name, namespace,
+                                    package_version)
+        else:
+            descriptor_version_key = (name, namespace, module_version)
+            try:
                 descriptor = \
                     package.descriptor_versions[descriptor_version_key]
-            return descriptor
-        except KeyError:
-            if identifier not in self.packages:
-                raise MissingPackage(identifier)
-            elif (name, namespace) not in package.descriptors:
-                raise MissingModule(identifier, name, namespace, 
-                                    package_version)
-            elif package_version and \
-                    package_version_key not in self.package_versions:
-                raise MissingPackageVersion(identifier, package_version)
-            elif module_version and descriptor_version_key not in \
-                    package.descriptor_versions:
+            except KeyError:
                 raise MissingModuleVersion(identifier, name, namespace,
                                            module_version, package_version)
-            else:
-                raise ModuleRegistryException(identifier, name, namespace,
-                                              package_version, module_version)
+        return descriptor
 
     def get_similar_descriptor(self, identifier, name, namespace=None,
                                package_version=None, module_version=None):
@@ -779,10 +777,6 @@ class ModuleRegistry(DBRegistry):
         except MissingModuleVersion:
             return self.get_similar_descriptor(identifier, name, namespace,
                                                package_version, None)
-#         except Exception:
-#             raise
-
-        return None
             
     def get_descriptor(self, module):
         """get_descriptor(module: class) -> ModuleDescriptor

vistrails/core/analogy/__init__.py
@@ -125,7 +125,7 @@ def perform_analogy_on_vistrail(vistrail, version_a, version_b, version_c,
                     a_connect.destination.spec == c_connect.destination.spec):
                     break
         if match is not None:
-            connection_remap[a_connect.id] = c_connect.id
+            connection_remap[a_connect.id] = match.id
         elif _debug:
             print "failed to find connection match", a_connect.id, a_source, \
                 a_dest

vistrails/core/application.py
@@ -226,13 +226,13 @@ class VistrailsApplicationInterface(object):
                     name = str(data[0])
                 # will try to convert version to int
                 # if it fails, it's a tag name
+                #maybe a tag name contains ':' in its name
+                #so we need to bring it back together
+                rest = ":".join(data[1:])
                 try:
-                    #maybe a tag name contains ':' in its name
-                    #so we need to bring it back together
-                    rest = ":".join(data[1:])
                     version = int(rest)
                 except ValueError:
-                    version = str(rest)
+                    version = rest
             elif len(data) == 1:
                 if use_filename and os.path.isfile(str(data[0])):
                     name = str(data[0])

vistrails/core/collection/vistrail.py
@@ -346,6 +346,7 @@ class VistrailEntity(Entity):
                 #             self.add_parameter_exploration_entity(pe)
                 
             # read persisted log entries
+            log = None
             try:
                 log = vistrail.get_persisted_log()
             except Exception, e:

vistrails/core/configuration.py
@@ -734,6 +734,8 @@ def get_system_config():
         sys_config = linux_config
     elif system.systemType in ['Darwin']:
         sys_config = mac_config
+    else:
+        return config
     for category, fields in sys_config.iteritems():
         if category not in base_config:
             config[category] = fields

vistrails/core/data_structures/graph.py
@@ -313,11 +313,14 @@ class Graph(object):
         
         if old_id == None:
             efroom = self.adjacency_list[old_froom]
+            forward_idx = None
             for i, edge in enumerate(efroom):
                 if edge[0] == old_to:
                     old_id = edge[1]
                     forward_idx = i
                     break
+            if forward_idx is None:
+                raise ValueError("No edge to %r" % old_to)
         else:
             forward_idx = self.adjacency_list[old_froom].index((old_to, old_id))
 
@@ -803,7 +806,7 @@ class TestGraph(unittest.TestCase):
          """Test sink and source degree consistency"""
          g = Graph()
          for i in xrange(100):
-             g.add_vertex(i);
+             g.add_vertex(i)
          for i in xrange(1000):
              v1 = random.randint(0,99)
              v2 = random.randint(0,99)

vistrails/core/db/locator.py
@@ -401,20 +401,13 @@ class DBLocator(_DBLocator, CoreLocator):
         If the connection exists it will update it, else it will add it
 
         """
-        if kwargs.has_key("id"):
-            id = kwargs["id"]
-        if kwargs.has_key("name"):
-            name = kwargs["name"]
-        if kwargs.has_key("host"):
-            host = kwargs["host"]
-        if kwargs.has_key("port"):
-            port = kwargs["port"]
-        if kwargs.has_key("user"):
-            user = kwargs["user"]
-        if kwargs.has_key("passwd"):
-            passwd = kwargs["passwd"]
-        if kwargs.has_key("db"):
-            db = kwargs["db"]
+        id = kwargs["id"]
+        name = kwargs["name"]
+        host = kwargs["host"]
+        port = kwargs["port"]
+        user = kwargs["user"]
+        passwd = kwargs["passwd"]
+        db = kwargs["db"]
 
         conn = DBConnection(id=id,
                             name=name,
@@ -681,7 +674,7 @@ class FileLocator(CoreLocator):
             pass
 
         if tag is None:
-            tag = '';
+            tag = ''
             
         ## execute and showSpreadsheetOnly should be written to the current
         ## configuration

vistrails/core/debug.py
@@ -165,7 +165,7 @@ class DebugPrint(object):
     so it will only get information of who called the DebugPrint functions.
 
     Example of usage:
-        >>> from core import debug
+        >>> from vistrails.core import debug
         >>> debug.DebugPrint.getInstance().set_message_level(
                     debug.DebugPrint.WARNING)
         # the following messages will be shown
@@ -386,6 +386,8 @@ def object_at(desc):
         target_id = desc
     elif isinstance(desc, basestring):
         target_id = int(desc, 16) # Reads desc as the hex address
+    else:
+        raise TypeError
     import gc
     for obj in gc.get_objects():
         if id(obj) == target_id:

commit df96ac177b2502750567b7b2a6b4fa491befb4a2

Fixed get_descriptor_by_name not raising MissingModule when module_version is set
This was caused by a regression in d6779b85

Loading a vistrail with an out-of-date subworkflow triggered this.

vistrails/core/modules/module_registry.py
@@ -765,13 +765,12 @@ class ModuleRegistry(DBRegistry):
                 package = self.package_versions[(identifier, package_version)]
             except KeyError:
                 raise MissingPackageVersion(identifier, package_version)
-        if not module_version:
-            try:
-                descriptor = package.descriptors[(name, namespace)]
-            except KeyError:
-                raise MissingModule(identifier, name, namespace,
-                                    package_version)
-        else:
+        try:
+            descriptor = package.descriptors[(name, namespace)]
+        except KeyError:
+            raise MissingModule(identifier, name, namespace,
+                                package_version)
+        if module_version:
             descriptor_version_key = (name, namespace, module_version)
             try:
                 descriptor = \