54fa4467580db946ec1a4c50e4ac5b8eed2a2ed3
David Koop <dkoop@umassd.edu>
2015-06-11 15:48:38 -0400

    Add script to create database

---
+++ scripts/create_db.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+###############################################################################
+##
+## Copyright (C) 2014-2015, New York University.
+## Copyright (C) 2011-2014, NYU-Poly.
+## Copyright (C) 2006-2011, University of Utah.
+## All rights reserved.
+## Contact: contact@vistrails.org
+##
+## This file is part of VisTrails.
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice,
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright
+##    notice, this list of conditions and the following disclaimer in the
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of the New York University nor the names of its
+##    contributors may be used to endorse or promote products derived from
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+import os
+import sys
+import tempfile
+sys.path.append(os.path.dirname(os.path.dirname(
+                os.path.abspath(__file__))))
+from vistrails.db.services import io
+from db_utils import parse_db_cmd_line
+
+def create_db(config, version=None):
+    db_connection = io.default_open_db_connection(config)
+    try:
+        io.get_version_from_db(db_connection)
+        raise Exception("Database already exists. Delete or upgrade it")
+    except AttributeError:
+        # asssume we don't have anything in the database
+        pass
+
+    if version is not None:
+        config['version'] = version
+    db_connection = io.open_db_connection(config)
+    # utils = io.get_sql_utils(version)
+    # db_connection = utils.open_db_connection(config)
+    # db_connection.__vt_db_version__ = version
+
+    io.create_db_tables(db_connection)
+    io.close_db_connection(db_connection)
+
+if __name__ == '__main__':
+    import vistrails.core.application
+
+    more_options = {'v:': ('schema version', False, 'version'),
+                    }
+    config, options = parse_db_cmd_line(sys.argv, more_options)
+    version = None
+    if options['v']:
+        version = options['v']
+
+    vistrails.core.application.init()
+    create_db(config, version)
