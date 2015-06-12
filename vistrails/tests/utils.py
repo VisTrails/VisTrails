edc15e441d4420be065495491a927a12b48efb29
Remi Rampin <remirampin@gmail.com>
2015-06-12 16:29:13 -0400

    Split build_pipeline() out of execute()
    Also, moves it and enable_package() to vistrails.core.vistrail, since it
    is to be used from DAT.

--- vistrails/tests/utils.py
+++ vistrails/tests/utils.py
@@ -46,25 +46,12 @@ except ImportError:
     import StringIO
 
 from vistrails.core.modules.vistrails_module import Module
+from vistrails.core.vistrail import enable_package
 
 
-def enable_package(identifier):
-    """Enables a package.
-    """
-    from vistrails.core.modules.module_registry import MissingPackage
-    from vistrails.core.packagemanager import get_package_manager
-
-    pm = get_package_manager()
-
-    try:
-        pm.get_package(identifier)
-    except MissingPackage:
-        dep_graph = pm.build_dependency_graph([identifier])
-        for pkg_id in pm.get_ordered_dependencies(dep_graph):
-            pkg = pm.identifier_is_available(pkg_id)
-            if pkg is None:
-                raise
-            pm.late_enable_package(pkg.codepath)
+__all__ = ['enable_package', 'execute', 'intercept_result',
+           'intercept_results', 'capture_stdout', 'capture_stderr',
+           'MockLogHandler']
 
 
 def execute(modules, connections=[], add_port_specs=[],
@@ -75,31 +62,8 @@ def execute(modules, connections=[], add_port_specs=[],
     doing that, intercept_result() can be used to check the results of each
     module.
 
-    modules is a list of module tuples describing the modules to be created,
-    with the following format:
-        [('ModuleName', 'package.identifier', [
-            # Functions
-            ('port_name', [
-                # Function parameters
-                ('Signature', 'value-as-string'),
-            ]),
-        ])]
-
-    connections is a list of tuples describing the connections to make, with
-    the following format:
-        [
-            (source_module_index, 'source_port_name',
-             dest_module_index, 'dest_module_name'),
-         ]
-
-    add_port_specs is a list of specs to add to modules, with the following
-    format:
-        [
-            (mod_id, 'input'/'output', 'portname',
-             '(port_sig)'),
-        ]
-    It is useful to test modules that can have custom ports through a
-    configuration widget.
+    The pipeline arguments are specified in
+    :func:`vistrails.core.vistrail.build_pipeline()`.
 
     The function returns the 'errors' dict it gets from the interpreter, so you
     should use a construct like self.assertFalse(execute(...)) if the execution
@@ -124,89 +88,12 @@ def execute(modules, connections=[], add_port_specs=[],
             (1, 'value', 2, 'value'),
         ]))
     """
+    from vistrails.core.vistrail import build_pipeline
     from vistrails.core.db.locator import XMLFileLocator
-    from vistrails.core.modules.module_registry import MissingPackage
-    from vistrails.core.packagemanager import get_package_manager
     from vistrails.core.utils import DummyView
-    from vistrails.core.vistrail.connection import Connection
-    from vistrails.core.vistrail.module import Module
-    from vistrails.core.vistrail.module_function import ModuleFunction
-    from vistrails.core.vistrail.module_param import ModuleParam
-    from vistrails.core.vistrail.pipeline import Pipeline
-    from vistrails.core.vistrail.port import Port
-    from vistrails.core.vistrail.port_spec import PortSpec
     from vistrails.core.interpreter.noncached import Interpreter
 
-    pm = get_package_manager()
-
-    port_spec_per_module = {} # mod_id -> [portspec: PortSpec]
-    j = 0
-    for i, (mod_id, inout, name, sig) in enumerate(add_port_specs):
-        mod_specs = port_spec_per_module.setdefault(mod_id, [])
-        ps = PortSpec(id=i,
-                      name=name,
-                      type=inout,
-                      sigstring=sig,
-                      sort_key=-1)
-        for psi in ps.port_spec_items:
-            psi.id = j
-            j += 1
-        mod_specs.append(ps)
-
-    pipeline = Pipeline()
-    module_list = []
-    for i, (name, identifier, functions) in enumerate(modules):
-        function_list = []
-        try:
-            pkg = pm.get_package(identifier)
-        except MissingPackage:
-            if not enable_pkg:
-                raise
-            enable_package(identifier)
-            pkg = pm.get_package(identifier)
-
-        for func_name, params in functions:
-            param_list = []
-            for j, (param_type, param_val) in enumerate(params):
-                param_list.append(ModuleParam(pos=j,
-                                              type=param_type,
-                                              val=param_val))
-            function_list.append(ModuleFunction(name=func_name,
-                                                parameters=param_list))
-        name = name.rsplit('|', 1)
-        if len(name) == 2:
-            namespace, name = name
-        else:
-            namespace = None
-            name, = name
-        module = Module(name=name,
-                        namespace=namespace,
-                        package=identifier,
-                        version=pkg.version,
-                        id=i,
-                        functions=function_list)
-        for port_spec in port_spec_per_module.get(i, []):
-            module.add_port_spec(port_spec)
-        pipeline.add_module(module)
-        module_list.append(module)
-
-    for i, (sid, sport, did, dport) in enumerate(connections):
-        s_sig = module_list[sid].get_port_spec(sport, 'output').sigstring
-        d_sig = module_list[did].get_port_spec(dport, 'input').sigstring
-        pipeline.add_connection(Connection(
-                id=i,
-                ports=[
-                    Port(id=i*2,
-                         type='source',
-                         moduleId=sid,
-                         name=sport,
-                         signature=s_sig),
-                    Port(id=i*2+1,
-                         type='destination',
-                         moduleId=did,
-                         name=dport,
-                         signature=d_sig),
-                ]))
+    pipeline = build_pipeline(modules, connections, add_port_specs, enable_pkg)
 
     interpreter = Interpreter.get()
     result = interpreter.execute(
@@ -237,8 +124,8 @@ def intercept_result(module, output_name):
     actual_setResult = module.set_output
     old_setResult = module.__dict__.get('set_output', None)
     results = []
-    modules_index = {}  # Maps a Module to an index in the list, so a module
-            # can change its result
+    # Maps a Module to an index in the list, so a module can change its result
+    modules_index = {}
     def new_setResult(self, name, value):
         if name == output_name:
             if self in modules_index:
