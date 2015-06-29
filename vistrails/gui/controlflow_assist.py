VisTrails/VisTrails

commit a68a750e6d609998a39101b6ef48babc15d10859

Make gui controller overridden methods match base
perform_action() was missing parameters, add_module() was only changing
parameter order.

vistrails/gui/controlflow_assist.py
@@ -175,7 +175,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
             for connection in input_connections:
                 self.connection_ids.remove(connection.id)
                 self.controller.delete_connection(connection.id)
-            group_inport_module = self.controller.add_module(input_module.location.x-offset[input_module], input_module.location.y, bm_identifier, 'InputPort')
+            group_inport_module = self.controller.add_module(bm_identifier, 'InputPort',
+                                                             input_module.location.x-offset[input_module], input_module.location.y)
             io_modules.append(group_inport_module)
             offset[input_module] += 130
             for p in group_inport_module.sourcePorts():
@@ -190,7 +191,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
         for connection in output_connections:
             self.connection_ids.remove(connection.id)
             self.controller.delete_connection(connection.id)
-        group_outport_module = self.controller.add_module(output_module.location.x+halfwidth+75, output_module.location.y, bm_identifier, 'OutputPort')
+        group_outport_module = self.controller.add_module(bm_identifier, 'OutputPort',
+                                                          output_module.location.x+halfwidth+75, output_module.location.y)
         io_modules.append(group_outport_module)
         for p in group_outport_module.destinationPorts():
             if p.name == 'InternalPipe':
@@ -207,7 +209,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
         io_connections.extend(self.controller.get_connections_to_and_from(self.controller.current_pipeline, [inner_group.id]))
         
         # Add Map module
-        map_module = self.controller.add_module(inner_group.location.x-120, inner_group.location.y, cf_identifier, 'Map')
+        map_module = self.controller.add_module(cf_identifier, 'Map',
+                                                inner_group.location.x-120, inner_group.location.y)
         io_modules.append(map_module)
         
         # Get group 'self' port object
@@ -217,7 +220,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
                 break
         
         # Add PythonSource
-        py_source_module = self.controller.add_module(inner_group.location.x, inner_group.location.y+75, bm_identifier, 'PythonSource')
+        py_source_module = self.controller.add_module(bm_identifier, 'PythonSource',
+                                                      inner_group.location.x, inner_group.location.y+75)
         io_modules.append(py_source_module)
         group_type = '('+bm_identifier+':Group)'
         bool_type = '('+bm_identifier+':Boolean)'
@@ -301,7 +305,8 @@ print 'InputList: %%s' %% InputList
         # Create and connect InputPort for each of the PythonSource inputs to force it to exist on group
         offset = 165
         for port_type, port_name, list_type, sortkey in add_input_ports:
-            group_inport_module = self.controller.add_module(py_source_module.location.x-offset, py_source_module.location.y+75, bm_identifier, 'InputPort')
+            group_inport_module = self.controller.add_module(bm_identifier, 'InputPort',
+                                                             py_source_module.location.x-offset, py_source_module.location.y+75)
             if (port_type, port_name) in [('input', 'UseCartesianProduct'), ('input', 'UserDefinedInputList')]:
                 self.controller.update_ports_and_functions(group_inport_module.id, [], [], [('optional', [True])])
             io_modules.append(group_inport_module)
@@ -314,7 +319,8 @@ print 'InputList: %%s' %% InputList
                     break
         
         # Create and connect OutputPort from Map to force it to exist on group
-        group_outport_module = self.controller.add_module(map_module.location.x, map_module.location.y-75, bm_identifier, 'OutputPort')
+        group_outport_module = self.controller.add_module(bm_identifier, 'OutputPort',
+                                                          map_module.location.x, map_module.location.y-75)
         io_modules.append(group_outport_module)
         output_portspec = map_module.get_port_spec('Result', 'output')
         for p in group_outport_module.destinationPorts():

vistrails/api/__init__.py
@@ -146,7 +146,7 @@ def add_module(x, y, identifier, name, namespace, controller=None):
         controller = get_current_controller()
     if controller.current_version==-1:
         controller.change_selected_version(0)
-    result = controller.add_module(x, y, identifier, name, namespace)
+    result = controller.add_module(identifier, name, namespace, x, y)
     controller.updatePipelineScene()
     result = controller.current_pipeline.modules[result.id]
     return result

vistrails/gui/vistrail_controller.py
@@ -36,8 +36,6 @@
 
 from __future__ import division
 
-import copy
-import math
 import os
 import uuid
 
@@ -48,6 +46,7 @@ from vistrails.core.configuration import get_vistrails_configuration
 from vistrails.core.data_structures.graph import Graph
 from vistrails.core import debug
 import vistrails.core.db.action
+import vistrails.core.db.io
 from vistrails.core.interpreter.default import get_default_interpreter
 from vistrails.core.vistrail.job import Workflow as JobWorkflow
 from vistrails.core.layout.version_tree_layout import VistrailsTreeLayoutLW
@@ -323,11 +322,11 @@ class VistrailController(QtCore.QObject, BaseController):
             if app and app.view == self.vistrail_view:
                 app.close()
 
-
     ##########################################################################
     # Actions, etc
     
-    def perform_action(self, action, quiet=None):
+    def perform_action(self, action, do_validate=True, raise_exception=False,
+                       quiet=None):
         """ performAction(action: Action, quiet=None) -> timestep
 
         performs given action on current pipeline.
@@ -341,7 +340,9 @@ class VistrailController(QtCore.QObject, BaseController):
         
         """
         if action is not None:
-            BaseController.perform_action(self,action)
+            BaseController.perform_action(self, action,
+                                          do_validate=do_validate,
+                                          raise_exception=raise_exception)
 
             if quiet is None:
                 if not self.quiet:
@@ -368,11 +369,6 @@ class VistrailController(QtCore.QObject, BaseController):
             self.recompute_terse_graph()
 
     ##########################################################################
-
-    def add_module(self, x, y, identifier, name, namespace='', 
-                   internal_version=-1):
-        return BaseController.add_module(self, identifier, name, namespace, x, y,
-                                         internal_version)
 
     def create_abstraction_with_prompt(self, module_ids, connection_ids, 
                                        name=""):
@@ -1380,9 +1376,10 @@ class TestVistrailController(vistrails.gui.utils.TestVisTrailsGUI):
                                         pipeline_view=DummyView(),
                                         auto_save=False)
         controller.change_selected_version(0L)
-        module = controller.add_module(0.0,0.0, 
-                        vistrails.core.system.get_vistrails_basic_pkg_id(), 
-                        'ConcatenateString')
+        module = controller.add_module(
+                vistrails.core.system.get_vistrails_basic_pkg_id(),
+                'ConcatenateString',
+                0.0, 0.0)
         functions = [('str1', ['foo'], -1, True),
                      ('str2', ['bar'], -1, True)]
         controller.update_functions(module, functions)

commit 42f3232b9bbab1fbffcfbdd395c818c61bcfb08d

Fixed broken controlflow assistant
Fixes bug introduced by a68a750e6d609998a39101b6ef48babc15d10859

vistrails/gui/controlflow_assist.py
@@ -176,7 +176,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
                 self.connection_ids.remove(connection.id)
                 self.controller.delete_connection(connection.id)
             group_inport_module = self.controller.add_module(bm_identifier, 'InputPort',
-                                                             input_module.location.x-offset[input_module], input_module.location.y)
+                                                             x=input_module.location.x-offset[input_module],
+                                                             y=input_module.location.y)
             io_modules.append(group_inport_module)
             offset[input_module] += 130
             for p in group_inport_module.sourcePorts():
@@ -192,7 +193,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
             self.connection_ids.remove(connection.id)
             self.controller.delete_connection(connection.id)
         group_outport_module = self.controller.add_module(bm_identifier, 'OutputPort',
-                                                          output_module.location.x+halfwidth+75, output_module.location.y)
+                                                          x=output_module.location.x+halfwidth+75,
+                                                          y=output_module.location.y)
         io_modules.append(group_outport_module)
         for p in group_outport_module.destinationPorts():
             if p.name == 'InternalPipe':
@@ -210,7 +212,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
         
         # Add Map module
         map_module = self.controller.add_module(cf_identifier, 'Map',
-                                                inner_group.location.x-120, inner_group.location.y)
+                                                x=inner_group.location.x-120,
+                                                y=inner_group.location.y)
         io_modules.append(map_module)
         
         # Get group 'self' port object
@@ -221,7 +224,8 @@ class QControlFlowAssistDialog(QtGui.QDialog):
         
         # Add PythonSource
         py_source_module = self.controller.add_module(bm_identifier, 'PythonSource',
-                                                      inner_group.location.x, inner_group.location.y+75)
+                                                      x=inner_group.location.x,
+                                                      y=inner_group.location.y+75)
         io_modules.append(py_source_module)
         group_type = '('+bm_identifier+':Group)'
         bool_type = '('+bm_identifier+':Boolean)'
@@ -306,7 +310,8 @@ print 'InputList: %%s' %% InputList
         offset = 165
         for port_type, port_name, list_type, sortkey in add_input_ports:
             group_inport_module = self.controller.add_module(bm_identifier, 'InputPort',
-                                                             py_source_module.location.x-offset, py_source_module.location.y+75)
+                                                             x=py_source_module.location.x-offset,
+                                                             y=py_source_module.location.y+75)
             if (port_type, port_name) in [('input', 'UseCartesianProduct'), ('input', 'UserDefinedInputList')]:
                 self.controller.update_ports_and_functions(group_inport_module.id, [], [], [('optional', [True])])
             io_modules.append(group_inport_module)
@@ -320,7 +325,8 @@ print 'InputList: %%s' %% InputList
         
         # Create and connect OutputPort from Map to force it to exist on group
         group_outport_module = self.controller.add_module(bm_identifier, 'OutputPort',
-                                                          map_module.location.x, map_module.location.y-75)
+                                                          x=map_module.location.x,
+                                                          y=map_module.location.y-75)
         io_modules.append(group_outport_module)
         output_portspec = map_module.get_port_spec('Result', 'output')
         for p in group_outport_module.destinationPorts():