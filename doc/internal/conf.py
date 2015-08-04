14679517d1bc5d105190967bd1b7fa2cf8d8ab16
Remi Rampin <remirampin@gmail.com>
2015-08-04 17:03:56 -0400

    Add style for ..todo paragraphs

--- doc/internal/conf.py
+++ doc/internal/conf.py
@@ -144,6 +144,8 @@ html_theme = 'classic'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_style = 'custom.css'
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
