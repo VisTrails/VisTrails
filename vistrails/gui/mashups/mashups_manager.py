3cb5b7cc11ebbef5a1a881768b4e2f6cb70b8017
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2015-08-26 16:11:05 +0200

    Added Job support to Mashups
    gui/mashups/mashup_app.py:
      Added progress bar and job handling logic to mashup execution
    
    gui/mashups/mashups_manager.py:
      Copy jobMonitor when copying vt controller

--- vistrails/gui/mashups/mashups_manager.py
+++ vistrails/gui/mashups/mashups_manager.py
@@ -186,6 +186,7 @@ class MashupsManager(object):
                                               pipeline_view=view)
         current_log = vt_controller.log
         newvt_controller.log = current_log
+        newvt_controller.jobMonitor = vt_controller.jobMonitor
         newvt_controller.disable_autosave()
         return newvt_controller
     
@@ -195,6 +196,7 @@ class MashupsManager(object):
         newvt_controller = BaseVistrailController(vistrail, None)
         current_log = vt_controller.log
         newvt_controller.log = current_log
+        newvt_controller.jobMonitor = vt_controller.jobMonitor
         # DAK: why does the base version copy the _mashups but the non-base
         # version doesn't??
         for m in vt_controller._mashups:
