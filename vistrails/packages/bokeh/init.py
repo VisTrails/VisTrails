9c75a5c0e125b7170c831b16b8bd70e460b71e37
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2015-12-18 18:08:37 +0100

    Bokeh package
    A bokeh package including figure methods and charts
    
    BokehOutput module supports these output modes:
    * spreadsheet - Displays plot in a QWebView cell
                    (buttons and zooming does not work)
    * browser - Displays plot in external browser
    * ipython - Displays plot in an IPython notebook
    
    Works with Bokeh v0.11pre3

---
+++ vistrails/packages/bokeh/init.py
@@ -0,0 +1,142 @@
+###############################################################################
+##
+## Copyright (C) 2014-2015, New York University.
+## Copyright (C) 2011-2014, NYU-Poly.
+## Copyright (C) 2006-2011, University of Utah.
+## All rights reserved.
+## Contact: contact@vistrails.org
+##
+## This file is part of VisTrails.
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice,
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright
+##    notice, this list of conditions and the following disclaimer in the
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of the New York University nor the names of its
+##    contributors may be used to endorse or promote products derived from
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+from __future__ import division
+
+import os.path
+
+from vistrails.core.modules.config import ModuleSettings
+from vistrails.core.modules.module_registry import get_module_registry
+from vistrails.core.modules.output_modules import OutputModule, \
+    IPythonMode, OutputMode, OutputModeConfig, ConfigField
+from vistrails.core.wrapper.specs import SpecList, ClassSpec, \
+    FunctionSpec
+from vistrails.core.wrapper.pythonclass import gen_class_module
+from vistrails.core.wrapper.pythonfunction import gen_function_module
+
+
+from identifiers import *
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+_modules = []
+
+class_spec_name = os.path.join(this_dir,'classes.xml')
+class_list = SpecList.read_from_xml(class_spec_name, ClassSpec)
+_modules.extend([gen_class_module(spec) for spec in class_list.module_specs])
+
+func_spec_name = os.path.join(this_dir,'functions.xml')
+func_list = SpecList.read_from_xml(func_spec_name, FunctionSpec)
+_modules.extend([gen_function_module(spec,
+                                     translations=func_list.get_translations())
+                 for spec in func_list.module_specs])
+
+from vistrails.packages.spreadsheet.widgets.webview.webview import WebViewCellWidget
+
+################# OUTPUT MODULES #############################################
+
+
+class bokehBrowserModeConfig(OutputModeConfig):
+    """ Bokeh browser output options
+    browser - the browser type to use (e.g. 'firefox')
+
+    """
+    mode_type = "browser"
+    _fields = [ConfigField('browser', None, str)]
+
+
+class bokehToBrowserMode(OutputMode):
+    """ Show Bokeh plot in external browser
+
+    """
+    priority = 200
+    mode_type = "browser"
+    config_cls = bokehBrowserModeConfig
+
+    @staticmethod
+    def can_compute():
+        return True
+
+    def compute_output(self, output_module, configuration):
+        plot = output_module.get_input('plot')
+        from bokeh.plotting import show, output_file
+        po = output_module.interpreter._file_pool.create_file(suffix='.html')
+        output_file(po.name, mode='inline')
+        kwargs = {}
+        if configuration['browser']:
+            kwargs['browser'] = configuration['browser']
+        show(plot, **kwargs)
+
+
+class bokehToIPythonMode(IPythonMode):
+    """ Show Bokeh plot in ipython notebook
+    Only works after calling output_notebook()
+    Must use output_notebook(resources=INLINE) until 0.11 is released
+
+    """
+    def compute_output(self, output_module, configuration):
+        plot = output_module.get_input('plot')
+        from bokeh.plotting import show
+        show(plot)
+
+
+class BokehOutput(OutputModule):
+    """ Bokeh plot output modes
+
+    """
+    _settings = ModuleSettings(configure_widget="vistrails.gui.modules."
+                       "output_configuration:OutputModuleConfigurationWidget")
+    _input_ports = [('plot', 'plotting|Figure')]
+    _output_modes = [bokehToBrowserMode, bokehToIPythonMode]
+
+    if get_module_registry().has_module('org.vistrails.vistrails.spreadsheet',
+                           'SpreadsheetCell'):
+        from vistrails.packages.spreadsheet.basic_widgets import SpreadsheetMode
+
+        class bokehToSpreadsheetMode(SpreadsheetMode):
+            def compute_output(self, output_module, configuration):
+                plot = output_module.get_input('plot')
+                # TODO: Use CDN when 0.11 is released
+                from bokeh.resources import INLINE
+                from bokeh.embed import file_html
+                html = file_html(plot, INLINE, "my plot")
+                po = output_module.interpreter._file_pool.create_file(suffix='.html')
+                with open(po.name, 'w') as f:
+                    f.write(html)
+                self.display_and_wait(output_module, configuration,
+                                                WebViewCellWidget, (None, po))
+        _output_modes.append(bokehToSpreadsheetMode)
+
+
+_modules.append(BokehOutput)
---
+++ vistrails/packages/bokeh/parser.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+###############################################################################
+##
+## Copyright (C) 2014-2015, New York University.
+## Copyright (C) 2011-2014, NYU-Poly.
+## Copyright (C) 2006-2011, University of Utah.
+## All rights reserved.
+## Contact: contact@vistrails.org
+##
+## This file is part of VisTrails.
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice,
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright
+##    notice, this list of conditions and the following disclaimer in the
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of the New York University nor the names of its
+##    contributors may be used to endorse or promote products derived from
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+
+from __future__ import division
+
+from bokeh.properties import *
+import importlib
+import os.path
+import pydoc
+
+from vistrails.core.wrapper.diff import apply_diff
+from vistrails.core.wrapper.specs import SpecList, ClassSpec, \
+    FunctionSpec
+from vistrails.core.wrapper.python_parser import PythonParser, parse_googledoc
+
+# minor modifications to default_key_to_type
+key_to_type = [['file', 'basic:File'],
+               ['array_like', 'basic:List'],
+               ['ndarray', 'basic:List'],
+               ['array', 'basic:List'],
+               ['sequence', 'basic:List'],
+               ['object', 'basic:List'],
+               ['vector', 'basic:List'],
+               ['Figure', 'plotting:Figure'],
+               ['color', 'basic:Color'], # bokeh color ("#hexvalue")
+               ['list', 'basic:List'],
+               ['tuple', 'basic:List'],
+               ['float', 'basic:Float'],
+               ['scalar', 'basic:Float'],
+               ['double', 'basic:Float'],
+               ['integer', 'basic:Integer'],
+               ['boolean', 'basic:Boolean'],
+               ['bool', 'basic:Boolean'],
+               ['dict', 'basic:Dictionary'],
+               ['string', 'basic:String'],
+               ['int', 'basic:Integer'],
+               ['any', 'basic:Variant'], #
+               ['str', 'basic:String']]
+
+# append type to the name of alternate ports
+alt_suffixes = {'basic:Integer': '(int)',
+                'basic:Float': '(float)',
+                'basic:String': '(str)',
+                'basic:Boolean': '(bool)',
+                'basic:List': '(list)',
+                'basic:Dictionary': '(dict)'}
+
+
+def parse_bokeh_property(prop):
+    """ parse property into string that wrapper can understand
+    """
+    if prop.name == 'filename':
+        return 'file'
+    if hasattr(prop, 'descriptor'):
+        prop = prop.descriptor
+    if isinstance(prop, Color) or isinstance(prop, ColorSpec):
+        # wrapper uses a 3 float color tuple with the color editor
+        return 'color or str'
+    if isinstance(prop, Enum):
+        # wrapper needs {'x', 'y'}
+        return str(prop).replace('Enum(', '{').replace(')', '}')
+    if isinstance(prop, String):
+        return 'str'
+    if isinstance(prop, Interval):
+        return str(prop.interval_type)
+    if isinstance(prop, DataSpec):
+        # DataSpec's can be lists
+        # wrapper understands 'or'
+        return ('string or dict or %(typ)s or list' %
+                {'typ': str(prop.type_params[2])})
+    if isinstance(prop, Either):
+        # wrapper understands 'or'
+        return ' or '.join([str(t) for t in reversed(prop.type_params)])
+    prop_to_type = [
+        [Regex, 'str'],
+        [Complex, 'list'],
+        [JSON, 'str'],
+        [Size, 'float'],
+        [Percent, 'float'],
+        [DistanceSpec, 'float'],
+        [Angle, 'float']]
+    for key, val in prop_to_type:
+        if isinstance(prop, key):
+            return val
+    return str(prop)
+
+
+def bokeh_property_parser(obj):
+    """ extract bokeh property information from an object
+    """
+    from bokeh.properties import Property
+    args = []
+    for argname in dir(obj):
+        arg = getattr(obj, argname)
+        if isinstance(arg, Property):
+            doc = (arg.__doc__ or '').strip()
+            if doc:
+                desc = doc + '\n' + 'Property: ' + str(arg)
+            else:
+                desc = 'Property: ' + str(arg)
+            prop = parse_bokeh_property(arg) + ', optional'
+            default = arg.class_default(obj)
+            if default is not None:
+                desc = desc + '\ndefault is ' + repr(default)
+            args.append((argname, prop, desc))
+    # check for docstring references
+    docstring = pydoc.getdoc(obj)
+    if hasattr(obj, '__module__') and 'bokeh.charts.builders.' in obj.__module__:
+        from bokeh.charts.chart_options import ChartOptions
+        args.extend(bokeh_property_parser(ChartOptions)[1])
+    if 'styling_line_properties' in docstring:
+        from bokeh.mixins import LineProps
+        args.extend(bokeh_property_parser(LineProps)[1])
+    if 'styling_fill_properties' in docstring:
+        from bokeh.mixins import FillProps
+        args.extend(bokeh_property_parser(FillProps)[1])
+    if 'styling_text_properties' in docstring:
+        from bokeh.mixins import TextProps
+        args.extend(bokeh_property_parser(TextProps)[1])
+    return '', args, [], [], []
+
+
+def parse_fig_method(f):
+    """ Figure methods use properties from corresponding marker/glyph
+    """
+    from bokeh.models import markers
+    from bokeh.models import glyphs
+    # CamelCase translation works for almost all method names
+    try:
+        marker = getattr(markers, f.__name__)
+    except:
+        marker = getattr(glyphs, f.__name__)
+    return bokeh_property_parser(marker)
+
+
+# Bokeh uses css colors, so we translate them to hex strings
+# outputs are probably not used
+# this must define input_t and output_t translator functions
+# This does not work on subclasses, they need to be defined explicitly
+translations = {
+    'basic:Color':
+        "def input_t(value):\n"
+        "    return '#%02X%02X%02X' % tuple([int(c*255) for c in value.tuple])\n"
+        "def output_t(value):\n" # Assumes receiving hex string, which is probably wrong
+        "    from vistrails.core.utils import InstanceObject\n"
+        "    return InstanceObject(tuple=tuple([float(ord(c))/255.0 for c in value.replace('#', '').decode('hex')]))",
+    'basic:Path':
+        "def input_t(value):\n"
+        "    return value.name\n"
+        "def output_t(value):\n"
+        "    from vistrails.core.modules.basic_modules import PathObject\n"
+        "    return PathObject(tuple=value)"}
+
+
+def parser():
+    """ Wraps Figure class, figure function, figure methods, and charts
+
+    """
+    parser = PythonParser(instance_type='plotting|Figure',
+                          parsers=[bokeh_property_parser, parse_googledoc],
+                          key_to_type=key_to_type,
+                          alt_suffixes=alt_suffixes)
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    ##########################################################################
+    # wrap classes
+
+    class_spec_name = os.path.join(this_dir,'classes.xml')
+    raw_class_spec_name = class_spec_name[:-4] + '-raw.xml'
+    class_spec_diff = class_spec_name[:-4] +  '-diff.xml'
+
+    # generate specs
+    classes = []
+    class_names = []
+    functions = []
+    func_names = []
+
+    # wrap Figure class
+    def add(base, clist, namespace):
+        if isinstance(clist, basestring):
+            clist = clist.split(',')
+        for c in clist:
+            klass = base + '.' + c
+            if klass in class_names:
+                print "Skipping duplicate:", klass
+                continue
+            class_names.append(klass)
+            classes.append(parser.parse_class(klass,
+                                              namespace=namespace,
+                                              attribute_parsing=False))
+            inspector = parser.parse_class(klass,
+                                           name=c+'Inspector',
+                                           namespace=namespace,
+                                           argument_parsing=False)
+            if (len(inspector.input_port_specs) > 1 or
+                len(inspector.output_port_specs) > 1):
+                classes.append(inspector)
+            for f in parser.parse_class_methods(klass,
+                                         namespace=namespace+'|'+c+'Methods'):
+                fname = (f.namespace, f.module_name)
+                if fname in func_names:
+                    print "Skipping duplicate:", fname
+                    continue
+                func_names.append(fname)
+                functions.append(f)
+    add('bokeh.plotting', 'Figure', namespace='plotting')
+
+    # Wrap figure methods
+    fig_method_parser = PythonParser(parsers=[parse_fig_method, parse_googledoc],
+                                     key_to_type=key_to_type,
+                                     alt_suffixes=alt_suffixes)
+    from bokeh.plotting import Figure
+    for name in dir(Figure):
+        arg = getattr(Figure, name)
+        if hasattr(arg, '__module__') and arg.__module__ == 'bokeh.plotting_helpers':
+            functions.append(fig_method_parser.parse_function(
+                                  arg,
+                                  name=name,
+                                  code_ref=name,
+                                  namespace='plotting',
+                                  method='plotting|Figure',
+                                  is_empty=True))
+
+    fun_spec_name = os.path.join(this_dir, 'functions.xml')
+    raw_fun_spec_name = fun_spec_name[:-4] + '-raw.xml'
+    fun_spec_diff = fun_spec_name[:-4] + '-diff.xml'
+
+    # wrap figure function
+    # figure function gets properties from Figure class
+    from bokeh.plotting import Figure
+    parse_figure = lambda x: bokeh_property_parser(Figure)
+    figure_parser = PythonParser(instance_type='plotting|Figure',
+                                 parsers=[parse_figure, parse_googledoc],
+                                 key_to_type=key_to_type,
+                                 alt_suffixes=alt_suffixes)
+    figure = figure_parser.parse_function('bokeh.plotting.figure',
+                                          namespace='plotting',
+                                          output_type='plotting|Figure')
+    functions.append(figure)
+
+    # wrap charts
+    from bokeh import charts
+    for name in dir(charts):
+        chart = getattr(charts, name)
+        if hasattr(chart, '__module__') and 'bokeh.charts.builders.' in chart.__module__:
+            figure = parser.parse_function('bokeh.charts.' + name,
+                                           namespace='charts')
+            functions.append(figure)
+
+    class_list = SpecList(classes, translations)
+    class_list.write_to_xml(raw_class_spec_name)
+    apply_diff(ClassSpec, raw_class_spec_name, class_spec_diff, class_spec_name)
+    fun_list = SpecList(functions, translations)
+    fun_list.write_to_xml(raw_fun_spec_name)
+    apply_diff(FunctionSpec, raw_fun_spec_name, fun_spec_diff, fun_spec_name)
+    return class_list, fun_list
+
+if __name__ == '__main__':
+    parser()
