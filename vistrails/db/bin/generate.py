970043fc08769b328c66ee92860199ed02ceb109
Tommy Ellqvist <tommy.ellqvist@nyu.edu>
2016-01-07 12:54:19 +0100

    Updates copyright template in db/bin/generate.py

--- vistrails/db/bin/generate.py
+++ vistrails/db/bin/generate.py
@@ -72,7 +72,7 @@ class DAOList(dict):
 COPYRIGHT_NOTICE = \
 """###############################################################################
 ##
-## Copyright (C) 2014-2015, New York University.
+## Copyright (C) 2014-2016, New York University.
 ## Copyright (C) 2011-2014, NYU-Poly.
 ## Copyright (C) 2006-2011, University of Utah.
 ## All rights reserved.
