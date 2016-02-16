5aed1bd1c5ca7ed349e87648a9f49c1c3db334a2
Remi Rampin <remirampin@gmail.com>
2016-02-16 16:34:31 -0500

    IPython bug 6709 is fixed

--- vistrails/core/api.py
+++ vistrails/core/api.py
@@ -657,8 +657,6 @@ class ModuleClass(type):
         raise TypeError("Don't use module_class(....), call "
                         "vistrail.add_module(module_class, ...) instead")
 
-    # Ignored by IPython because of bug 6709
-    # https://github.com/ipython/ipython/issues/6709
     def __repr__(self):
         return "<Module class %r from %s>" % (self.descriptor.name,
                                               self.descriptor.identifier)
