69a61d6b0f87806b3106d189623b9a48dd70b529
David Koop <dkoop@umassd.edu>
2016-08-19 11:16:20 -0400

    Update 2.0.0 copyright and xml processing code

--- vistrails/db/versions/v2_0_0/domain/mashup.py
+++ vistrails/db/versions/v2_0_0/domain/mashup.py
@@ -1,6 +1,7 @@
 ###############################################################################
 ##
-## Copyright (C) 2011-2012, NYU-Poly.
+## Copyright (C) 2014-2016, New York University.
+## Copyright (C) 2011-2014, NYU-Poly.
 ## Copyright (C) 2006-2011, University of Utah.
 ## All rights reserved.
 ## Contact: contact@vistrails.org
@@ -15,7 +16,7 @@
 ##  - Redistributions in binary form must reproduce the above copyright
 ##    notice, this list of conditions and the following disclaimer in the
 ##    documentation and/or other materials provided with the distribution.
-##  - Neither the name of the University of Utah nor the names of its
+##  - Neither the name of the New York University nor the names of its
 ##    contributors may be used to endorse or promote products derived from
 ##    this software without specific prior written permission.
 ##
@@ -32,6 +33,8 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ##
 ###############################################################################
+
+from __future__ import division
 
 from auto_gen import DBMashuptrail as _DBMashuptrail
 from id_scope import IdScope
