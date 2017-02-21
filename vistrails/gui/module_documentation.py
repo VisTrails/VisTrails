e1830c368b809af17f0228e8998a0e7280dd3610
Remi Rampin <remirampin@gmail.com>
2017-02-21 15:20:21 -0500

    Change module doc from QTextEdit to QTextBrowser

--- vistrails/gui/module_documentation.py
+++ vistrails/gui/module_documentation.py
@@ -66,15 +66,9 @@ class QModuleDocumentation(QtGui.QDialog, QVistrailsPaletteInterface):
         self.layout().addWidget(self.name_label)
         self.package_label = QtGui.QLabel("")
         self.layout().addWidget(self.package_label)
-        # self.closeButton = QtGui.QPushButton('Ok', self)
-        self.textEdit = QtGui.QTextEdit(self)
+        self.textEdit = QtGui.QTextBrowser(self)
         self.layout().addWidget(self.textEdit, 1)
-        self.textEdit.setReadOnly(True)
-        self.textEdit.setTextCursor(QtGui.QTextCursor(self.textEdit.document()))
-        # self.layout().addWidget(self.closeButton)
-        # self.connect(self.closeButton, QtCore.SIGNAL('clicked(bool)'), 
-        #              self.close)
-        # self.closeButton.setShortcut('Enter')
+        self.textEdit.setOpenExternalLinks(True)
 
         self.update_descriptor()
 
